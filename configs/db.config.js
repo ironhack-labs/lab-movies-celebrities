6a3c76bde3964ec2e9085b5d7b7bfd92d2c179fb
sandrabosk <sandraboskovic@hotmail.com>
2021-01-16 21:00:22 +0100

    add movies and celebrities updated

---
+++ configs/db.config.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-movies-celebrities";
+
+mongoose
+  .connect(MONGO_URI, {
+    useCreateIndex: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then((x) => console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`))
+  .catch((err) => console.error("Error connecting to mongo", err));
