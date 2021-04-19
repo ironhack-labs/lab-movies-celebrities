23cea45653b5548c3d0c1c30b738f49da893ebd1
alescofet <alex.escofet96@gmail.com>
2021-04-19 18:34:12 +0200

    Iteration 4 done

---
+++ models/Movie.model.js
@@ -0,0 +1,11 @@
+const mongoose = require(`mongoose`)
+const Schema = mongoose.Schema
+const movieSchema = new Schema({
+    title:String,
+    genre: String,
+    plot: String,
+    cast: {type: Schema.Types.ObjectId, ref: `Celebrity.model`}
+}, {versionKey:false, timestamps:true})
+const Movie = mongoose.model(`Movie`, movieSchema)
+
+module.exports = Movie
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const Celebrity = require("../models/celebrity.model");
+const Celebrity = require("../models/Celebrity.model");
 const { get } = require("./movies.routes");
 const router = express.Router();
 
