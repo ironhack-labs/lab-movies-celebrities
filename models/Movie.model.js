21b47a473b1b638c197c878e6e10b2b176fa6b44
alescofet <alex.escofet96@gmail.com>
2021-04-19 22:10:03 +0200

    Lab done

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -4,7 +4,7 @@ const movieSchema = new Schema({
     title: String,
     genre: String,
     plot: String,
-    cast: [{type: Schema.Types.ObjectId, ref: `Celebrity.model`}]
+    cast: [{type: Schema.Types.ObjectId, ref: `Celebrity`}]
 }, {versionKey:false, timestamps:true})
 const Movie = mongoose.model(`Movie`, movieSchema)
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -46,4 +46,50 @@ router.get(`/all`, (req, res) => {
     });
 });
 
+router.get(`/:_id`,(req,res)=>{
+Movie.findById(req.params)
+.populate(`cast`)
+.then((result) => {
+    res.render("movies/movie-details", {movie: result})
+}).catch((err) => {
+    console.log(err);
+    
+});
+})
+
+router.post(`/:_id/delete`, (req,res)=>{
+    Movie.findByIdAndDelete(req.params._id)
+    .then((result) => {
+        console.log(result)
+        res.redirect(`/movies/all`)
+    }).catch((err) => {
+        console.log(err);  
+    }); 
+})
+
+router.get(`/:_id/edit`, (req,res)=>{
+    Celebrity.find()
+    .then((result) => {
+        res.render(`movies/edit-movie`, {celebs:result})
+    }).catch((err) => {
+        console.log(err);
+        
+    });
+})
+
+router.post(`/:_id/edit`, (req,res)=>{
+    const {title,genre,plot} = req.body
+    Movie.findByIdAndUpdate(req.params._id, {title: title, genre: genre, plot: plot})
+    .then((result) => {
+        Movie.findByIdAndUpdate(result._id, {$push:{cast: req.body.cast}})
+        .then((result2)=>{
+            console.log(result2);
+            res.redirect("/movies/all")
+        })
+    }).catch((err) => {
+        console.log(err);  
+    }); 
+})
+
+router.post
 module.exports = router;
