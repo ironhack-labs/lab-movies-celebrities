c1632077b610ac5cb84f91c89f255da9b6cf380e
Raquel Rodriguez <srtademaciel@gmail.com>
2021-04-20 09:43:03 +0200

    done without styles

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -31,4 +31,52 @@ router.get('/celebrities', (req, res)=>{
     
 })
 
+router.get('/celebrities/:_id', (req, res)=>{
+    Celebrity.findById(req.params)
+   .then((result)=>{
+    console.log(result)
+    res.render('celebrities/celebrity-details', {celeb: result})
+   })
+   .catch((error)=>{
+   console.log(error)
+   })
+   
+})
+
+router.post('/celebrities/:_id/delete', (req, res) => {
+    Celebrity.findByIdAndDelete(req.params._id)
+    .then((result)=>{
+    console.log(result)
+    res.redirect('/celebrities')
+    })
+    .catch((error)=>{
+    console.log(error)
+    })
+    
+})
+
+router.get('/celebrities/:_id/edit', (req, res) => {
+    Celebrity.findById(req.params._id)
+    /* .populate('cast') */
+    .then((result)=>{
+    res.render('celebrities/edit-celebrity', result)
+    })
+    .catch((error)=>{
+    console.log(error)
+    })
+    
+})
+
+router.post('/celebrities/:_id/edit', (req, res) => {
+    Celebrity.findByIdAndUpdate(req.params._id, req.body)
+    .then((result)=>{
+    console.log(result)
+    res.redirect('/celebrities')
+    })
+    .catch((error)=>{
+    console.log(error)
+    })
+    
+})
+
 module.exports = router
