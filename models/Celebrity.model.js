74b2d7fbe020f941aca10bc6c720475915db593d
Israel Zambrana <zambrana90@gmail.com>
2021-06-19 14:05:58 +0200

    iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,27 @@
 //  Add your code here
+
+const mongoose = require("mongoose");
+const celebritySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: true,
+    },
+    catchPhrase: {
+      type: String,
+      required: true,
+    },
+  },
+
+  {
+    timestamps: true,
+  }
+);
+
+const Celebrity = mongoose.model("Drone", celebritySchema);
+
+module.exports = Celebrity;
