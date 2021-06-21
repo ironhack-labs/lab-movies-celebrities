8877799ddd4e2662b36efdd8bd2bc481060a9d3e
Paloma Borro <palomabs93@gmail.com>
2021-06-21 17:05:46 +0200

    Iteración 3 con error 404

--- app.js
+++ app.js
@@ -25,8 +25,12 @@ const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerC
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
 // 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
+const indexRoutes = require('./routes/index');
+const celebritiesRoutes = require('./routes/celebrities.routes');
+const moviesRoutes = require('./routes/movies.routes');
+app.use('/', indexRoutes);
+app.use('/celebrities', celebritiesRoutes);
+app.use('/movies', moviesRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
