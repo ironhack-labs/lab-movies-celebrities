0c5a71769ba4f8571d5f4484a4e2c03ba21189f0
Andrei <andrei_1002@hotmail.com>
2021-06-23 11:09:03 +0200

    añadidos controllers

--- controllers/celebrities.controller.js
+++ controllers/celebrities.controller.js
@@ -1,6 +1,24 @@
 const Celebrity = require("../models/Celebrity.model");
 
-module.exports.createCelebrity = ((req, res, next) => {
-    res.render("/celebrities/new-celebrity")
+
+module.exports.create =((req, res, next) => {
+    res.render("celebrities/new-celebrity")
 })
 
+module.exports.doCreate =((req, res, next) => {
+    Celebrity.create(req.body)
+        .then(() => {
+            res.redirect("/celebrities")
+        })
+        .catch(() => {
+            res.render("celebrities/new-celebrity")
+        })
+});
+
+module.exports.list =((req, res, next) => {
+    Celebrity.find()
+    .then((celebrities) =>{
+        res.render("celebrities/celebrities", { celebrities })
+    })
+    .catch(e => console.error(e))
+})
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,28 +1,11 @@
 const router = require("express").Router();
-const Celebrity = require("../models/Celebrity.model")
+const Celebrity = require("../models/Celebrity.model");
+const celebrityControllers = require("../controllers/celebrities.controller");
 
-router.get("/", (req, res, next) => {
-    Celebrity.find()
-        .then((celebrities) =>{
-            res.render("celebrities/celebrities", { celebrities })
-        })
-        .catch(e => console.error(e))
-})
+router.get("/", celebrityControllers.list)
 
-router.get("/create", (req, res, next) => {
-    res.render("celebrities/new-celebrity")
-} );
+router.get("/create", celebrityControllers.create);
 
-router.post("/create", (req, res, next) => {
-    Celebrity.create(req.body)
-        .then(() => {
-            res.redirect("/celebrities")
-        })
-        .catch(() => {
-            res.render("celebrities/new-celebrity")
-        })
-});
-
-
+router.post("/create", celebrityControllers.doCreate);
 
 module.exports = router;
