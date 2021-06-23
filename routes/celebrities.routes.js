dc55665518c92a1a097c0f4a04d257b1af9f0e01
Andrei <andrei_1002@hotmail.com>
2021-06-23 11:02:14 +0200

    terminada iteración 4a

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,14 @@
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model")
 
+router.get("/", (req, res, next) => {
+    Celebrity.find()
+        .then((celebrities) =>{
+            res.render("celebrities/celebrities", { celebrities })
+        })
+        .catch(e => console.error(e))
+})
+
 router.get("/create", (req, res, next) => {
     res.render("celebrities/new-celebrity")
 } );
@@ -13,6 +21,8 @@ router.post("/create", (req, res, next) => {
         .catch(() => {
             res.render("celebrities/new-celebrity")
         })
-})
+});
+
+
 
 module.exports = router;
