a982e8c8f587871a2944ee3b18340d110681b8cd
Marcos Salvo <marcossalvo95@gmail.com>
2021-06-23 14:08:37 +0200

    Bonus hecho

--- controllers/celebrities.controller.js
+++ controllers/celebrities.controller.js
@@ -22,3 +22,36 @@ module.exports.list =((req, res, next) => {
     })
     .catch(e => console.error(e))
 })
+
+module.exports.detail = ((req, res, next) => {
+    const { id } = req.params;
+    Celebrity.findById(id)
+        .then((celebrity) => {
+            console.log(celebrity)
+            res.render("celebrities/celebrity-details", celebrity )
+        } )
+})
+
+module.exports.delete = ((req, res, next) => {
+    const { id } = req.params;
+    Celebrity.findByIdAndDelete(id)
+        .then(() => {
+            res.redirect("/celebrities")
+        })
+        .catch((e) => console.error(e))
+});
+
+module.exports.edit = ((req, res, next) => {
+    const { id } = req.params
+    Celebrity.findById(id)
+        .then((celebrity) => {
+            res.render("celebrities/edit-celebrity", celebrity)
+        })
+        .catch((e) => console.error(e))
+})
+
+module.exports.doEdit = ((req, res, next) => {
+    Celebrity.findByIdAndUpdate(req.params.id, req.body)
+        .then(() => res.redirect(`/celebrities/${req.params.id}`))
+        .catch(() => res.redirect(`/celebrities/${req.params.id}/edit`))
+})
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -8,4 +8,12 @@ router.get("/create", celebrityControllers.create);
 
 router.post("/create", celebrityControllers.doCreate);
 
+router.get("/:id", celebrityControllers.detail);
+
+router.post("/:id/delete", celebrityControllers.delete);
+
+router.get("/:id/edit", celebrityControllers.edit)
+
+router.post("/:id/edit", celebrityControllers.doEdit)
+
 module.exports = router;
