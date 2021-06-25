75eacc0f216c5c2dee1d3a821d4ba7e9321b5fab
Luca TC <lucatuveri@outlook.it>
2021-06-25 23:34:37 +0200

    set work

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,15 @@
-//  Add your code here
+/*Iteration #2: The Celebrity model
+Our first step is to create the Celebrity model and add some celebrities in our database.
+
+The Celebrity model should have:
+
+name - String (like Tom Cruise, Beyonce, Daffy Duck, etc.)
+occupation - String (what the celebrity does, why they are famous. 
+    For example actor,singer, comedian, or you can put unknown if your celebrity is someone like Kim Kardashian)
+catchPhrase - String (every celebrity needs a good catch phrase.
+    Well maybe not all of them have one in real life, but all of our celebrities will have a catch phrase.
+        This can be pretty much anything.)
+Go ahead and locate the Celebrity.model.js model file in the models folder. 
+Using schema,
+create the Celebrity model with the above mentioned properties. Don't forget to export the model. 2.
+In the Celebrity.model.js model file: */
---
+++ routes/movies-routes.js
@@ -0,0 +1,7 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+// all your routes here
+
+module.exports = router;
+/* remember to link these two new files to either app.js or routes/index.js so your server has access to them. */
