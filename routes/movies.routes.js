c9dd7b297b7db85d9de7c54b53ae4a765a5ca201
Silviu Dilimot <silviudilimot@gmail.com>
2021-06-26 22:31:25 +0200

    To do: enable multiple celebs selection. Properly populate drop=down list on edit-movie page

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -40,8 +40,7 @@ router.post("/movie/create", (req, res, next) => {
     if(!validationConst){
         res.render('movies/new-movie', {errorMessage: `All fields are mandatory.`})
         return
-    }
-    
+    }   
 
     Movie
         .findOne({title})
@@ -52,53 +51,57 @@ router.post("/movie/create", (req, res, next) => {
             }
         })
 
-
-
     Movie
         .create( movie)
-        .then( () => res.send(movie))
-        // .then( () => res.redirect('/movies'))
+        // .then( () => res.send(movie))
+        .then( () => res.redirect('/movies'))
         .catch(err => console.log(err))
-
 });
 
+router.get('/movies/:id', (req, res, next) => {
+    // res.send("Success!!!")
+    const {id} = req.params
+    Movie
+        .findById(id)
+        .populate('cast')
+        .then(movie => {
+            res.render('movies/movie-details', movie)
+        })
+})
 
-// router.post("/movie/create", (req, res, next) => {
-//     const movie = req.body
-//     const {title, image, genre, plot, cast} = movie
-//     const validationConst = title && genre && image && plot 
-
-//     if(!validationConst){
-//         res.render('movies/new-movie', {errorMessage: `All fields are mandatory.`})
-//         return
-//     }
-    
-
-//     Movie
-//         .findOne({title})
-//         .then( foundMovie => {
-//             if(foundMovie){
-//                 res.render('movies/new-movie', {errorMessage: `${movie} already registered.`})
-//                 return
-//             }
-//         })
+router.post('/movie/:id/delete', (req, res, next) => {
+    const {id} = req.params
+    Movie   
+        .findByIdAndRemove(id)
+        .then(deleted => {
+            res.redirect('/movies')
+        })
+        .catch(err => res.send("Error. Can't delete, can't programm."))
+})
 
 
+router.get("/movie/:id/edit", (req, res, next) => {
+    const {id} = req.params
+    Movie
+        .findById(id)
+        .populate('cast')
+        // .then( movie => res.send({movie} ))    
+        .then( movie => res.render('movies/edit-movie', movie ))    
+});
 
-//     Movie
-//         .create( movie)
-//         .then( () => res.redirect('/movies'))
-//         .catch(err => console.log(err))
+router.post('/movie/:id/edit', (req, res, next) => {
+    const {id} = req.params
+    const {title, genre, image, plot, cast} = req.body
+    Movie
+        .findByIdAndUpdate(id, {title, genre, image, plot})
+        .then( () => res.redirect('/movies'))
+        .catch( (err) => res.send(`Get a grip. You've got errors: ${err}`))
+        
+})
 
+// router.get("/movie-details", (req, res, next) => {
+//     res.render('movies/movie-details')
 // });
-
-    router.get("/edit-movie", (req, res, next) => {
-        res.render('movies/edit-movie')
-    });
-
-    router.get("/movie-details", (req, res, next) => {
-        res.render('movies/movie-details')
-    });
 
 
 
