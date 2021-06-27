881222b6c6e9980156d43a4956f8c7f81057398c
Luca TC <lucatuveri@outlook.it>
2021-06-27 13:17:31 +0200

    edit movies handling

--- routes/movies-routes.js
+++ routes/movies-routes.js
@@ -27,17 +27,15 @@ router.get('/movies/list', (req, res) => {
         .then(movies => res.render('./../views/movies/movies', { movies }))
         .catch(err => console.log(err))
 })
+/*GET movie details*/
+router.get('/movie/details', (req,res)=> {
+    
+    const { movie_id } = req.query
 
+    console.log('movie id req',movie_id)
+    Movie
+        .findById(movie_id)
+        .then(theMovie => res.render('./../views/movies/edit-movie', theMovie))
+        .catch(err => console.log(err))})
 
 module.exports = router;
-/* remember to link these two new files to either app.js or routes/index.js so your server has access to them. */
-/*
-Create the /celebrities/create POST route in routes/celebrities.routes.js.
-In that route we have to create an instance of the Celebrity model
-(don't forget, we should get all the info from the form through req.body)
-If there is an error, render the celebrities/new-celebrity view so the user can try again and
-If there is no error, redirect to the page with the list of celebrities.
-This route will be created in the next iteration /celebrities
-In the views/index.hbs view file:
-Add a link that goes to the page you just created with the form to create a new celebrity.
- */
