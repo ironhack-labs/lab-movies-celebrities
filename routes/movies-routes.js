cbd7da95b999661ef1d48351d9b0697a28d1bad1
Luca TC <lucatuveri@outlook.it>
2021-06-27 21:17:49 +0200

    9.6 handling with edit

--- routes/movies-routes.js
+++ routes/movies-routes.js
@@ -27,33 +27,48 @@ router.get('/movies/list', (req, res) => {
         .catch(err => console.log(err))
 })
 /*GET movie details*/
-router.get('/movie/details/:movie_id', (req,res)=> {
-    
+router.get('/movie/details/:movie_id', (req, res) => {
+
     const { movie_id } = req.params
 
-    
+
     Movie
-        .findById(movie_id)        
+        .findById(movie_id)
         // .then(themovie => console.log('themovie', themovie)) 
-        .populate('cast')         
+        .populate('cast')
         .then(theMovie => res.render('movies/movie-details', theMovie))
-        .catch(err => console.log(err))})
+        .catch(err => console.log(err))
+})
+/*POST movie delete */
+router.post('/movies/:movie_id/delete', (req, res) => {
+    const { movie_id } = req.params
+       
+    Movie    
+        .findByIdAndRemove(movie_id)
+        .then(movies => res.redirect('/movies/list'))
+})
+
+
+/*GET movie edit */
+router.get('/movie/edit', (req, res) => {
+
+    const { movie_id } = req.query
+
+    Movie
+        .findById(movie_id)
+        .then(movie => res.render('movies/edit-movie', movie))
+        .catch(err => console.log(err))
+})
+
+
+
+
+
 
 module.exports = router;
 /**
 Iteration #9: Deleting Movies
-Now that we have a list of movies, a movie details page, and a page to create new movies, 
-we only have 2 features left to implement: editing movies and deleting them. 
-Since deleting is simpler, let's start with that.
 
-
-Route /movies/:id/delete		HTTP Verb POST	Description Delete a specific movie
-	
-Steps we will follow in this iteration:
-In the movies/movie-details.hbs file:
-Add a <form> tag that makes a POST request to /movies/:id/delete where the :id is replaced by the actual id of the movie.
-Add a <button> tag inside the form so that it can be submitted.
-Create the /movies/:id/delete POST route in your routes/movies.routes.js file
 In the route:
 Use the Movie model's findByIdAndRemove() method to delete the specific movie by its id.
 If everything is good (.then()), redirect to the list of movies page
