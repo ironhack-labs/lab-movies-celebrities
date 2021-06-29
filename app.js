93118d9d0507c48dbf66d67108429a4147bd0b69
Morgane Deschamps <morgane.deschamps@gmail.com>
2021-06-29 12:17:28 +0200

    starting it3

--- app.js
+++ app.js
@@ -24,9 +24,22 @@ const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerC
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 
+
+
+
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const celebrities = require('./routes/celebrities.routes');
+app.use('/celebrities', index);
+
+const movies = require('./routes/movies.routes');
+app.use('/movies', index);
+
+
+
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,14 @@
 //  Add your code here
+const mongoose = require('mongoose');
+const { Schema, model } = mongoose;
+
+const celebritySchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchphrase: String
+  }
+);
+
+
+module.exports = model('Celebrity', celebritySchema);
