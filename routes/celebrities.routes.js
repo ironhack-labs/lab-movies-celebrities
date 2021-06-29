c46d39790d303b354728470171a670c21c720a50
Joao Pedro <joaopbbezerra@gmail.com>
2021-06-29 17:42:28 +0100

    Bonus

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -33,7 +33,28 @@ router.get("/celebrities", async(req, res)=>{
     }
 })
 
+router.get("/celebrities/:celebrityId", async (req, res)=>{
+    const celebrityDetail = await CelebrityModel.findById(req.params.celebrityId)
+    res.render("celebrities/celebrity-details", {celebrityDetail})
+})
 
+router.get("/celebrities/:celebrityId/edit", async (req, res)=>{
+    const celebrityEdit = await CelebrityModel.findById(req.params.celebrityId)
+    res.render("celebrities/celebrity-edit", {celebrityEdit})
+})
 
+router.post("/celebrities/:celebrityId/edit", async (req, res)=>{
+    const {name, occupation, catchPhrase} = req.body
+    await CelebrityModel.findByIdAndUpdate(req.params.celebrityId),{
+        name,
+        occupation,
+        catchPhrase
+    }
+    res.redirect("/celebrities")
+})
 
+router.post("/celebrities/:celebrityId/delete", async (req, res)=>{
+    await CelebrityModel.findByIdAndDelete(req.params.celebrityId)
+    res.redirect("/celebrities")
+})
 module.exports = router;
