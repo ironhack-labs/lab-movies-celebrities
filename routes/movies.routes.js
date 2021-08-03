d9ede39a402b83b7684054327d77edb4dec366c3
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-08-04 00:30:57 +0300

    complete iteration 5-7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,36 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
-// all your routes here
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((moviesFromDb) => {
+      res.render("movies/movies", { moviesFromDb });
+    })
+    .catch((err) => console.log(`Err while rendering movie list: ${err}`));
+});
+
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find()
+    .then((celebritiesFromDb) => {
+      console.log("CELEB in MOVIES", celebritiesFromDb);
+      res.render("movies/new-movie", { celebritiesFromDb });
+    })
+    .catch((err) => console.log(`Err while creating movie: ${err}`));
+});
+
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+
+  Movie.create({ title, genre, plot, cast })
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log(`Error while adding new movie to the DB: ${err}`);
+      res.redirect("movies/new-movie");
+    });
+});
 module.exports = router;
