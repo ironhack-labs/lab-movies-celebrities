e027799a9639f288bd80433e131ac6640d043289
Karina Narkeviciute <karina.narkeviciute@gmail.com>
2021-08-04 01:34:11 +0300

    complete iteration 8-9

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -15,7 +15,6 @@ router.get("/movies", (req, res, next) => {
 router.get("/movies/create", (req, res, next) => {
   Celebrity.find()
     .then((celebritiesFromDb) => {
-      console.log("CELEB in MOVIES", celebritiesFromDb);
       res.render("movies/new-movie", { celebritiesFromDb });
     })
     .catch((err) => console.log(`Err while creating movie: ${err}`));
@@ -33,4 +32,64 @@ router.post("/movies/create", (req, res, next) => {
       res.redirect("movies/new-movie");
     });
 });
+
+router.get("/movies/:movieId", (req, res, next) => {
+  const movieId = req.params.movieId;
+
+  Movie.findById(movieId)
+    .populate("cast")
+    .then((singleMovieFromDb) => {
+      res.render("movies/movie-details", { movie: singleMovieFromDb });
+    })
+    .catch((err) => {
+      console.log(`Error while rendering movie details`, err);
+      next(err);
+    });
+});
+
+//
+// EDIT
+//
+router.get("/movies/:movieId/edit", (req, res, next) => {
+  const movieId = req.params.movieId;
+
+  Movie.findById(movieId)
+    .populate("cast")
+    .then((singleMovieFromDb) => {
+      res.render("movies/edit-movie", { movie: singleMovieFromDb });
+    })
+    .catch((error) => {
+      console.log("error displaying movie editor", error);
+      next(error);
+    });
+});
+
+router.post("/movies/:movieId/edit", (req, res, next) => {
+  const { movieId } = req.params.movieId;
+  const { title, genre, plot, cast } = req.body;
+
+  Movie.findByIdAndUpdate(movieId, { title, genre, plot, cast }, { new: true })
+    .then(() => {
+      res.redirect("movies/movie-details");
+    })
+    .catch((error) => {
+      console.log("error editing movie", error);
+      next(error);
+    });
+});
+
+//
+// DELETE
+//
+router.post("/movies/:movieId/delete", (req, res, next) => {
+  const movieId = req.params.movieId;
+
+  Movie.findByIdAndDelete(movieId)
+    .then(() => res.redirect("/movies"))
+    .catch((error) => {
+      console.log("error deleting movie", error);
+      next(error);
+    });
+});
+
 module.exports = router;
