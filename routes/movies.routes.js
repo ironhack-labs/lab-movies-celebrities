998930ae515321952b8ade1790231fdf65eec3df
Hongshan <zhou.hongshan636@gmail.com>
2021-08-04 08:46:48 +0200

    ite5-7 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,36 @@
 const router = require("express").Router();
 
-// all your routes here
+const Movie = require("../models/Movie.model.js");
+const Celebrity = require("../models/Celebrity.model.js");
+
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find()
+    .then((celebritiesFromDB) => {
+      res.render("movies/new-movie.hbs", { celebrities: celebritiesFromDB });
+    })
+    .catch((err) =>
+      console.log(`Err while displaying movie input page: ${err}`)
+    );
+});
+
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.create({ title, genre, plot, cast })
+    .then(() => res.redirect("/movies"))
+    .catch(() => res.render("movies/new-movie.hbs"));
+});
+
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((moviesFromDB) => {
+      res.render("movies/movies.hbs", {
+        movies: moviesFromDB,
+      });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB: ", error);
+      next(error);
+    });
+});
 
 module.exports = router;
