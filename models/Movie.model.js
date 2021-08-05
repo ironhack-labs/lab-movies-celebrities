df11aae3e437269957e01d5476105a65e71491c6
Peter Mayer <peter_my@gmx.de>
2021-08-05 10:14:08 +0200

    Delete Movie / Movies Details

---
+++ models/Movie.model.js
@@ -0,0 +1,15 @@
+const { Schema, model } = require('mongoose');
+ 
+const moviesSchema = new Schema(
+  {
+    title: String, 
+    genre: String, 
+    plot: String, 
+    cast: [String], 
+  },
+  {
+    timestamps: true
+  }
+);
+ 
+module.exports = model('Movie', moviesSchema);
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -5,17 +5,57 @@ const Movie = require('../models/Movie.model.js'); // <== add this line before y
 // GET route to retrieve and display all the books
 router.get('/movies', (req, res, next) => {
   Movie.find()
-    .then(allTheMoviesFromDB => {
-      // -> allTheBooksFromDB is a placeholder, it can be any word
-      console.log('Retrieved books from DB:', allTheMoviesFromDB);
- 
-      res.render('movies-list.hbs', { movies: allTheMoviesFromDB});
+    .then(allTheMoviesFromDB => { 
+      res.render('movies/movies.hbs', { movies: allTheMoviesFromDB});
     })
     .catch(error => {
-      console.log('Error while getting the books from the DB: ', error);
+      console.log('Error while getting the movies from the DB: ', error);
  
       // Call the error-middleware to display the error page to the user
       next(error);
     });
 });
+
+router.get('/movies/create', (req, res) => res.render('movies/new-movie.hbs'));
+
+
+router.post('/movies/create', (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.create({ title, genre, plot, cast })
+  .then(movieFromDB => {
+    // -> allTheBooksFromDB is a placeholder, it can be any word
+    console.log(`New Movie created: ${movieFromDB.title}.`);
+    res.redirect('/movies');
+  })
+  .catch(error => {
+    console.log('Error while getting the books from the DB: ', error);
+
+    // Call the error-middleware to display the error page to the user
+    next(error);
+  });
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  const { id } = req.params;
+  Movie.findById(id)
+  .then(movieFromDB => {
+    // -> allTheBooksFromDB is a placeholder, it can be any word
+    console.log(`Movie from DB: ${movieFromDB.title}.`);
+    res.render('movies/movie-details', {movie: movieFromDB});
+  })
+  .catch(error => {
+    console.log('Error while getting the books from the DB: ', error);
+
+    // Call the error-middleware to display the error page to the user
+    next(error);
+  });
+});
+
+router.post('/movies/:movieId/delete', (req, res, next) => {
+  const { movieId } = req.params;
+  Movie.findByIdAndDelete(movieId)
+    .then(() => res.redirect('/movies'))
+    .catch(error => next(error));
+});
+
 module.exports = router;
