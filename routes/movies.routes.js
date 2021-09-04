aed2bc6fd3fb4160bf9b4566e7e294d03ce3520a
dany <dacr63@gmail.com>
2021-09-05 00:08:14 +0200

    all done, basic styling

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -5,8 +5,10 @@ const Celebrity = require("../models/Celebrity.model");
 const Movie = require("../models/Movie.model")
 
 
-// all your routes here
+// Movie related routes
 
+
+// Render movie creation form
 router.get('/movies/create', (req, res) => {
     
     Celebrity
@@ -15,6 +17,7 @@ router.get('/movies/create', (req, res) => {
         .catch(err => console.log(err))
 })
 
+// Create movie & redirect to movie list
 router.post('/movies/create', (req, res) => {
 
     const {title, genre, plot, cast} = req.body
@@ -25,7 +28,7 @@ router.post('/movies/create', (req, res) => {
     .catch(err => console.log(err))
 })
 
-
+// Render movie list
 router.get('/movies', (req, res) => {
 
     Movie
@@ -33,6 +36,53 @@ router.get('/movies', (req, res) => {
     .populate('cast')
     .then(theMovies => res.render('./movies/movies', {movies: theMovies}))
     .catch(err => console.log(err))
+})
+
+
+// Render movie details
+router.get('/movies/id/:id', (req, res) => {
+
+    const {id} = req.params
+
+    Movie
+    .findById(id)
+    .populate('cast')
+    .then(theMovie => res.render('movies/movie-details', theMovie))
+    .catch(err => console.log(err))
+})
+
+
+// Delete movies
+router.post('/movies/id/:id/delete', (req, res) => {
+
+    const {id} = req.params
+
+    Movie
+    .findByIdAndDelete(id)
+    .then(res.redirect('/movies'))
+    .catch(err => console.log(err))
+})
+
+// Edit movies
+router.get('/movies/id/:id/edit', (req, res) => {
+
+    const { id } = req.params
+
+    Movie
+    .findById(id)
+    .populate('cast')
+    .then(theMovie => res.render('./movies/edit-movie', theMovie))
+    .catch(err => console.log(err))
+
+})
+
+router.post('/movies/id/:id', (req, res) => {
+
+    const {id, title, genre, plot, cast} = req.body
+
+    Movie
+    .findByIdAndUpdate(id, {title, genre, plot, cast})
+    .then(res.redirect('/movies'))
 })
 
 module.exports = router;
