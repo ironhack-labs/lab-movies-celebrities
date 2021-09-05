58fcf9e3e64e97afd4be2c066ac64237b19e94ae
Sara Mansori <sara.mansori.amar@gmail.com>
2021-09-05 16:39:03 +0200

    finished bonus update celebrities and delete celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -21,7 +21,7 @@ router.get("/celebrities/details/:id", (req, res) => {
             res.render("./celebrities/celebrity-details", celebrity);
         })
         .catch((err) => console.log(err));
-});
+})
 
 router.get("/celebrities/create", (req, res) => {
     res.render("./celebrities/new-celebrity")
@@ -39,6 +39,42 @@ router.post("/celebrities/create", (req, res) => {
     .catch(err => console.log(err))
 
     
+})
+
+router.get("/celebrities/:id/edit", (req, res) => {
+
+    const { id } = req.params;
+
+    Celebrity
+        .findById(id)
+        .then((celebrity) => {
+            res.render("./celebrities/edit-celebrity", celebrity)
+        })
+        .catch((err) => console.log(err))
+
+    const celebrityDetails = {};
+
+});
+
+router.post("/celebrities/:id/edit", (req, res) => {
+    const { id } = req.params;
+    const { name, occupation, catchPhrase } = req.body
+
+    Celebrity
+    .findByIdAndUpdate(id, {name, occupation, catchPhrase}, {new: true})
+    .then((celebrity) => {
+        res.redirect(`/celebrities/details/${celebrity._id}`)
+    });
+});
+
+router.post("/celebrities/:id/delete", (req, res) => {
+    const { id } = req.params;
+
+    Celebrity
+    .deleteOne({_id: id})
+    .then( () => {
+        res.redirect("/celebrities")
+    })
 })
 
 module.exports = router;
