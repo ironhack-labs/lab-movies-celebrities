861502fe226ec881160b9b0dc88eb1cf3e306f18
Sara Mansori <sara.mansori.amar@gmail.com>
2021-09-05 12:30:02 +0200

    iteration 8 movie details

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -11,6 +11,17 @@ router.get("/movies", (req, res) => {
         .catch((err) => console.log(err));
 });
 
+router.get("/movies/details/:id", (req, res) => {
+    const { id } = req.params;
+
+    Movie.findById(id)
+        .populate("cast")
+        .then((movie) => {
+            res.render("./movies/movie-details", movie);
+        })
+        .catch((err) => console.log(err));
+});
+
 router.get("/movies/create", (req, res) => {
     Celebrity.find()
         .select("name")
