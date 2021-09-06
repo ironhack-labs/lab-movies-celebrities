5c22112b56084a1427a97fda940d8094593db57b
Monika Geiger <geigermonika@hotmail.hu>
2021-09-06 20:46:37 +0200

    lab done
    Co-authored-by: Coops023 <Coops023@users.noreply.github.com>

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,18 @@
 //  Add your code here
+const {
+    Schema,
+    model
+} = require("mongoose");
+
+const celebritySchema = new Schema({
+    name: String,
+    occupation: {
+        type: String,
+        default: "unknown"
+    },
+    catchPhrase: String,
+}, );
+
+const Celebrity = model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const router = express.Router();
+const Celebrity = require('../models/Celebrity.model');
+
+
+router.get(
+    "/create",
+    (req, res) => {
+        res.render("celebrities/new-celebrity")
+    })
+
+router.get(
+    '/',
+    (req, res) => {
+        Celebrity.find()
+            .then(allCelebs => {
+                res.render('celebrities/celebrities', {
+                    allCelebs
+                })
+            })
+    });
+
+router.post(
+    "/create",
+    (req, res) => {
+        const {
+            name,
+            occupation,
+            catchPhrase
+        } = req.body
+        Celebrity.create({
+                name,
+                occupation,
+                catchPhrase
+            })
+            .then(newCelebrity => res.redirect("/celebrities"))
+            .catch(err => res.render("/celebrities/new-celebrity"))
+    })
+
+
+
+
+
+module.exports = router;
