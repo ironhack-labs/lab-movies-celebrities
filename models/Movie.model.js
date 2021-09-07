49d91798eef1fb49b77ebb00bac66405d5dba674
lauraAlabau <arlaura3@gmail.com>
2021-09-07 21:30:12 +0200

    Iteration #6: Adding New Movies

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -2,7 +2,7 @@ const { Schema, model } = require('mongoose');
 
 const movieSchema = new Schema(
   {
-    name: {
+    title: {
         type: String
     },
     genre: {
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,22 @@
 const router = require("express").Router();
 
-// all your routes here
+// require the Movies model here
+const Movie = require('../models/Movie.model');
+
+/* Iteration #3: Adding New Celebrities */
+router.get('/movies/create', (req, res, next) => {
+    res.render('movies/new-movie.hbs');
+  });
+  
+  router.post('/movies/create', (req, res, next) => {
+    const { title, genre, plot, cast } = req.body;
+    Movie
+      .create({ title, genre, plot, cast })
+      .then(() => res.redirect('/movies'))
+      .catch(error => next(error));
+  }); 
+
+
+
 
 module.exports = router;
