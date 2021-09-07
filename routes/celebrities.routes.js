3382f02b4dfed9bfca9ff2ae822513424e4f7460
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-07 21:07:52 +0200

    done everything, bonus included

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -36,4 +36,58 @@ router.get('/celebrities', (req, res) => {
 		})
 		.catch((err) => console.log('Error while showing all the celebrities GET ->', err));
 });
+// --------------------------------------------
+// DELETE celerities - one
+router.post('/celebrities/:id/delete', (req, res, next) => {
+	// Iteration #5: Delete the drone
+	const { id } = req.params;
+
+	Celebrity.findByIdAndDelete(id)
+		.then(() => res.redirect('/celebrities'))
+		.catch((error) => console.log('Error while trying to delete the celebrity POST router ->', error));
+});
+// ------------------------------------------------------
+// UPDATE celebrities - GET route to display the form to update a specific celebrity
+// ------------------------------------------------------
+router.get('/celebrities/:id/edit', (req, res, next) => {
+	// Iteration #4: Update the drone
+	const { id } = req.params;
+
+	//const movies = Movie.findById(id);
+	const celebrities = Celebrity.findById(id);
+
+	Promise.all([ celebrities ])
+		.then(([ celebrities ]) => res.render('celebrities/edit-celebrity.hbs', { celebrities }))
+		.catch((err) => console.log('Error while trying to show the celebrity info GET router ->', err));
+	//res.render('movies/edit-movie.hbs', [ movies, celebrities ])
+	//res.send([ movies, celebrities ])
+});
+// ------------------------------------------------------
+// UPDATE movies - POST route to actually make updates on a specific movie
+// ------------------------------------------------------
+router.post('/celebrities/:id/edit', (req, res) => {
+	// Iteration #4: Update the drone
+	const { id } = req.params;
+	const { name, occupation, catchPhrase } = req.body;
+
+	Celebrity.findByIdAndUpdate(id, { name, occupation, catchPhrase }, { new: true })
+		// sin id a lo mejor
+		.then((celebrity) => res.redirect(`/celebrities/details/${celebrity._id}`))
+		.catch((error, celebrity) => {
+			console.log('Error while updating a celebrity ->', error);
+			res.render('celebrities/edit-celebrity.hbs', celebrity);
+		});
+});
+// ------------------------------------------------------
+// SHOW celebrities DETAIL
+// ------------------------------------------------------
+router.get('/celebrities/details/:id', (req, res) => {
+	const { id } = req.params;
+
+	Celebrity.findById(id)
+		.then((celebrity) => {
+			res.render('./celebrities/celebrity-details', celebrity);
+		})
+		.catch((err) => console.log('Error while trying to show the movie info GET router ->', err));
+});
 module.exports = router;
