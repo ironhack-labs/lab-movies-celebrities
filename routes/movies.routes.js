fcfe15f3a0930e7e5b75ae178d10ef410043e103
Ana Martins <ana.af.martins@gmail.com>
2021-09-07 17:53:25 +0200

    lab done
    Co-authored-by: FlavianAlbert <Hribu@users.noreply.github.com>

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,6 +4,47 @@ const Movie = require("../models/Movie.model");
 const Celebrity = require("../models/Celebrity.model");
 // all your routes here
 
+router.post("/:id/delete", (req, res) => {
+  const id = req.params.id;
+  Movie.findByIdAndRemove(id)
+    .then((deletedMovie) => {
+      res.redirect("/movies");
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/:id/edit", (req, res) => {
+  const id = req.params.id;
+  // Celebrity.find();
+  Movie.findById(id)
+    .populate("cast")
+    .then((movie) => {
+      console.log("cast");
+      res.render("movies/edit-movie", { movie: movie });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.post("/:id", (req, res) => {
+  const id = req.params.id;
+  const { title, genre, plot, cast } = req.body;
+  Movie.findByIdAndUpdate(id, { title, genre, plot, cast })
+    .then((movie) => {
+      res.redirect("/movies");
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/:id", (req, res) => {
+  const id = req.params.id;
+  Movie.findById(id)
+    .populate("cast")
+    .then((movie) => {
+      res.render("movies/movie-details", { movie: movie });
+    })
+    .catch((err) => console.log(err));
+});
+
 router.get("/new-movie", (req, res) => {
   Celebrity.find()
     .then((celebs) => {
