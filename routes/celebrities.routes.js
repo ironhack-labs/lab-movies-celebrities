343dcfa50165638098d6140bdce87cb776cee32e
lauraAlabau <arlaura3@gmail.com>
2021-09-07 18:11:21 +0200

    Iteration #3: Adding New Celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
 
+// require the Celebrity model here
+const Celebrity = require('../models/Celebrity.model');
 
-// all your routes here
+/* Iteration #3: Adding New Celebrities */
+router.get('/celebrities/create', (req, res, next) => {
+  res.render('celebrities/new-celebrity.hbs');
+});
+
+router.post('/celebrities/create', (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity
+    .create({ name, occupation, catchPhrase  })
+    .then(() => res.redirect('/celebrities'))
+    .catch(error => next(error));
+}); 
 
 module.exports = router;
