4391e8efcb7519bcb71a66d057e2f6c897e089a7
Espe Cabrer <mecabrerv@gmail.com>
2021-09-08 00:06:44 +0200

    Iteration 8-10.
    Pte bonus + Style

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -35,9 +35,56 @@ router.get("/movies", (req, res) => {
             console.log('Error while getting movies from the DB: ', error);
             next(error);
           });
-
 });
 
+router.get("/movies/:movieId", (req, res) => {
+
+    const {movieId} = req.params;
+
+    Movie
+        .findById(movieId)
+        .populate("cast")
+        .then(movie => res.render("movies/movie-details", movie))
+        .catch(error => {
+            console.log('Error whith DB: ', error);
+        })
+})
+
+router.post("/movies/:movieId/delete", (req, res) => {
+
+    const {movieId} = req.params;
+
+    Movie
+        .findByIdAndRemove(movieId)
+        .then(() => res.redirect("/movies"))
+        .catch(error => console.log(error))
+})
+
+router.get("/movies/:movieId/edit", (req, res) => {
+
+    const {movieId} = req.params;
+
+    const movieToUpdate = Movie
+                            .findById(movieId);
+                  
+
+    const celebrities =  Celebrity
+                            .find();
+                         
+    Promise.all([movieToUpdate, celebrities])
+                .then(([movieToUpdate, celebrities]) =>  res.render("movies/edit-movie", {movieToUpdate, celebrities}))
+});
+
+router.post("/movies/:movieId", (req, res) => {
+
+    const {movieId} = req.params;
+    const { title, genre, plot, cast} = req.body
+
+    Movie.findByIdAndUpdate(movieId, {title, genre, plot, cast}, {new: true})
+        .then(() => res.redirect(`/movies/${movieId}`))
+        .catch(err => console.log("update error: ", err))
+});
+ 
 
 
 module.exports = router;
