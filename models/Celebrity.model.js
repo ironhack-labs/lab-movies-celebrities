3e5c91598c908872d99bf199b6000ca779f2f07f
Jaume Ballester <86907672+jaumebt90@users.noreply.github.com>
2021-09-08 20:34:58 +0200

    first

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,17 @@
 //  Add your code here
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Celebrity = model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
