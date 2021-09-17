8e38c99517a399c663c7d68c7861872991a10061
CherylHuang1988 <cherylhuang1988@gmail.com>
2021-09-17 22:52:40 +0200

    Slove the problem with Cast showing

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -4,7 +4,12 @@ const movieSchema = new mongoose.Schema({
   title: String,
   genre: String,
   plot: String,
-  cast: Array,
+  cast: [
+    {
+      type: mongoose.Types.ObjectId,
+      ref: "Celebrity",
+    },
+  ],
 });
 
 const Movie = mongoose.model("Movie", movieSchema);
