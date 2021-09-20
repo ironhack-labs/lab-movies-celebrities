74066032410d1b58afa645d088781958bdb51ee9
Noemi Radvan <radvanno@gmail.com>
2021-09-20 14:07:14 +0200

    Done till 7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,36 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require("../models/Movie.model.js");
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+router.get("/create", (req, res) => {
+  Celebrity.find()
+    .then((allCelebrities) => {
+      res.render("movies/new-movie", { celebs: allCelebrities });
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+});
+
+router.post("/create", (req, res) => {
+  const { title, genre, plot, cast } = req.body;
+  console.log(req.body);
+  Movie.create({ title, genre, plot, cast })
+    .then((createdMovie) => res.redirect("/movies"))
+    .catch((error) => res.render("movies/new-movie"));
+});
+
+router.get("/", (req, res) => {
+  Movie.find()
+    .then((allMovies) => {
+      console.log(allMovies);
+      res.render("movies/movies", { movies: allMovies });
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
+});
 
 module.exports = router;
