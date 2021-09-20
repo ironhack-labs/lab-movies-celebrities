74dc79ddb623dcab9fae01823971426cdfbf4dd3
Noemi Radvan <radvanno@gmail.com>
2021-09-20 10:58:58 +0200

    4th done, oh man, it was hard

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,10 +1,9 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
-const express = require("express");
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model.js");
 
 // all your routes here
-router.get("/create", (req, res, next) => {
+router.get("/create", (req, res) => {
   res.render("celebrities/new-celebrity");
 });
 
@@ -14,6 +13,17 @@ router.post("/create", (req, res) => {
   Celebrity.create({ name, occupation, catchPhrase })
     .then((createdCelebrity) => res.redirect("/celebrities"))
     .catch((error) => res.render("celebrities/new-celebrity"));
+});
+
+router.get("/", (req, res) => {
+  Celebrity.find()
+    .then((allCelebrities) => {
+      console.log(allCelebrities);
+      res.render("celebrities/celebrities", { celebs: allCelebrities });
+    })
+    .catch((error) => {
+      console.log("error", error);
+    });
 });
 
 module.exports = router;
