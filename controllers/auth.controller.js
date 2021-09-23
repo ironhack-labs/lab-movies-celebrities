1ff147d4af394f209e36eb147830f49ac7a939b5
romlopez1 <romibernard68@gmail.com>
2021-09-23 17:36:27 -0500

    se cambia bcrypt

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,10 +1,16 @@
 const User = require("../models/User.model");
+const bcryptjs = require("bcryptjs")
+const saltRounds = 10
 
 exports.signUp = (req, res) => {
     res.render("auth/signup")
 }
-exports.signUpSubmit = (req, res) => {
+exports.signUpSubmit = async (req, res) => {
     const { username, email, password } = req.body
+
+    const salt = await bcryptjs.genSalt(saltRounds)
+
+
 }
 
 exports.login = (req, res) => {
