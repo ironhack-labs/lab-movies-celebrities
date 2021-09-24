b63bca7434c491144d6455be40b1a9a5a7520fc7
ErickRoo <erick.rod.ag@outlook.com>
2021-09-24 11:24:32 -0500

    done -3

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const authController = require("./../controllers/authController");
+const authController = require("./../Controllers/authController");
 
 const routeGuards = require("./../middlewares/route-guard");
 
