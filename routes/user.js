2ee0558b47e1b42da4c7987815ffd420a7ce6331
ErickRoo <erick.rod.ag@outlook.com>
2021-09-24 11:24:32 -0500

    done -3

--- routes/user.js
+++ routes/user.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 
-const userController = require("./../controllers/userController");
+const userController = require("./../Controllers/userController");
 
 const routeGuards = require("./../middlewares/route-guard");
 
