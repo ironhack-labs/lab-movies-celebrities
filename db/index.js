5510289890385066c77d4df28d33e2fb3baea681
romlopez1 <romibernard68@gmail.com>
2021-09-24 11:43:10 -0500

    se cambia mongodburl

--- db/index.js
+++ db/index.js
@@ -5,10 +5,10 @@ const mongoose = require("mongoose");
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
-const MONGO_URI = process.env.MONGODB_URI || "mongodb://localhost/lab-movies-celebrities";
+const MONGO_URL = process.env.MONGODB_URL || "mongodb://localhost/lab-movies-celebrities";
 
 mongoose
-  .connect(MONGO_URI, {
+  .connect(MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useFindAndModify: false,
