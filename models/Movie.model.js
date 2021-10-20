847a1e017324ca3bcf08bc4308a1293f62e727e6
Alice <alice.jost@outlook.de>
2021-10-20 15:13:04 +0200

    Iteration 5: create Movie model

---
+++ models/Movie.model.js
@@ -0,0 +1,12 @@
+const { Schema, model } = require('mongoose');
+
+const movieModel = new Schema(
+    {
+        title: String,
+        genre: String,
+        plot: String,
+        cast: { type: Schema.Types.ObjectId, ref: 'Celebrity' }
+    }
+)
+
+module.exports = model('Movie', movieModel);
