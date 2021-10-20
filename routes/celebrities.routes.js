e664edec08f95e721c1ef8fb71e4370c4d0ca253
Dominika Lipka <dominika.lipka96@gmail.com>
2021-10-20 08:24:37 +0200

    Iteration 4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -10,10 +10,24 @@ router.post('/celebrities/create', (req, res, next) => {
     const {name, occupation, catchPhrase} = req.body;
     Celebrity.create({name, occupation, catchPhrase})
         .then(() => {
-            res.render('celebrities/celebrities')
+            res.redirect('/celebrities')
         })
         .catch(() => {
             res.render('celebrities/new-celebrity')
+        })
+})
+
+router.get('/celebrities', (req, res, next) => {
+    Celebrity.find()
+        .then((celebritiesFromDB) => {
+            const data = {
+                celebritiesList: celebritiesFromDB
+            }
+            res.render('celebrities/celebrities', data)
+        })
+        .catch((error) => {
+            console.log('An error occured, could not load celebrities list', error);
+            next(error);
         })
 })
 
