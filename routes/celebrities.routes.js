b0068ab3e2de49314b5e3fc79a9473bff09d3e23
Alice <alice.jost@outlook.de>
2021-10-20 14:56:54 +0200

    Iteration 3: functionality to create

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,20 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model')
 
-// all your routes here
+router.get('/celebrities/create', (req, res, next) => {
+    res.render("celebrities/new-celebrity.hbs")
+});
+
+router.post('/celebrities/create', (req, res, next) => {
+    const {name, occupation, catchPhrase} = req.body
+    Celebrity.create({ name, occupation, catchPhrase })
+        .then( () => {
+                res.redirect('/celebrities')
+        })
+        .catch( () => {
+            res.redirect('celebrities/new-celebrity')
+        })
+})
+
 
 module.exports = router;
