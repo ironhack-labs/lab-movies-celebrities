3df8c5ce1c06eb9a9089b554d3a5a68c19ccdf1f
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-10-20 20:16:43 +0200

    Iteration 7 8

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -7,44 +7,34 @@ const router = require("express").Router();
 
 router.get("/celebrities/create", (req, res, next) => {
     res.render("celebrities/new-celebrity")
-
 })
 
 
 router.post("/celebrities/create", (req, res, next)=>{
     const {name, occupation, catchPhrase} = req.body;
     Celebrity.create({name, occupation, catchPhrase})
-    .then (() => {
-        res.redirect("/celebrities")
-
-    })
-
-    .catch((err) => {
-        console.log("ups, an error has been detected getting the information from the create form of celebrities", err);
-        res.redirect("/celebrities")
-    })
+        .then (() => {
+            res.redirect("/celebrities")
+        })
+        .catch((err) => {
+            console.log("Error getting the information from the form created for celebrities", err);
+            res.redirect("/celebrities")
+        })
 
 })
-
-
 
 router.get("/celebrities",(req, res, next) => {
     Celebrity.find()
     .then((allCelebritiesFromDB) => {
-        
         const data = {
             celebritiesArr : allCelebritiesFromDB
-
-            
         }
         res.render("celebrities/celebrities", data)
-
     })
-    .catch()
-
-    
-
-
+    .catch((err) => {
+        console.log("Error displaying the information from the form created for celebrities", err);
+        res.redirect("/celebrities")
+    })
 })
 
 // all your routes here
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,4 +1,5 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
 const Movie = require("../models/Movie.model")
@@ -9,25 +10,53 @@ router.get("/movies/create", (req, res, next) => {
             res.render("movies/new-movie", {celebritiesAr: allCelebritiesFromDB})
         })
 
-        .catch((error) => {
-            console.log("Error getting Celebrities from DB", error);
-            next(error)
+        .catch((err) => {
+            console.log("Error getting the information from the form cast label created for celebrities", err);
+            next(err)
         })
-    
 })
 
 router.post("/movies/create", (req, res, next) => {
     const {title, genre, plot, cast} = req.body;
     Movie.create({title, genre, plot, cast})
-    .then(() =>{
-        res.redirect("/movies")
-    })
-
-    
-    .catch((err) =>{
-        console.log("ups, an error has been detected getting the information from the create form of movies", err)
-    })
-
+        .then(() => {
+            res.redirect("/movies")
+        })
+        .catch((err) => {
+            console.log("Error displaying the information from the form created for movies", err);
+            next(err)
+        })
 
 })
+
+
+router.get("/movies", (req, res, next) => { 
+    Movie.find()
+        .then((moviesFromDB) =>{
+            const data = {
+                moviesArr : moviesFromDB
+            }
+            res.render("movies/movies", data);
+        })
+        .catch((err) => {
+            console.log("Error getting information from all the movies in the DB", err);
+            next(err)
+        })
+})
+
+router.get("/movies/:moviesId", (req, res, next) => {
+    Movie.findById(req.params.moviesId)
+        .populate("cast")
+        .then((moviesFromDB) => {
+            res.render("movies/movie-details", moviesFromDB)
+        })
+        .catch((err) => {
+            console.log("Error getting information from all the movies in the DB", err);
+            next(err)
+        })
+})
+
+
+
+
 module.exports = router;
