76f3051635f7244061e6723e4dda37e98c5b23cd
Nekruz Avgani <nekruz.avgani@gmail.com>
2021-10-20 13:38:24 +0100

    Iteration #2: The Celebrity model

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,15 @@
 //  Add your code here
+const { Schema, model } = require('mongoose');
+
+const celebSchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model('Book', celebSchema);
