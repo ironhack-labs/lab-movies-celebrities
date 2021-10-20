f8fe7d40dc76c43c4fba755bf65c720e69a52a40
Aintzane Morales <aintzanemorales1992@gmail.com>
2021-10-20 15:45:57 +0200

    Iteration 4 READ funcionality

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,4 +1,4 @@
-const CelebrityModel = require("../models/Celebrity.model");
+const Celebrity = require("../models/Celebrity.model");
 
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
@@ -10,20 +10,36 @@ router.get("/celebrities/create", (req, res, next) => {
 })
 
 
-router.post("celebrities/create", (req, res, next)=>{
+router.post("/celebrities/create", (req, res, next)=>{
     const {name, occupation, catchPhrase} = req.body;
-    
-    Celebrity.create(req.body)
+    Celebrity.create({name, occupation, catchPhrase})
     .then (() => {
-        console.log(req.body)
         res.redirect("/celebrities")
 
     })
 
     .catch((err) => {
         console.log("ups, an error has been detected getting the information from the create form", err);
+        res.redirect("/celebrities")
+    })
+
+})
+
+
+router.get("/celebrities",(req, res, next) => {
+    Celebrity.find()
+    .then((allCelebritiesFromDB) => {
+        // console.log("Celebrities", allCelebritiesFromDB)
+        const data = {
+            celebritiesArr : allCelebritiesFromDB
+        }
+        res.render("celebrities/celebrities.hbs", data)
 
     })
+    .catch()
+
+    
+
 
 })
 
