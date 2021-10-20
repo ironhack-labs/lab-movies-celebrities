08bcca5f8c471c19dbdcffc1e22eecfb35278813
Alice <alice.jost@outlook.de>
2021-10-20 14:10:19 +0200

    Iteration 2: creating celebrity model

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,11 @@
-//  Add your code here
+const { Schema, model } = require('mongoose');
+
+const celebrityModel = new Schema (
+    {
+        name: String,
+        occupation: String,
+        catchPhrase: String,
+    }
+)
+
+module.exports = model('Celebrity', celebrityModel);
