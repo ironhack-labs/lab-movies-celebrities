bbc2a573a03b177dae49be001cc0f152b5a408e1
Geoffrey Laville <laville.geoffrey@gmail.com>
2021-10-20 22:40:41 +0200

    iteration 5

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -1,17 +1,16 @@
-// //  Add your code here
-
-// const { Schema, model } = require('mongoose');
+const { Schema, model } = require('mongoose');
 
 
-// const celebritySchema = new Schema(
-//   {
-//     name: String,
-//     occupation: String,
-//     catchPhrase: String,
-//   },
-//   {
-//     timestamps: true
-//   }
-// );
+const movieSchema = new Schema(
+  {
+    title: String,
+    genre: String,
+    plot: String,
+    cast: {type: Schema.Types.ObjectId, ref: 'cast'}
+  },
+  {
+    timestamps: true
+  }
+);
 
-// module.exports = model('Celebrity', celebritySchema);
+module.exports = model('Movie', movieSchema);
