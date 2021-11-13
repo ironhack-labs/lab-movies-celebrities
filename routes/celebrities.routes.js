5fef6cd65fbba4d8c8384e474d46e69e3939312d
Miguel Angel Abad <miguel.abad86@gmail.com>
2021-11-13 19:05:14 +0100

    Complete bonus iterations on celebrities detailed view, edit and delete

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -4,7 +4,7 @@ const Celebrity = require('./../models/Celebrity.model')
 router.get('/', (req, res, next) => {
   Celebrity.find()
     .then((celebrities) => {
-      res.render('celebrities/celebrities', {celebrities})
+      res.render('celebrities/celebrities', { celebrities })
     })
     .catch((err) => console.error(err))
 })
@@ -23,6 +23,30 @@ router.post('/create', (req, res, next) => {
         errorMessage: 'Error creating a new celebrtity',
       })
     )
+})
+
+router.get('/:celebId', (req, res, next) => {
+  Celebrity.findById(req.params.celebId)
+    .then((celeb) => res.render('celebrities/celebrity-details', celeb))
+    .catch((err) => console.error(err))
+})
+
+router.post('/:celebId/delete', (req, res, next) => {
+  Celebrity.findByIdAndRemove(req.params.celebId)
+    .then(() => res.redirect('/celebrities'))
+    .catch((err) => console.error(err))
+})
+
+router.get('/:celebId/edit', (req, res, next) => {
+  Celebrity.findById(req.params.celebId)
+    .then((celeb) => res.render('celebrities/edit-celebrity', celeb))
+    .catch((err) => console.error(err))
+})
+
+router.post('/:celebId/edit', (req, res, next) => {
+  Celebrity.findByIdAndUpdate(req.params.celebId, req.body)
+    .then(() => res.redirect(`/celebrities/${req.params.celebId}`))
+    .catch((err) => console.error(err))
 })
 
 module.exports = router
