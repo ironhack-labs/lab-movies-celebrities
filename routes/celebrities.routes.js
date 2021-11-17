23927e3cf8b1cc375d2415835a3488c54db84cdd
JoseLValdes <jolvaldgo@gmail.com>
2021-11-17 15:40:02 +0100

    Iteration #6

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,3 @@
-// const express = require("express");
-// const router = express.Router();
-
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,24 @@
-
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
-// all your routes here
+router.get("/movies/create", async (req, res, next) => {
+  try {
+    const celebrities = await Celebrity.find({});
+    res.render("movies/new-movie", { celebrities });
+  } catch (err) {
+    console.log("err", err);
+  }
+});
+
+router.post("/movies/create", async (req, res, next) => {
+  try {
+    await Movie.create(req.body);
+    res.redirect("/movies");
+  } catch (err) {
+    res.render("/movies/create");
+    console.log("err", err);
+  }
+});
 
 module.exports = router;
