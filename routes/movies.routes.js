307a45c2af3c07911986a102649932a47e43517c
Fede <f.gomezlara@outlook.es>
2021-11-17 16:40:10 +0100

    iteration 7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,14 +1,17 @@
 //Require the router
 const router = require('express').Router()
+const mongoose = require('mongoose')
 
 
 //MODELS
 const Movie = require('../models/Movie.model')
 const Celebrity = require('../models/Celebrity.model')
 
+
 //ROUTES
-router.get('/', (req, res) => {
-    res.render('./movies/movies.hbs')
+router.get('/', async (req, res) => {
+    const movies = await Movie.find({})
+    res.render('./movies/movies.hbs', { movies })
 })
 
 router.get('/create', async (req, res) => {
@@ -25,17 +28,17 @@ router.post('/create', async (req, res) => {
     try {
         const { title, genre, plot, cast } = req.body
         const newMovie = await Movie.create({ title, genre, plot, cast })
-        req.body.cast.forEach((actor) => { //Push each actor ID inside the cast array
-            Movie.findByIdAndUpdate(newMovie._id, { $push: { cast: actor._id } })
-        })
-        res.render('/movies/movies.hbs')
+        // console.log(cast) //Cast is an array of actor IDs saved as String types
+        // cast.forEach(async(actor) => { //Push each one inside the cast array
+        //     console.log(actor)
+        //     await Movie.findByIdAndUpdate(newMovie._id, { $push: { cast: mongoose.Types.ObjectId(actor) } }) //Convert to ObjectId type
+        // })
+        res.render('./movies/movies.hbs')
     }
     catch (err) {
         console.log('Error creating a new movie:', err)
         res.render('./movies/newMovie.hbs')
     }
 })
-
-
 
 module.exports = router
