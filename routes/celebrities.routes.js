af4509e3d9b867bd9035883c9c0a60363d1cd165
Guillem Ferrer <guillemfd@gmail.com>
2021-11-17 18:16:06 +0100

    Iteraction 4 in progress

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -9,6 +9,14 @@ router.get("/", (req, res) => {
     res.render("../views/celebrities/celebrities.hbs");
   });
 
+//intento de iteraction 4 -------------------------------------------------------------------
+//   router.get("/", async (req, res) => {
+//     const celebrity = await Celebrity.findById(req.params.id).populate('name')
+//     const {name} = celebrity
+//     res.render("../views/celebrities/celebrities.hbs", {name});
+//   });
+
+
 
 /* GET create new celebrity page */ //renderizan todas a partir de /celebrities
 router.get("/create", (req, res) => {
@@ -22,7 +30,7 @@ try{
     const createdCelebrity = await Celebrity.create({name, occupation, catchPhrase})
     res.render("./celebrities/celebrities.hbs", {justCreatedCelebrity: createdCelebrity.name})
     }catch(err){
-    res.render("./celebrities/new-celebrity", {errorCreation: err})
+    res.render("./celebrities/new-celebrity", { errorCreation: err})
     }
 
 })
