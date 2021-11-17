7daa263532522b2982c67391eda2fc8aabdbc499
Guillem Ferrer <guillemfd@gmail.com>
2021-11-17 14:51:32 +0100

    Iteration 2 (the last one was #1 in fact...

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,2 +1,11 @@
-//  Add your code here
+const { Schema, model } = require("mongoose");
 
+const celebritySchema = new Schema({
+    name: {type: String, required: true},
+    occupation: {type: String, required: true},
+    catchPhrase: {type: String},
+  }, {timestamps: true}
+);
+
+const Celebrity = model("Celebrity", celebritySchema);
+module.exports = Celebrity;
