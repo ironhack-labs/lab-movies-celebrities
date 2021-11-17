fa1a7edeee06d47650a95f82f9c2dd90db586376
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-17 15:39:40 +0000

    interaction 6  done

---
+++ models/Movie.model.js
@@ -0,0 +1,15 @@
+//  Add your code here
+const { Schema, model } = require("mongoose");
+
+
+const movieSchema = new Schema({
+    title: {type: String, required: true},
+    genre: {type: String},
+    plot: {type: String},
+    cast: [{type: Schema.Types.ObjectId, ref: 'Celebrity'}]
+  }, {timestamps: true}
+);
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
