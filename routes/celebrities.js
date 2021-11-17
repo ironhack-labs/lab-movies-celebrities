e44497dde088df3c2bbcfea3d49d0f0d8b33abc2
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-17 18:58:19 +0000

    code formated

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,38 +1,42 @@
 const router = require("express").Router();
-const Celebrity = require('../models/Celebrity.model')
+const Celebrity = require("../models/Celebrity.model");
 
 /* GET celebrities page */
 
 router.get("/celebrities", async (req, res) => {
-    try{
-        const allCelebrities = await Celebrity.find({})
-        res.render("./celebrities/celebrities", {allCelebrities});
-        console.log(allCelebrities)
-    }catch (err){
-        console.log(err)
-    }
+  try {
+    const allCelebrities = await Celebrity.find({});
+    res.render("./celebrities/celebrities", { allCelebrities });
+    console.log(allCelebrities);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 router.get("/celebrities/create", (req, res) => {
-    res.render("./celebrities/newCelebrity");
+  res.render("./celebrities/newCelebrity");
 });
 
 router.post("/celebrities/create", async (req, res) => {
-    const {name, occupation, catchPhrase} = req.body
-    //console.log(req.body)
-    try {
-        const createdCelebrity = await Celebrity.create({name, occupation, catchPhrase})
-        const allCelebrities = await Celebrity.find({})
-        if(createdCelebrity) {
-            res.render("./celebrities/celebrities", {allCelebrities});
-        }else {
-            res.render("./celebrities/newCelebrity", {errMsg: "There is an error creating a new celebrity. Please try again"})
-        }
-    }catch (err) {
-        console.log(err)
+  const { name, occupation, catchPhrase } = req.body;
+  //console.log(req.body)
+  try {
+    const createdCelebrity = await Celebrity.create({
+      name,
+      occupation,
+      catchPhrase,
+    });
+    const allCelebrities = await Celebrity.find({});
+    if (createdCelebrity) {
+      res.render("./celebrities/celebrities", { allCelebrities });
+    } else {
+      res.render("./celebrities/newCelebrity", {
+        errMsg: "There is an error creating a new celebrity. Please try again",
+      });
     }
-    
+  } catch (err) {
+    console.log(err);
+  }
 });
-
 
 module.exports = router;
--- routes/movies.js
+++ routes/movies.js
@@ -1,86 +1,92 @@
 const router = require("express").Router();
-const Celebrity = require('../models/Celebrity.model')
-const Movie = require('../models/Movie.model')
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 
 // all your routes here
 
-//GET routes
+//GET movies route
 router.get("/movies", async (req, res) => {
-    try{
-        const allMovies = await Movie.find({})
-        res.render("movies/movies", {allMovies});
-        console.log(allMovies)
-    }catch (err){
-        console.log(err)
-    }
+  try {
+    const allMovies = await Movie.find({});
+    res.render("movies/movies", { allMovies });
+    console.log(allMovies);
+  } catch (err) {
+    console.log(err);
+  }
 });
 
-router.get("/movies/create", async (req, res) => {
-    try {
-        const allCelebrities = await Celebrity.find({})
-        res.render("./movies/newMovie", {allCelebrities})
-    }catch (err){
-        console.log(err)
-    }
-})
+//GET create-movie route
 
-//POST create route (when the form is filled)
+router.get("/movies/create", async (req, res) => {
+  try {
+    const allCelebrities = await Celebrity.find({});
+    res.render("./movies/newMovie", { allCelebrities });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//POST (when the form of create movie is filled)
 router.post("/movies/create", async (req, res) => {
-    const {title, genre, plot, cast} = req.body
-    
-    try {
-        const createdMovie = await Movie.create({title, genre, plot, cast})
-        if(createdMovie) {
-            res.redirect("/movies")
-        }else {
-            res.render("./movies/newMovie", {errMsg: "There is an error creating a new Movie. Please try again"})
-        }
-    }catch (err) {
-        console.log(err)
+  const { title, genre, plot, cast } = req.body;
+
+  try {
+    const createdMovie = await Movie.create({ title, genre, plot, cast });
+    if (createdMovie) {
+      res.redirect("/movies");
+    } else {
+      res.render("./movies/newMovie", {
+        errMsg: "There is an error creating a new Movie. Please try again",
+      });
     }
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 //GET movies by its ID
 router.get("/movies/:id", async (req, res) => {
-    try{
-    const movie = await Movie.findById(req.params.id).populate('cast')
-    res.render("./movies/movieDetails", movie)
-    }catch (err) {
-        console.log(err)
-    }    
-})
+  try {
+    const movie = await Movie.findById(req.params.id).populate("cast");
+    res.render("./movies/movieDetails", movie);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //POST movies for deleting by its ID
-router.post("/movies/:id/delete", async (req, res)=>{
-    try{
-        const deletedMovie = await Movie.findByIdAndDelete(req.params.id)
-        res.redirect("/movies")
-    }catch(err){
-      console.log(err)
-    } 
-  })
+router.post("/movies/:id/delete", async (req, res) => {
+  try {
+    const deletedMovie = await Movie.findByIdAndDelete(req.params.id);
+    res.redirect("/movies");
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //GET movies for editing
 router.get("/movies/:id/edit", async (req, res) => {
-    try{
-    const updatedMovie = await Movie.findById(req.params.id).populate("cast")
-    const castCelebrity = await Celebrity.find({})
-    res.render("./movies/editMovie", {movie: updatedMovie, castCelebrity: castCelebrity})
-    }catch (err) {
-        console.log(err)
-    }    
-})
+  try {
+    const updatedMovie = await Movie.findById(req.params.id).populate("cast");
+    const castCelebrity = await Celebrity.find({});
+    res.render("./movies/editMovie", {
+      movie: updatedMovie,
+      castCelebrity: castCelebrity,
+    });
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 //POST movies for editing
-router.post("/movies/:id", async (req, res)=>{
-    try{
-        const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body)
-        console.log(updatedMovie)
-        res.redirect(`/movies/${updatedMovie.id}`)
-    }catch(err){
-      console.log(err)
-    } 
-  })
-
+router.post("/movies/:id", async (req, res) => {
+  try {
+    const updatedMovie = await Movie.findByIdAndUpdate(req.params.id, req.body);
+    console.log(updatedMovie);
+    res.redirect(`/movies/${updatedMovie.id}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
 
 module.exports = router;
