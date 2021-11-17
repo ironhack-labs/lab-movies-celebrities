027e4bb455e54c3746b63a8fd91839c72e0d4851
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-17 14:29:42 +0100

    Iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,30 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
-// all your routes here
+const Celebrity = require("../models/Celebrity.model")
+
+/* GET create celebrity page */
+router.get("/create", (req, res, next) => {
+    res.render("celebrities/new-celebrity.hbs");
+});
+
+/* GET create celebtities page */
+router.get("/celebrities", (req, res, next) => {
+    res.render("celebrities/celebrities.hbs");
+});
+
+/* POST create celebrity page */
+router.post("/create", async (req, res, next) => {
+    const {name, occupation, catchPhrase} = req.body
+    try{
+        const newCelebrity = await Celebrity.create({name, occupation, catchPhrase})
+        res.redirect("/celebrities")
+    } catch(err){
+        console.log(err)
+        res.render("celebrities/new-celebrity.hbs");
+    }
+});
+
+
 
 module.exports = router;
