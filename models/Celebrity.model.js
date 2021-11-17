d38f3932bd90704785f24c4919a6a092d7cdb5a4
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-17 14:23:59 +0000

    interaction 2 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,14 @@
 //  Add your code here
+const { Schema, model } = require("mongoose");
+
+
+const celebritySchema = new Schema({
+    name: {type: String, required: true},
+    occupation: {type: String, enum: ["actor", "singer", "comedian"], default: "unknown"},
+    catchPhrase: {type: String}
+  }, {timestamps: true}
+);
+
+const Celebrity = model("Book", bookSchema);
+
+module.exports = Celebrity;
