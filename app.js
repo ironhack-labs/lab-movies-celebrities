15704b34bd203528bbd219d03983bb2e24c4eacb
Santi Jimenez <jimenez.santi@gmail.com>
2021-11-17 12:55:59 +0100

    Iteration 1

--- app.js
+++ app.js
@@ -27,6 +27,8 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+app.use('/celebrities', require("./routes/celebrities"))
+app.use('/movies', require("./routes/movies"))
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
