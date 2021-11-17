e22941130c20d145b8caae54901fff6a41f1004b
Aarón López <n_aaron5@hotmail.com>
2021-11-17 14:32:10 +0100

    iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,3 +1,22 @@
-const router = require("express").Router();
+const router    = require("express").Router();
+const chalk     = require('chalk')
+const Celebrity = require('../models/Celebrity.model')
 
+router.get('/create', (req, res)=>{
+    res.render('celebrities/new-celebrity')
+})
+
+router.post('/create', async (req, res)=>{
+    console.log(req.body)
+    const {name, occupation, catchPhrase} = req.body
+    try{
+        const createdCelebrity = await Celebrity.create({name, occupation, catchPhrase})
+        console.log(createdCelebrity)
+        res.redirect('/celebrities')
+    } catch{
+        console.log(chalk.bgRed(err))
+        res.render('celebrities/new-celebrity')
+    }
+    
+})
 module.exports = router;
