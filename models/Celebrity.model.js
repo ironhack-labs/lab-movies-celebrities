9eafbed1101c9e496d9d4cbf121e7ff4d9b4851b
nachbr91 <nabero91@gmail.com>
2021-11-17 15:55:55 +0100

    Iteration 2 and 3 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,10 @@
-//  Add your code here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const celebritySchema = new Schema ({
+  name: {type: String},
+  occupation: {type: String},
+  catchPhrase: {type: String}
+}, {versionKey: false, timestamps: true});
+
+module.exports = mongoose.model('Celebrity', celebritySchema);
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,20 @@
-const router = require("express").Router();
+const router = require('express').Router();
+const Celebrity = require('../models/Celebrity.model');
 
-// all your routes here
+// GET create new celebrity page
+router.get('/create', (req, res, next) => {
+  res.render('./celebrities/newCelebrity.hbs');
+});
+
+// POST create new celebrity
+router.post('/create', async (req, res, next) =>{
+  const {name, occupation, catchPhrase} = req.body;
+  try {
+    const createdCelebrity = await Celebrity.create({name, occupation, catchPhrase});
+    res.redirect('/celebrities');
+  } catch (err) {
+    res.render('./celebrities/newCelebrity.hbs');
+  };
+});
 
 module.exports = router;
