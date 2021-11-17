0c4e0109a44415419474874cffb64634d78ca9d1
TatianaPrada <tatiana.1227@hotmail.com>
2021-11-17 15:20:50 +0000

    chalk installed, databased changed

--- db/index.js
+++ db/index.js
@@ -2,6 +2,8 @@
 // https://www.npmjs.com/package/mongoose
 const mongoose = require("mongoose");
 
+const chalk = require('chalk')
+
 // ℹ️ Sets the MongoDB URI for our app to have access to it.
 // If no env has been set, we dynamically set it to whatever the folder name was upon the creation of the app
 
@@ -15,7 +17,7 @@ mongoose
     useCreateIndex: true
   })
   .then((x) => {
-    console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`);
+    console.log(chalk.bgBlueBright(`Connected to Mongo! Database name: "${x.connections[0].name}"`));
   })
   .catch((err) => {
     console.error("Error connecting to mongo: ", err);
