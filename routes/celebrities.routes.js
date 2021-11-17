c4f523f3857687abbe9c443678c7e98dbf259dfa
JosePedro <guedes.jose@gmail.com>
2021-11-17 09:43:19 +0000

    updated version

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,5 @@
 const Celebrity = require("../models/Celebrity.model");
 const router = require("express").Router();
-const Movie = require("../models/Movie.model");
 
 // all your routes here
 
@@ -8,15 +7,12 @@ router.get("/celebrities/create",(req,res)=>{
     res.render("celebrities/new-celebrity");
 })
 
-
-router.post("/celebrities/create/:id",(req,res)=>{
-    
+router.post("/celebrities/create/",(req,res)=>{
     const { name, occupation, catchPhrase } = req.body;
     Celebrity
     .create({name, occupation, catchPhrase})
     .then((createdCeleb) => {
-      res.redirect(`celebrities${createdCeleb._id}`);
-    })
+      res.redirect("celebrities")
     .then((createdCeleb) => {
         return Movie.findByIdAndUpdate(createdCeleb, { $push: { celebrities: createdCeleb._id } })
       })
@@ -26,23 +22,10 @@ router.post("/celebrities/create/:id",(req,res)=>{
 router.get("/celebrities",(req,res)=>{
    
     Celebrity.find()
-    .then((celebList) => {  // array with found books
-        console.log(celebList)
-        
-        res.render("celebrities/celebrities", { listofCelebs: celebList })
-      })
-      .catch( (err) => console.log(err));
+    .then((celebList) => { 
+    res.render("celebrities/celebrities", { listofCelebs: celebList })
     })
-
-
-
-
-
-
-
-
-
-
-
+    .catch( (err) => console.log(err));
+    })})
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,32 +2,44 @@ const router = require("express").Router();
 const Movie = require("../models/Movie.model");
 const Celebrity = require("../models/Celebrity.model");
 
+
+
+router.get("/movies", (req,res)=>{
+    Movie.find()
+    .then((movieList)=>{
+        res.render("movies", {movieList})
+    }) })
 router.get("/movies/create",(req,res)=>{
-    res.render("movies/new-movie");
-})
+    Celebrity.find()
+    .then((celebsList)=> {
+        res.render("movies/new-movie", {celebsList: celebsList})
+    })
 
 router.post("/movies/create",(req,res)=>{
     const { title, genre, plot, cast } = req.body;
     Movie
     .create({ title, genre, plot ,cast })
     .then((createdMovie) => {
-            res.redirect(`movies${createdMovie._id}`);
+            res.redirect("movies");
           })
     .catch( (err) => console.log(err));
+})})
 
-});
+router.get("/movies/create",(req,res)=>{
+    res.render("movies/new-movie");
+})
 
-router.get("/movies",(req,res)=>{
-    Movie
-    .find()
-    .populate("celebrities")
-    .then((movieList) => {  // array with found books
-        console.log(movieList)
-        res.render("movies/movies", { listofMovies: movieList })
-      })
-      .catch( (err) => console.log(err));
+router.get("/movies/:moviesID", (req,res)=>{
+
+    Movie.findById(movieID)
+    .populate("cast")
+    .then((foundMovie)=>{
+        res.render("movies/movie-details", {foundMovie})
     })
-
+    .catch((err)=>{
+        console.log(err)
+    })
+})
 
 
 module.exports = router;
