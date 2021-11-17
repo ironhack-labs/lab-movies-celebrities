8a6174faca6a1e25adeb134ea282c40f1c4faaef
nachbr91 <nabero91@gmail.com>
2021-11-17 17:58:06 +0100

    Iteration 6 and 7 done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,42 @@
 const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model');
+const Movie = require('../models/Movie.model');
 
-// all your routes here
+// GET create new movie page
+router.get('/create', async (req, res, next) => {
+  try {
+    const loadCelebrities = await Celebrity.find();
+    res.render('./movies/newMovie.hbs', {
+      loadCelebrities: loadCelebrities,
+    });
+  } catch (err) {
+    console.log('Error:', err);
+  }
+});
+
+// POST create new movie
+router.post('/create', async (req, res, next) => {
+  const {title, genre, plot, cast} = req.body;
+  try {
+    const createdMovie = await Movie.create({title, genre, plot, cast});
+    console.log(createdMovie);
+    res.redirect('/movies');
+  } catch (err){
+    console.log('Error:', err)
+    res.render('./movies/newMovie.hbs');
+  };
+});
+
+// GET movies pages
+router.get('/', async (req, res, next) => {
+  try {
+    const showMovies = await Movie.find();
+    res.render('./movies/movies.hbs', {
+      showMovies: showMovies,
+    });
+  } catch (err) {
+    console.log('Error:', err);
+  };
+});
 
 module.exports = router;
