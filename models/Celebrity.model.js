3eddaeba12de586f82224d35b83d02280e85d377
JoseLValdes <jolvaldgo@gmail.com>
2021-11-17 13:04:11 +0100

    Iteration #2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,13 @@
-//  Add your code here
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const modelSchema = new Schema(
+  {
+    name: { type: String, required: true },
+    occupation: { type: String, required: true },
+    catchPhrase: { type: String, required: true },
+  },
+  { versionKey: false, timestamps: true }
+);
+
+module.exports = mongoose.model("Model", modelSchema);
