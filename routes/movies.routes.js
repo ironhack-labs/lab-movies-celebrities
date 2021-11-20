30685167b6da7c8f6205ccdf5227579892bf48de
Philipp Kastl <picastle@posteo.de>
2021-11-20 16:35:28 +0100

    add iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,8 +1,30 @@
 //make a reference between function Router from express to our local variable router
 
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
 // handle routes to movies here
+
+router.get("/", async (req, res, next) => {
+  const allMovies = await Movie.find();
+  res.render("movies/movies", { allMovies });
+});
+
+router.get("/create", async (req, res, next) => {
+  const allCelebs = await Celebrity.find();
+
+  res.render("movies/new-movie", { allCelebs });
+});
+
+router.post("/create", async (req, res, next) => {
+  console.log(req.body);
+  try {
+    const newMovie = req.body;
+    await Movie.create(newMovie);
+  } catch (error) {}
+  res.redirect("/movies");
+});
 
 //exports
 module.exports = router;
