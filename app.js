970caae20466f0ac7c05720162cbc491ce1685ff
phimanim <aliker.imane@gmail.com>
2021-11-20 13:18:27 +0100

    it 1

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 const index = require('./routes/index');
 app.use('/', index);
 
+const celebritiesRoutes = require('./routes/celebrities.routes')
+app.use('/', celebritiesRoutes)
+
+const moviesRoutes = require('./routes/movies.routes')
+app.use('/', moviesRoutes)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
