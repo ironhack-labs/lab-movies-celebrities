8c22158af33a100b7310b41fc15f81cabef3611d
Philipp Kastl <picastle@posteo.de>
2021-11-20 15:18:51 +0100

    add iteration 3

--- config/index.js
+++ config/index.js
@@ -29,11 +29,14 @@ module.exports = (app) => {
 
   // Normalizes the path to the views folder
   app.set("views", path.join(__dirname, "..", "views"));
+
   // Sets the view engine to handlebars
   app.set("view engine", "hbs");
   // Handles access to the public folder
   app.use(express.static(path.join(__dirname, "..", "public")));
 
   // Handles access to the favicon
-  app.use(favicon(path.join(__dirname, "..", "public", "images", "favicon.ico")));
+  app.use(
+    favicon(path.join(__dirname, "..", "public", "images", "favicon.ico"))
+  );
 };
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,8 +1,35 @@
 //make a reference between function Router from express to our local variable router
 
+//require celebrity model
+const Celebrity = require("../models/Celebrity.model");
+
 const router = require("express").Router();
 
 //insert handles here
+
+//route to celebrities.hbs
+router.get("/", (req, res, next) => {
+  res.render("celebrities/celebrities", {});
+});
+
+//route to new-celebrities.hbs
+router.get("/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
+//route data from form of new-celebrity
+router.post("/create", async (req, res, next) => {
+  try {
+    const newCeleb = req.body;
+    await Celebrity.create(newCeleb);
+  } catch (error) {
+    console.error(`Following error occured by post new celebrity: ${error}`);
+    res.redirect("/create");
+  }
+
+  //why do we use only /celebrities instead of /celebrities/celebrities
+  //is it because of level of folder or path in browser
+  res.redirect("/celebrities");
+});
 
 //export
 
