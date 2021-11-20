82bd6379d36c3f491faad68f1779b42c6a5c0c69
Philipp Kastl <picastle@posteo.de>
2021-11-20 14:30:33 +0100

    add iteration 1

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,9 @@
+//make a reference between function Router from express to our local variable router
+
+const router = require("express").Router();
+
+//insert handles here
+
+//export
+
+module.exports = router;
---
+++ routes/movies.routes.js
@@ -0,0 +1,8 @@
+//make a reference between function Router from express to our local variable router
+
+const router = require("express").Router();
+
+// handle routes to movies here
+
+//exports
+module.exports = router;
