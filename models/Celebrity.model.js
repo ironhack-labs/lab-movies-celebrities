89c5266061095ca9895474c5f64e7caec7d095b3
Philipp Kastl <picastle@posteo.de>
2021-11-20 14:35:08 +0100

    add iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,10 @@
+const { Schema, model } = require("mongoose");
+
 //  Add your code here
+const celebritySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+
+module.exports = model("Celebrity", celebritySchema);
