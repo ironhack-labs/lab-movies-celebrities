4bfc526951037ae9abf0846d5404ccf51413a975
Lilli Reilly <lilli.reilly@gmail.com>
2021-11-22 20:43:41 +0100

    Finished iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,5 +2,20 @@
 const router = require("express").Router();
 
 // all your routes here
+router.get('/celebrities/create', (req, res, next) => {
+    res.render("celebrities/new-celebrity")
+  });
 
+  router.post('/celebrities/create', (req, res, next) => {
+    const {name, occupation, catchPhrase} = req.body
+    celebrityModel.create({name, occupation, catchPhrase})
+    .then((celebrities) => {
+      console.log(celebrities)
+      res.redirect("/celebrities")
+    })
+    .catch((err) => {
+      console.log(err)
+      res.render("/celebrities/new-celebrity")
+    })
+  });
 module.exports = router;
