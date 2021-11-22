2125de97b3fccb08e95ca4d682acd4a78bc58314
Lilli Reilly <lilli.reilly@gmail.com>
2021-11-22 20:54:47 +0100

    Finished iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,6 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model")
 
 // all your routes here
 router.get('/celebrities/create', (req, res, next) => {
@@ -18,4 +19,14 @@ router.get('/celebrities/create', (req, res, next) => {
       res.render("/celebrities/new-celebrity")
     })
   });
+  router.get("/celebrities", (req, res, next) => {
+    const celebrityInfo = Celebrity.find()
+    .then((celebrityInfo) => {
+      console.log(celebrityInfo)
+      res.render('/celebrities', {celebrities: celebrityInfo})
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  })
 module.exports = router;
