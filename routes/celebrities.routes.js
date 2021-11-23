32281b10a39cdccfed3292ba2f041fe269d647f9
phimanim <aliker.imane@gmail.com>
2021-11-23 16:09:16 +0100

    it 3-4-5

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,34 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require ('../models/Celebrity.model');
 
-// all your routes here
+router.get('/celebrities/create', (req, res, next) => {
+    res.render("celebrities/new-celebrity");
+});
+  
+router.post('/celebrities/create',(req, res, next) => {
+   const celebrity = req.body;
+
+   Celebrity
+   .create(celebrity)
+   .then(()=> res.redirect("/celebrities"))
+   .catch((error) => next(error));
+});
+
+router.get('/celebrities', (req, res, next) => {
+  const celebrity = req.body;
+
+    Celebrity
+    .find(celebrity)
+    .then((celebrities) => {
+      console.log(celebrities);
+      res.render("celebrities/celebrities.hbs", [celebrities]);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+  
+  });
+  
+
 
 module.exports = router;
