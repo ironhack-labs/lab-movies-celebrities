f427b79a1f13925998a002080070c93e545e3e9e
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-11-29 23:19:32 +0100

    Iteration 6 completed

--- app.js
+++ app.js
@@ -34,6 +34,8 @@ app.use('/celebrities', celebritiesRoutes);
 const moviesRoutes = require('./routes/movies.routes');
 app.use('/movies', moviesRoutes);
 
+
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,11 +1,34 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+
+const Celebrity = require("../models/Celebrity.model")
+const Movie = require("../models/Movie.model")
+
 // all your routes here
 
-router.get("/movies", (req, res, next) => {
-    res.render("movies/movies");
-  });
+router.get("/create", async (req, res, next) => {
+    try {
+        const Celebrities = await Celebrity.find();
+        res.render("movies/new-movie.hbs", {Celebrities});
+    } catch(err){
+        console.log(err)
+    }
+});
 
+router.get("/", async (req, res, next) => {
+   res.render("movies/movies.hbs")
+});
+
+router.post("/create", async (req, res, next) => {
+    const {title, genre, plot, cast} = req.body
+    try{
+        const newMovie = await Movie.create({title, genre, plot, cast})
+        res.redirect("/movies")
+    } catch(err){
+        console.log(err)
+        res.render("movies/new-movie.hbs", {Celebrities});
+    }
+});
 
 module.exports = router;
