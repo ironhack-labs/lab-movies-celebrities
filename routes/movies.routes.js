a10ba5972bc5a75be86c7b012934e4a99c901171
Raquel <90280742+raqhidcor@users.noreply.github.com>
2021-11-29 23:59:50 +0100

    Iteration 10 completed

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -7,6 +7,8 @@ const Movie = require("../models/Movie.model")
 
 // all your routes here
 
+//Route to create a movie
+
 router.get("/create", async (req, res, next) => {
     try {
         const allCelebrities = await Celebrity.find();
@@ -15,6 +17,8 @@ router.get("/create", async (req, res, next) => {
         console.log(err)
     }
 });
+
+//Route to get all the movies
 
 router.get("/", async (req, res, next) => {
     try{
@@ -25,6 +29,7 @@ router.get("/", async (req, res, next) => {
   }
   });
 
+//Create movie post route
 
 router.post("/create", async (req, res, next) => {
     const {title, genre, plot, cast} = req.body
@@ -37,6 +42,8 @@ router.post("/create", async (req, res, next) => {
     }
 });
 
+//Route to get the details for one movie
+
 router.get("/:id", async (req, res) => {
   try {
       const selectedMovie = await Movie.findById(req.params.id).populate('cast');
@@ -46,6 +53,8 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+//Route for delete a movie selected by its ID 
+
 router.post("/:id/delete", async (req, res) => {
   try {
       const deletedMovie = await Movie.findByIdAndRemove(req.params.id);
@@ -54,6 +63,32 @@ router.post("/:id/delete", async (req, res) => {
       res.render("not-found.hbs", { errorMsg: "Movie not deleted" });
   }
 });
+
+//Route to edit a movie selected by its ID 
+
+router.get("/:id/edit", async (req, res) => {
+  try{
+      const selectedMovie = await Movie.findById(req.params.id);
+      const allCelebrities = await Celebrity.find();
+      res.render("movies/edit-movie.hbs", {selectedMovie, allCelebrities})
+  } catch(err){
+      console.log(err)
+  }
+});
+
+//Route post to edit a movie selected by its ID
+
+router.post("/:id", async (req, res) => {
+  console.log("edit")
+  const {title, genre, plot, cast} = req.body
+  try {
+      const editeddMovie = await Movie.findByIdAndUpdate(req.params.id, {title, genre, plot, cast});
+      res.redirect(`/movies/${req.params.id}`)
+  } catch(err) {
+      res.render("not-found.hbs", { errorMsg: "Movie not deleted" });
+  }
+});
+
 
 
 module.exports = router;
