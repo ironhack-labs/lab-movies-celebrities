c486f55ad7d9593acdbd3736a02a92680c4ade97
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2021-12-06 15:14:32 -0600

    Iteration 4: listing celebrities done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -11,16 +11,27 @@ router.get('/celebrities/create',(req,res,next)=>{
 //Hacer el post del formulario "Crear celebridad" para mandarlo a la DB
 router.post('/celebrities/create',(req,res,next)=>{
     const {name,occupation,catchPhrase} = req.body
+    console.log("EL REQ BODY",req.body)
     Celebrity.create({name,occupation,catchPhrase})
     .then((newCelebrity)=>{
         console.log('New celebrity Has Been Created: ',newCelebrity)
-        res.render('/celebrities')
+        res.redirect('/celebrities')
     })
     .catch((err)=>{
         res.render('celebrities/new-celebrity')
     })
 })
 
+
+//Crear el GEY para /celebrities
+router.get('/celebrities',(req,res,next)=>{
+    Celebrity.find()
+    .then((allCelebritiesFromDB)=>{
+        console.log("All Celebrities From DB: ",allCelebritiesFromDB)
+        res.render('celebrities/celebrities',{celebrities:allCelebritiesFromDB})
+    })
+    .catch(err=>console.log("ERROR en Getting ALL Celebrities from DB",err))
+})
 
 
 
