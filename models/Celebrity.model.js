30cd8ab10eb46383af55085949d72f458221197b
Oswaldo Aviles <oswaldoavilesib[1;7rgmail.com>
2021-12-06 14:56:58 -0600

    Iteration 4 Done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,5 +1,5 @@
 //  Add your code here
-const {Schema, model} = require('mangoose');
+const {Schema, model} = require('mongoose');
 
 const celebritySchema = new Schema(
     {
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,9 +1,27 @@
 const router = require('express').Router();
 
+const Celebrity = require('../models/Celebrity.model')
+
+
+//Mostrar el view para agregar una celebridad
+router.get('/celebrities/create',(req,res,next)=>{
+    res.render('celebrities/new-celebrity')
+})
+
+//Hacer el post del formulario "Crear celebridad" para mandarlo a la DB
+router.post('/celebrities/create',(req,res,next)=>{
+    const {name,occupation,catchPhrase} = req.body
+    Celebrity.create({name,occupation,catchPhrase})
+    .then((newCelebrity)=>{
+        console.log('New celebrity Has Been Created: ',newCelebrity)
+        res.render('/celebrities')
+    })
+    .catch((err)=>{
+        res.render('celebrities/new-celebrity')
+    })
+})
 
 
 
 
-
-
-module.exports = router
+module.exports = router;
