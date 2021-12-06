fce0fcbb5c7b82271d3d914483d018aeaef14499
Oswaldo Jose Aviles Ibarra <oswaldoavilesib>
2021-12-06 17:00:36 -0600

    Iteration 6: Form with Select Celebrities done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,13 +2,22 @@ const router = require('express').Router();
 
 const Movie = require('../models/Movie.model');
 
+const Celebrity = require('../models/Celebrity.model')
+
 
 //Hacer el GET de la URL para crear películas.
 
 router.get('/movies/create',(req,res,next)=>{
-    res.send('Hello')
+    Celebrity.find()
+    .then((dbCelebrities) =>{
+        console.log(dbCelebrities)
+        res.render('movies/new-movie',{dbCelebrities})
+    })
+    .catch(err=>console.log("ERROR EN GETTING MOVIES CREATE",err))
 })
 
+
+//Hacer el POST en la URL para meter los datos a la DB
 
 
 
