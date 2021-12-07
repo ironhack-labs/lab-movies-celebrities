cecc6805b78befbf019cf92293ca25136ba61862
SantiagoCortina <cortina.zapfe@gmail.com>
2021-12-07 00:11:00 -0600

    Iteracion 5, done.

---
+++ models/Movie.model.js
@@ -0,0 +1,15 @@
+const {Schema, model} = require('mongoose');
+
+const movieSchema = new Schema({
+    title:{
+        type:String,
+        require: true
+    },
+    genre: {
+        type:String
+    },
+    plot:String, 
+    cast: [{ type: Schema.Types.ObjectId, ref: 'celebSchema' }]
+}, {timestamps:true})
+
+module.exports = model('Movie', movieSchema);
