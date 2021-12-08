2c0f005bb1408ae32ec37695b9471cc018b4183c
SantiagoCortina <cortina.zapfe@gmail.com>
2021-12-07 21:55:13 -0600

    Iteracion 7, done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,6 +2,43 @@ const express = require('express');
 const router = express.Router();
 
 // require the Movie model here
+const Movie = require("../models/Movie.model");
+const Celeb = require("../models/Celebrity.model");
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then(movies =>{
+        res.render('movies/movies', {movies})
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('Error')
+    })
+});
+
+router.get('/movies/create', (req, res, next) => {
+    Celeb.find()
+        .then(celebs => {
+            res.render('movies/new-movie', {celebrities: celebs});
+        })
+        .catch(err => {
+            console.log(err)
+            res.send('Error')
+        })
+});
+
+router.post('/movies/create', (req, res, next) => {
+    const {title, genre, plot, cast, ... rest} = req.body
+
+    Movie.create({title, genre, plot, cast})
+    .then(movie => {
+        res.redirect('/movies')
+    })
+    .catch(err => {
+        console.log(err)
+        res.send('Error')
+    })
+});
 
 
 
