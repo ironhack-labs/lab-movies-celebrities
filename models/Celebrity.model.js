d03a6eed41fb9fa8f32bbb3a869e82c181443f6a
Sarah Ortega <sarah.ortega.fuentes@gmail.com>
2021-12-13 15:30:25 -0600

    Iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,15 @@
 //  Add your code here
+const { Schema, model } = require("mongoose");
+
+const CelebritySchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+
+const Celebrity = model("Celebrity", CelebritySchema);
+
+module.exports = Celebrity;
+
+
+
