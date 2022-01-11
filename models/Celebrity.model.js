b3472b68ddef244434844bd196bee8c240f3f65f
Chun Lam <Chun01@hotmail.fr>
2022-01-11 18:55:03 +0100

    Iteration3done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,14 @@
 //  Add your code here
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const celebritySchema = new Schema({
+    name: String,
+    occuation:String,
+    catchPhrase:String
+  });
+  
+  const CelebrityModel = mongoose.model("celebrity", celebritySchema);
+  
+  module.exports = CelebrityModel;
+  
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,20 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const express = require("express");
+const router = require("express").Router();
+const CelebrityModel = require("../models/Celebrity.model");
+
+// all your routes here
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity.hbs");
+});
+
+router.post("/celebrities/create", async function (req, res, next) {
+  try {
+    await CelebrityModel.create(req.body);
+    res.redirect("/celebrities");
+  } catch (err) {
+    next(err);
+  }
+});
+
+module.exports = router;
