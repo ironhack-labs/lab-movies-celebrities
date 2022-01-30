1eeea1de336f1a1ad370ac2b535c4fafaf1d0ea9
Vitor Afonso <vittokm@hotmail.com>
2022-01-30 11:35:38 +0000

    iteration1 completed

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//jshint esversion:8
 // ℹ️ Gets access to environment variables/settings
 // https://www.npmjs.com/package/dotenv
 require('dotenv/config');
@@ -27,6 +28,13 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const celebritiesRoutes = require('./routes/celebrities.routes');
+app.use('/', celebritiesRoutes);
+
+const moviesRoutes = require('./routes/movies.routes');
+app.use('/', moviesRoutes);
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,6 @@
+//jshint esversion:8
+const router = require("express").Router();
+
+// all your routes here
+
+module.exports = router;
