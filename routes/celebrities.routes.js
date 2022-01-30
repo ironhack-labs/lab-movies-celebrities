7aec48f10be11ee21a8ed6b6374948d5054a6a04
Vitor Afonso <vittokm@hotmail.com>
2022-01-30 19:07:45 +0000

    iteration5 completed

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -14,9 +14,9 @@ router.post('/celebrities/create', (req, res, next) =>  {
     const {name, occupation, catchPhrase} = req.body;
 
     Celebrity.create({name, occupation, catchPhrase})
-        .then(celebrityFromDB => {
+        .then(dbCelebrities => {
             
-            console.log(celebrityFromDB);
+            //console.log(dbCelebrities);
             res.redirect('celebrities/celebrities');
         })
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,35 @@
 //jshint esversion:8
 const router = require("express").Router();
 
+const Movie = require('../models/Movie.model');
+const Celebrity = require("../models/Celebrity.model");
 // all your routes here
+
+/************************* ALL MOVIES *****************************/
+
+router.get('/movies/create', (req, res, next) =>  {
+    Celebrity.find()
+        .then(dbCelebrities => {
+            res.render('movies/new-movie', {dbCelebrities});
+        })
+        .catch(err => {
+            res.render('celebrities/new-celebrity');
+            console.log('Something went wrong while displaying movie-create view.', err);
+        });
+    
+});
+
+router.post('/movies/create', (req, res, next) =>  {
+    const {title, genre, plot, cast} = req.body;
+    console.log(title, genre, plot, cast);
+    Movie.create({title, genre, plot, cast})
+        .then(dbMovie => {
+            console.log(dbMovie);
+            res.redirect('/movies/movies');
+        })
+        .catch(err => {
+            console.log('Something went wrong while creating new movie', err);
+        });
+});
 
 module.exports = router;
