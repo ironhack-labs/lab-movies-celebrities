66d28ad7a694fc43fac7593c9062d006a8414707
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-01-31 13:27:44 +0100

    finish iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,6 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-const Celebrity = require('../models/Celebrity.model.js');
+const Celebrity = require('../models/Celebrity.model');
 
 // all your routes here
 router.get('/celebrities/create', (req, res, next) => {
@@ -9,12 +9,20 @@ router.get('/celebrities/create', (req, res, next) => {
 
 router.post('/celebrities/create', (req, res, next) => {
     Celebrity.create(req.body)
-        .then((data) => {
+        .then(() => {
             res.redirect('/celebrities');
         })
         .catch(() => {
             res.render('celebrities/new-celebrity');
         })
+})
+
+router.get('/celebrities', (req, res, next) => {
+    Celebrity.find()
+        .then(celebrities => {
+            res.render('celebrities/celebrities', { celebrities: celebrities })
+        })
+        .catch((e) => console.log(e));
 })
 
 
