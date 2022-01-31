6205e0d8239d2e42bc5e353314ba2e5ea0f47636
Arturo Vivar <arthas1237@hotmail.com>
2022-01-31 13:41:08 +0100

    iteration5

---
+++ models/Movie.mode.js
@@ -0,0 +1,25 @@
+//Schema para el modelo de Movies
+
+
+const { Schema, model } = require("mongoose");
+
+const movieSchema = new Schema({
+  title: String,
+  genre: String,
+  plot: String,
+  cast: {
+      type: [Schema.Types.ObjectId],
+      ref: "Celebrity",
+  },
+},
+
+{
+  timestamps: true
+}
+
+
+);
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
