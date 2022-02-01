cf4b4701c14512a6dd847aee950bc9579041224c
Clara Villa <Clara.Villa@lego.com>
2022-02-01 15:38:53 +0000

    iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,2 +1,31 @@
 const router = require("express").Router();
+
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find().then((foundCelebs) => {
+    res.render("movies/new-movie", { celebs: foundCelebs });
+  });
+});
+
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  console.log(title, genre, plot, cast);
+  Movie.create({ title, genre, plot, cast })
+    .then((newMovie) => {
+      console.log("added movie " + newMovie.title);
+      res.redirect("/movies");
+    })
+    .catch((error) => {
+      console.log(error);
+      next(error);
+      res.redirect("/movies/create");
+    });
+});
+
+router.get("/movies", (req, res, next) => {
+  res.render("movies/movies");
+});
+
 module.exports = router;
