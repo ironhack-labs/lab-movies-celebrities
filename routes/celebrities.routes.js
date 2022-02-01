a980a96b2ae0d58adb5a33b491caa3332d1edc24
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-01 15:03:14 +0100

    Solved Iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,25 @@
+// Iteration #3
 const router = require("express").Router();
 
-// all your routes here
+const Celebrity = require("../models/Celebrity.model");
+
+// GET route for celebrities/create:
+router.get("/celebrities/create", (req, res) => res.render("celebrities/new-celebrity"));
+
+// POST route for celebrities/create:
+router.post("/celebrities/create", (req, res) => {
+    const { name } = req.body;
+    Celebrity.findOne({ name })
+      .then((celebrityFromDB) => {
+        if (!celebrityFromDB) {
+          Celebrity.create({ name })
+          .then(() => res.redirect('/celebrities'));
+        } else {
+          res.render("/celebrities/create", { message: "It seems this celebrity is already registered." });
+          return;
+        }
+      })
+      .catch((err) => console.log(`Error while creating a new celebrity: ${err}`));
+  });
 
 module.exports = router;
