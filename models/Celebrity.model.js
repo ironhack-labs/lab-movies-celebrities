90fa004c7cef065fa799875ac6134b0ccdb23af2
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-01 14:56:05 +0100

    Iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,14 @@
-//  Add your code here
+// Iteration #2
+
+//initiate connection using mongoDB mongoose
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+  } 
+);
+
+module.exports = model("Celebrity", celebritySchema);
