9c3d354482e1c02d613611f0fb6049f256f26007
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-01 15:14:06 +0100

    Solved Iteration 5

---
+++ models/Movie.model.js
@@ -0,0 +1,18 @@
+// Iteration #5
+const { Schema, model } = require('mongoose');
+
+const movieSchema = new Schema(
+  {
+    title: String,
+    genre: String,
+    plot: String,
+    cast: { type: Schema.Types.ObjectId, ref: "Celebrity" }
+  },
+  {
+    timestamps: true
+  }
+);
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
