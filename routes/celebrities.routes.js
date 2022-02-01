0da3871b5d598be591259586cb48a044a72c90f0
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-01 17:22:29 +0100

    Solved Iteration 7 (Iteration 6 does not work correctly)

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -8,14 +8,14 @@ router.get("/celebrities/create", (req, res) => res.render("celebrities/new-cele
 
 // POST route for celebrities/create:
 router.post("/celebrities/create", (req, res) => {
-    const { name } = req.body;
+    const { name, occupation, catchPhrase } = req.body;
     Celebrity.findOne({ name })
       .then((celebrityFromDB) => {
         if (!celebrityFromDB) {
-          Celebrity.create({ name })
+          Celebrity.create({ name, occupation, catchPhrase })
           .then(() => res.redirect('/celebrities'));
         } else {
-          res.render("/celebrities/create", { message: "It seems this celebrity is already registered." });
+          res.render("/celebrities/create", { message: "It seems this celebrity is already created." });
           return;
         }
       })
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,33 @@
+// Iteration #5
 const router = require("express").Router();
 
-// all your routes here
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
+
+// GET route for movies/create:
+router.get("/movies/create", (req, res) => res.render("movies/new-movie"));
+
+// POST route for movies/create:
+router.post("/movies/create", (req, res) => {
+    const { title, genre, plot } = req.body;
+    Movie.findOne({ title })
+      .then((movieFromDB) => {
+        if (!movieFromDB) {
+          Movie.create({ title, genre, plot })
+          .then(() => res.redirect('/movies'));
+        } else {
+          res.render("/movies/create", { message: "It seems this movie is already created." });
+          return;
+        }
+      })
+      .catch((err) => console.log(`Error while creating a new movie: ${err}`));
+  });
+
+// GET all movies from the database:
+router.get("/movies", (req, res) => {
+    Movie.find()
+      .then((moviesFromDB) => res.render("movies/movies", { movies: moviesFromDB }))
+      .catch((err) => console.log(`Error while getting movies from the database: ${err}`));
+  });
 
 module.exports = router;
