a090beb77b2b33d010ad100994ceca3b20d7a72c
David Vekony <dave.vekony@gmail.com>
2022-02-01 20:39:42 +0000

    iteration 3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,31 @@
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
 
-// all your routes here
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("celebrities/celebrities", { celebrities });
+    })
+    .catch((err) => console.log(err));
+});
+
+router.get("/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity", {});
+});
+
+router.post("/create", (req, res, next) => {
+  Celebrity.create(req.body)
+    .then((celeb) => {
+      console.log(celeb);
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      res.render("celebrities/new-celebrity");
+    });
+});
+
+router.get("/new-celebrity", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
 
 module.exports = router;
