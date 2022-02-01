7394bfff2d8527a1c095f021e0ca6906dde75acc
László Tarnai <laszlo.tarnai@LEGO.com>
2022-02-01 21:28:51 +0100

    all done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -5,53 +5,59 @@ const Celebrity = require("../models/Celebrity.model");
 const Movie = require("../models/Movie.model");
 
 // GET route for movies/create:
-router.get("/movies/create", (req, res) => res.render("movies/new-movie"));
+router.get('/movies/create', (req, res, next) => {
+  Celebrity.find()
+  .then(listOfCelebrities => res.render('movies/new-movie', { celebrity: listOfCelebrities }))
+  .catch(err => console.log('Error retrieving celebrities list: ', err))
+});
 
 // POST route for movies/create:
-router.post("/movies/create", (req, res) => {
-    const { title, genre, plot } = req.body;
-    Movie.findOne({ title })
-      .then((movieFromDB) => {
-        if (!movieFromDB) {
-          Movie.create({ title, genre, plot })
-          .then(() => res.redirect('/movies'));
-        } else {
-          res.render("/movies/create", { message: "It seems this movie is already created." });
-          return;
-        }
-      })
-      .catch((err) => console.log(`Error while creating a new movie: ${err}`));
-  });
+router.post('/movies/create', (req, res, next) => {
+  const { title, genre, plot, cast} = req.body;
 
-// GET all movies from the database:
+  Movie.create({title, genre, plot, cast})
+  .then(() => res.redirect('/movies'))
+  .catch(err => res.render("movies/create"))
+});
+
+// Iteration #5 - GET all movies from the database:
 router.get("/movies", (req, res) => {
     Movie.find()
       .then((moviesFromDB) => res.render("movies/movies", { movies: moviesFromDB }))
       .catch((err) => console.log(`Error while getting movies from the database: ${err}`));
   });
 
-// GET route for displaying the movie details page:
+// Iteration #8 - GET route to display the details of a specific movie:
 router.get('/movies/:movieId', (req, res, next) => {
-    const { movieId } = req.params;
-   
-    Movie.findById(movieId)
-      .populate('author comments') // <-- the same as .populate('author).populate('comments')
-      .populate({
-        // we are populating author in the previously populated comments
-        path: 'comments',
-        populate: {
-          path: 'author',
-          model: 'User'
-        }
-      })
-      .then(foundMovie => res.render('movies/details', foundMovie))
-      .catch(err => {
-        console.log(`Err while getting a single movie from the database: ${err}`);
-        next(err);
-      });
-  });
+  const { movieId } = req.params;
 
-// POST route to delete a movie from the database:
+  Movie.findById(movieId)
+  .populate('cast')
+  .then(theMovie => res.render('movies/movie-details.hbs', { movie: theMovie }))
+  .catch(err => console.log('Error while retrieving movie details: ', err))
+});
+
+// Iteration #9 - GET route to display the form to update a specific movie
+router.get('/movies/:movieId/edit', (req, res, next) => {
+  const { movieId } = req.params;
+ 
+  Movie.findById(movieId)
+    .populate('cast')
+    .then(movieToEdit => res.render('movies/edit-movie', { movie: movieToEdit }))
+    .catch(err => console.log('Error while retrieving movie details: ', err));
+});
+
+// Iteration #9 - POST route to actually make updates on a specific movie
+router.post('/movies/:movieId/edit', (req, res, next) => {
+  const { movieId } = req.params;
+  const { title, genre, plot, cast } = req.body;
+ 
+  Movie.findByIdAndUpdate(movieId, { title, genre, plot, cast })
+    .then(() => res.redirect(`/movies`))
+    .catch(err => console.log('Error while retrieving movie details: ', err));
+});
+
+// Iteration #10 - POST route to delete a movie from the database:
 router.post('/movies/:movieId/delete', (req, res, next) => {
   const { movieId } = req.params;
  
