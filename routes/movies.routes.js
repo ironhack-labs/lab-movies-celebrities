9ba3c22589e48eca84024ac612cfff04fc53d05c
Vicente Garcia <vicentegarciaayllon@gmail.com>
2022-02-01 02:11:24 +0100

    finish iterations 8, 9 and 10

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,6 @@
 const Movie = require("../models/Movie.model");
 const Celebrity = require("../models/Celebrity.model");
+const { route } = require("./celebrities.routes");
 
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
@@ -30,6 +31,49 @@ router.get('/movies', (req, res, next) => {
     Movie.find()
         .then(movies => {
             res.render('movies/movies', { movies: movies });
+        })
+});
+
+router.get('/movies/:id', (req, res, next) => {
+    Movie.findById(req.params.id)
+        .populate('cast')
+        .then((movie) => {
+            res.render('movies/movie-details', { movie: movie})
+        })
+        .catch((e) => console.log(e));
+})
+
+router.post('/movies/:id/delete', (req, res, next) => {
+    Movie.findByIdAndDelete(req.params.id)
+        .then(() => Movie.find())
+        .then((movies) => {
+            res.render('movies/movies', { movies: movies });
+        })
+        .catch((e) => console.log(e));
+})
+
+router.get('/movies/:id/edit', (req, res, next) => {
+    Movie.findById(req.params.id)
+        .populate('cast')
+        .then((movie) => {
+            Celebrity.find()
+            .then(celebrities => {
+                console.log(movie, celebrities)
+                res.render('movies/edit-movie', { movie: movie, celebrities: celebrities });
+            })
+        })
+        .catch((e) => console.log(e));
+})
+
+router.post('/movies/:id', (req, res, next) => {
+    const data = { title, genre, plot, cast } = req.body;
+
+    Movie.findByIdAndUpdate(req.params.id, data)
+        .then(() => {
+            res.redirect(`/movies/${req.params.id}`);
+        })
+        .catch(() => {
+            res.render(`movies/${req.params.id}/edit`);
         })
 })
 
