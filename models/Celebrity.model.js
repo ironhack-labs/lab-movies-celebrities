73e0076d30c526b3f7870e7d9b145daf77678bb7
Clara Villa <Clara.Villa@lego.com>
2022-02-01 14:06:58 +0000

    iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,17 @@
 //  Add your code here
+const { model, Schema } = require("mongoose");
+
+const celebritySchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    occupation: String,
+    catchPhrase: String,
+  },
+  { timestamp: true }
+);
+
+modules.export = model("Celebrity".celebritySchema);
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,2 +1,4 @@
 const router = require("express").Router();
 module.exports = router;
+
+const Celebrity = require("..models/Celebrity.model");
