a3e808fe2c562e90e549c91e0239c1d2a3c524a9
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-01 15:14:17 +0100

    Iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,22 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+//require the model
+const Celebrity = require("../models/Celebrity.model");
+
+//GET route to display form to create a celebrity
+router.get('/celebrities/create', (req, res, next) => {
+    res.render('celebrities/new-celebrity.hbs');
+});
+
+//POST route to post info about new celebrity
+router.post('/celebrities/create', (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+    Celebrity.create({ name, occupation, catchPhrase })
+    .then(() => res.redirect('/celebrities'))
+    .catch((error) => res.render('celebrities/new-celebrity.hbs'));
+});
+
 // all your routes here
 
 module.exports = router;
