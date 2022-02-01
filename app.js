6bcabc8be6d507ff7d147d4894b0ec18c9d05f09
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-01 14:51:11 +0100

    Iteration 1

--- app.js
+++ app.js
@@ -28,6 +28,14 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 const index = require('./routes/index');
 app.use('/', index);
 
+//route for movies
+const moviesRoutes = require('./routes/movies.routes');
+app.use('/', moviesRoutes);
+
+//route for celebrities
+const celebritiesRoutes = require('./routes/celebrities.routes');
+app.use('/', celebritiesRoutes);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
