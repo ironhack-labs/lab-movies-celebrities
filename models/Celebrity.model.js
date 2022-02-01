f244040a87879aa7cb70b9220995f021be97c9f0
ElisaMamolo <elisa.carlotta.mamolo@lego.com>
2022-02-01 15:04:37 +0100

    it 2 - celebrity model

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,17 @@
-//  Add your code here
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+    comments: [{ type: Schema.Types.ObjectId, ref: "Comment" }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Celebrity = model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
