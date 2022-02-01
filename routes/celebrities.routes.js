0138e9e76ea79e9924fedfe8f90565066e32d654
Amanda Rodrigues <amanda.rodrigues@lego.com>
2022-02-01 15:24:35 +0100

    Iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -9,12 +9,21 @@ router.get('/celebrities/create', (req, res, next) => {
     res.render('celebrities/new-celebrity.hbs');
 });
 
-//POST route to post info about new celebrity
+//POST get all info abou new celeb user submitted from the form. use this to create a new celeb in the database
 router.post('/celebrities/create', (req, res, next) => {
     const { name, occupation, catchPhrase } = req.body;
     Celebrity.create({ name, occupation, catchPhrase })
     .then(() => res.redirect('/celebrities'))
     .catch((error) => res.render('celebrities/new-celebrity.hbs'));
+});
+
+//GET all celebs and render the list
+router.get('/celebrities', (req, res, next) => {
+    Celebrity.find()
+    .then((allCelebs) => {
+        console.log('Retrieced celebs from DB:', allCelebs);
+        res.render('celebrities/celebrities.hbs', { celebs : allCelebs });
+    });
 });
 
 // all your routes here
