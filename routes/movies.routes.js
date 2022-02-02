97405eb920f7cdd56d8d99c2c3628c8da546ae48
AnaModesto21 <77633954+AnaModesto21@users.noreply.github.com>
2022-02-02 19:27:40 +0000

    Done up to iteration 9

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,17 +1,26 @@
 const router = require("express").Router();
 const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+
 
 // all your routes here
 router.get('/', (req,res) =>{
-    Celebrity.find()
+    Movie.find()
+    .populate('cast')
     .then(allMoviesFromDB => {
+        console.log(allMoviesFromDB)
         res.render('movies/movies.hbs', {movies: allMoviesFromDB});
     })
     .catch(error => {next(error)});
 })
 
 router.get('/create', (req,res) =>{
-    res.render('movies/new-movie.hbs')
+        Celebrity.find()
+        .then(allCelebritiesFromDB => {
+            console.log(allCelebritiesFromDB)
+            res.render('movies/new-movie.hbs', {celebrities: allCelebritiesFromDB});
+        })
+        .catch(error => {next(error)});
 })
 
 router.post('/create', (req,res,next) =>{
@@ -19,10 +28,28 @@ router.post('/create', (req,res,next) =>{
 
     Movie.create({title, genre, plot, cast})
     .then(() => {
-        res.redirect('/');
+        res.redirect('/movies');
     })
     .catch(error => {res.render('movies/new-movie.hbs'); next(error)});
 });
 
+router.post('/:id/delete', (req, res) => {
+    const {id} = req.params;
+
+    Movie.findByIdAndDelete(id)
+    .then(() => {
+        res.redirect('/movies');
+    })
+    .catch(error => next(error));
+})
+
+router.get('/:id', (req,res) =>{
+    Movie.findById(req.params.id)
+    .populate("cast")
+    .then((theMovie) => {
+        res.render('movies/movies-details.hbs', {movie: theMovie})
+    })
+    .catch(error => {next(error)});
+})
 
 module.exports = router;
