2e1fdb3b4b64f973d9f5143f7d841f4c10737ab1
sarapuertas <saritapuertas@hotmail.com>
2022-02-05 22:16:24 +0100

    it 5

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,43 @@
+const router = require("express").Router();
+const { redirect } = require("express/lib/response");
+// const app = require('../app')
+
+const Celebrity = require('../models/Celebrity.model')
+
+// Routes:
+
+
+// Create a celebrity
+
+router.get('/create', (req, res) => {
+    res.render('celebrities/new-celebrity')
+})
+router.post('/create', (req, res) => {
+
+    const { name, occupation, catchPhrase } = req.body
+
+    Celebrity
+        .create({ name, occupation, catchPhrase })
+        .then(() => res.redirect('/celebrities'))
+        .catch(err => {
+            res.render('celebrities/new-celebrity')
+            console.log(err)
+        })
+})
+
+// List the celebrities
+
+router.get('/', (req, res, next) => {
+
+    Celebrity
+        .find()
+        .then(celebrities => res.render('celebrities/celebrities', { celebrities }))
+        .catch(err => console.log(err))
+
+})
+
+
+
+
+
+module.exports = router
---
+++ routes/movies.routes.js
@@ -0,0 +1,6 @@
+const router = require("express").Router();
+
+// Routes:
+
+
+module.exports = router;
