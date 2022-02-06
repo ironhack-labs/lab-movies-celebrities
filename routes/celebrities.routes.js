cc2425298c81539a5c8e39003286cefafb7604d4
Ernesto Espinoza M <emenocal07@gmail.com>
2022-02-06 21:23:41 +0100

    Terminado al 95%

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,13 +1,12 @@
 const router = require("express").Router();
-
 const Celebrity = require("../models/Celebrity.model");
 
-// RUTA A CREAR CELEBRIDADES (GET)
+// RUTA A CREAR FAMOSOS (GET)
 router.get("/crear", (req, res, next) => {
     res.render("celebrities/new-celebrity")
 });
 
-// RUTA A CREAR CELEBRIDADES (POST)
+// RUTA A CREAR FAMOSOS(POST)
 router.post("/crear", (req, res, next) => {
 
     const { name, occupation, catchPhrase } = req.body
@@ -18,13 +17,57 @@ router.post("/crear", (req, res, next) => {
         .catch(err => res.render("celebrities/new-celebrity"))
 });
 
-//RUTA A  LISTAR CELEBRIDADES
+//RUTA A  LISTAR FAMOSOS
 router.get("/", (req, res, next) => {
 
     Celebrity
         .find()
-        .then((celebrity => res.render("celebrities/celebrities", {celebrity})))
-        .catch(err=> console.log(err))
+        .then((celebrity => res.render("celebrities/celebrities", { celebrity })))
+        .catch(err => console.log(err))
 });
+//RUTA DETALLE DE FAMOSOS
+router.get("/:celeb_id", (req, res, next) => {
 
+    const { celeb_id } = req.params
+
+    Celebrity
+        .findById(celeb_id)
+        .then(celeb => res.render("celebrities/celebrity-details", celeb))
+        .catch(err => console.log(err))
+})
+
+//RUTA EDITAR FAMOSO (GET)
+router.get("/:celeb_id/editar", (req, res, next) => {
+
+    const { celeb_id } = req.params
+
+    Celebrity
+        .findById(celeb_id)
+        .then((celebDetails) => res.render("celebrities/edit-celebrity", celebDetails))
+        .catch(err => console.log(err))
+})
+//RUTA EDITAR FAMOSOS (POST)
+router.post("/:celeb_id/editar", (req, res, next) => {
+
+    const { celeb_id } = req.params
+    const { name, occupation, catchPhrase } = req.body
+
+    Celebrity
+        .findByIdAndUpdate(celeb_id, { name, occupation, catchPhrase }, { new: true })
+        .then(() => res.redirect("/celebridades"))
+        .catch(err => console.log(err))
+})
+
+
+//RUTA BORRAR FAMOSOS
+router.post("/:celeb_id/eliminar", (req, res, next) => {
+
+    const { celeb_id } = req.params
+
+    Celebrity
+        .findByIdAndDelete(celeb_id)
+        .then(() => res.redirect("/celebridades"))
+        .catch(err => console.log(err))
+
+})
 module.exports = router
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,17 +3,13 @@ const router = require("express").Router();
 const Movie = require("../models/Movie.model");
 const Celebrity = require("../models/Celebrity.model");
 
+
 // RUTA A CREAR PELICULAS (GET)
 router.get("/crear", (req, res, next) => {
+    Celebrity
+        .find()
+        .then(CelebName => res.render("movies/new-movie", { CelebName }))
 
-    const { cast } = req.body
-
-    Movie
-    .findById(cast)
-    .populate('name')
-        .then(name => res.send(`Hola ${Movie}`))
-
-    // INTENTAR ESTO DE NUEVO, HE MODIFICADO EL MODELO DE CELEBRITIES PARA PONER A MONGOOSE EN UNA VARIBLE Y APARENTEMENTE LLEVARMELO A MOVIES. INTENTAR VER SI CAST O MOVIE ARROJAN ALGOEN LA CONSOLA
 });
 
 // RUTA A CREAR PELICULAS (POST)
@@ -25,7 +21,7 @@ router.post("/crear", (req, res, next) => {
         .create({ title, genre, plot, cast })
         .then(() => res.redirect("/peliculas"))
         .catch(err => res.render("movies/new-movie"))
-    
+
 });
 
 //RUTA LISTADO PELICULAS
@@ -37,5 +33,64 @@ router.get("/", (req, res, next) => {
         .catch(err => console.log(err))
 
 });
+
+//RUTA DETALLE PELICULAS
+router.get("/:movie_id", (req, res, next) => {
+
+    const { movie_id } = req.params
+
+    Movie
+        .findById(movie_id)
+        .populate("cast")
+        .then(movie => res.render("movies/movie-details", movie))
+        .catch(err => console.log(err))
+})
+
+//RUTA EDITAR PELICULAS (GET)
+router.get("/:movie_id/editar", (req, res, next) => {
+
+    const { movie_id } = req.params
+
+    Movie
+        .findById(movie_id)
+    // Celebrity
+    //     .find()
+    //     .then(allDetails =>{
+    //         res.render("movies/edit-movie", allDetails)
+    //     })
+
+        .populate("cast")
+        //     .then(theMovie => {
+        //         return Movie.find()
+        //     })
+        //     .then(allDetails =>{})
+        // Celebrity
+        //     .find()
+        .then((movDetails) => res.render("movies/edit-movie", movDetails))
+        .catch(err => console.log(err))
+})
+//RUTA EDITAR PELICULAS (POST)
+router.post("/:movie_id/editar", (req, res, next) => {
+
+    const { movie_id } = req.params
+    const { title, genre, plot, cast } = req.body
+
+    Movie
+        .findByIdAndUpdate(movie_id, { title, genre, plot, cast }, { new: true })
+        .then(() => res.redirect("/peliculas"))
+        .catch(err => console.log(err))
+})
+
+//RUTA BORRAR PELICULAS
+router.post("/:movie_id/eliminar", (req, res, next) => {
+
+    const { movie_id } = req.params
+
+    Movie
+        .findByIdAndDelete(movie_id)
+        .then(() => res.redirect("/peliculas"))
+        .catch(err => console.log(err))
+
+})
 
 module.exports = router
