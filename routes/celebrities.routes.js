7cccd011f9a55aa9b140f8f26f31c87720faf5c9
Mario Diaz <mariodzls@gmail.com>
2022-02-06 14:22:03 +0100

    Paso 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,8 +2,9 @@ const router = require('express').Router()
 const { redirect } = require('express/lib/response')
 const Celebrity = require('../models/Celebrity.model')
 
+
 router.get("/crear", (req, res) => {
-        res.render('celebrities/new-celebrity')
+    res.render('celebrities/new-celebrity')
 })
 
 router.post("/crear", (req, res) => {
@@ -11,9 +12,17 @@ router.post("/crear", (req, res) => {
 
     Celebrity
         .create({ name, ocuppation, catchPhrase })
-        .then(() => res.redirect('/celebrities'))
+        .then(() => res.redirect('/celebridades/celebridades'))
         .catch(err => console.log(err))
 })
+
+router.get("/celebridades", (req, res, next) => {
+    Celebrity
+        .find()
+        .then(celebrities => res.render('celebrities/celebrities', { celebrities }))
+        .catch(err => console.log(err))
+})
+
 
 
 
