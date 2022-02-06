7e3e0fe9d40f56121948b6af0ffb68c352598d1a
Salvador rus <saersalva@gmail.com>
2022-02-06 17:40:38 +0100

    a seguir

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,15 @@
 //  Add your code here
+const { Schema, model } = require('mongoose')
+
+const celebritySchema = new Schema(
+    {
+        name: String,
+        ocupation: String,
+        catchPhrase: String,
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('Book', celebritySchema)
