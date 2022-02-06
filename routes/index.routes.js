834a3a4f3bfabe69e933d550c3aa0e925d83568f
Laura Del valle <l.delvallebermejo@gmail.com>
2022-02-06 17:52:41 +0100

    celebrity part done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,21 @@
 const router = require("express").Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
+router.get("/", (req, res, next) => { 
   res.render("index");
 });
 
+
+
+
 module.exports = router;
+
+
+
+
+
+
+
+
+
+
