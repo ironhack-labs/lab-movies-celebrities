12b2b7c196240b040722374ebe1fd4b31750da9f
dorrrn <doan7tran@gmail.com>
2022-02-08 20:11:55 +0100

    created movies list, details pages and delete function

--- routes/movies.js
+++ routes/movies.js
@@ -5,7 +5,7 @@ const Movie = require("../models/Movie.model");
 router.get("/", (req, res, next) => {
   Movie.find()
     .then((movieFromDB) => {
-      res.render("movies/movies-list", { movies: movieFromDB });
+      res.render("movies/movies-list", { movie: movieFromDB });
     })
     .catch();
 });
@@ -26,12 +26,42 @@ router.post("/new-movie", (req, res, next) => {
   const { title, genre, plot, cast } = req.body;
   const movieDetails = req.body;
 
+  //   const movieDetails = {
+  //       //const castArr = req.body.cast.split(",");
+  //   }
+
   Movie.create(movieDetails)
     .then(() => {
       res.redirect("/movies");
     })
     .catch((err) => {
       console.log("Error creating new movie..", err);
+    });
+});
+
+//==== Create route to movie details
+router.get("/:movieId", (req, res, next) => {
+  const movieId = req.params.movieId;
+
+  Movie.findById(movieId)
+    .then((movieDetails) => {
+      res.render("movies/movie-details", movieDetails);
+    })
+    .catch((err) => {
+      console.log("Error displaying movie..", err);
+    });
+});
+
+//====== Create route for movie delete page
+router.post("/:movieId/delete", (req, res, next) => {
+  const movieId = req.params.movieId;
+
+  Movie.findByIdAndDelete(movieId)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log("Error deleting movie", err);
     });
 });
 
