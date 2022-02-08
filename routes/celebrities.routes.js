3d734edf20b19bb7b72de714bd2d6ec845b56241
GJ <gjschoutsen@tutanota.com>
2022-02-08 17:22:11 +0100

    added route for form

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -6,4 +6,20 @@ router.get("/celebrities/create", (req, res, next) => {
     res.render("celebrities/new-celebrities")
 });
 
+router.post("/celebrities/create", (req, res, next) => {
+    const celebDetails = {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase
+    }
+     
+        Celebs
+            .create(celebDetails)
+            .then((celeb) => {
+                res.redirect("/celebrities")
+            })
+            .catch(err => res.render("celebrities/new-celebrities"));
+
+});
+
 module.exports = router;
