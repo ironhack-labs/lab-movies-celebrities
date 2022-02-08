8863ebe004bb20037808f10cb3eb012555451157
Bart Huiskamp <bartolameus.h@gmail.com>
2022-02-08 16:57:58 +0100

    iteration 2 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,16 @@
-//  Add your code here
+const { Schema, model, Mongoose } = require('mongoose');
+ 
+
+const CelebritySchema = new Schema(
+  {
+    Name: String,
+    Occupation: String,
+    CatchPhrase: String 
+  },
+  {
+    timestamps: true
+  }
+);
+
+ 
+module.exports = model('Celebrity', celebrityschema);
