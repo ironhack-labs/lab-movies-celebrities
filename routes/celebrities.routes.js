bad1159aca8f42f431ba2f058451e1033731e958
mgispert <mcg-511@hotmail.com>
2022-02-08 17:50:00 +0100

    finish iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,23 +3,31 @@ const Celebs = require("../models/Celebrity.model");
 //const Movie = require("../models/Author.model");
 
 router.get("/celebrities/create", (req, res, next) => {
-    res.render("celebrities/new-celebrities")
+  res.render("celebrities/new-celebrities");
 });
 
 router.post("/celebrities/create", (req, res, next) => {
-    const celebDetails = {
-        name: req.body.name,
-        occupation: req.body.occupation,
-        catchPhrase: req.body.catchPhrase
-    }
-     
-        Celebs
-            .create(celebDetails)
-            .then((celeb) => {
-                res.redirect("/celebrities")
-            })
-            .catch(err => res.render("celebrities/new-celebrities"));
+  const celebDetails = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
 
+  Celebs.create(celebDetails)
+    .then((celeb) => {
+      res.redirect("/");
+    })
+    .catch((err) => res.render("celebrities/new-celebrities"));
+});
+
+router.get("/celebrities/celebrities-list", (req, res, next) => {
+  Celebs.find()
+    .then((celebsArr) => {
+      res.render("celebrities/celebrities-list", { celebs: celebsArr });
+    })
+    .catch((err) => {
+      console.log("sorry", err);
+    });
 });
 
 module.exports = router;
