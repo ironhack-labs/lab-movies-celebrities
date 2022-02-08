e89a344932cc841c4ffedc5409e1ba09fb7df127
dorrrn <doan7tran@gmail.com>
2022-02-08 19:26:06 +0100

    Create route to movies/create

--- routes/movies.js
+++ routes/movies.js
@@ -1,4 +1,3 @@
-
 const router = require("express").Router();
 const Movie = require("../models/Movie.model");
 
@@ -11,5 +10,29 @@ router.get("/", (req, res, next) => {
     .catch();
 });
 
+//===== Create GET-route for /movies/new-movie
+router.get("/new-movie", (req, res, next) => {
+  Movie.find()
+    .then((movieDetails) => {
+      res.render("movies/new-movie", { movie: movieDetails });
+    })
+    .catch((err) => {
+      console.log("Error getting movie details from DB...", err);
+    });
+});
+
+// ===== Create POST-route for movies/new-movie submit page
+router.post("/new-movie", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  const movieDetails = req.body;
+
+  Movie.create(movieDetails)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log("Error creating new movie..", err);
+    });
+});
 
 module.exports = router;
