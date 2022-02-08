6aae67d31618ebc902e88cfc8648867138a3111c
GJ <gjschoutsen@tutanota.com>
2022-02-08 15:27:02 +0100

    added create view

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,4 +1,9 @@
 const router = require("express").Router();
+const Celebs = require("../models/Celebrity.model");
+//const Movie = require("../models/Author.model");
 
+router.get("/celebrities/create", (req, res, next) => {
+    res.render("celebrities/new-celebrities")
+});
 
 module.exports = router;
