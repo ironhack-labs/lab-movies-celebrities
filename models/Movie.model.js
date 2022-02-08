b79e3b9d08d956f2622fad4bd19e0458aaa09635
denisesenguel <denisesenguel@gmail.com>
2022-02-08 19:53:24 +0100

    iteration 5: added movie model with references

---
+++ models/Movie.model.js
@@ -0,0 +1,20 @@
+const {Schema, model} = require('mongoose');
+
+const movieSchema = new Schema(
+    {
+        title: String,
+        genre: String,
+        plot: String,
+        cast: Array(
+            {
+                type: Schema.Types.ObjectId, 
+                ref: "Celebrity"
+            }
+        )
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('Movie', movieSchema);
