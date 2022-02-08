770542cbf3b2e15ca29dbdaaaa820bcb0ff29755
GJ <gjschoutsen@tutanota.com>
2022-02-08 18:15:52 +0100

    iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,3 +1,28 @@
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+
+router.get("/movies/create", (req, res, next) => {
+
+    Movie.find()
+        .populate("celebs")
+        .then((celebArr) => {
+            res.render("movies/new-movie", {celebs: celebArr});    
+        })
+        .catch(err => consolelog("Error finding celebs", err))
+  });
+  
+  router.post("/movie/create", (req, res, next) => {
+    const celebDetails = {
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase,
+    };
+  
+    movie.create(celebDetails)
+      .then((celeb) => {
+        res.redirect("/");
+      })
+      .catch((err) => res.render("celebrities/new-celebrities"));
+  });
 
 module.exports = router;
