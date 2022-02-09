67fe32a37b8934a816238546cdf0846361074d8e
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-02-09 16:38:26 -0600

    Iteration 2 completed

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
 
+const celebritySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  occupation: {
+    type: String,
+    required: true,
+    default: "Unknown",
+  },
+  catchPhrase: {
+    type: String,
+    required: true,
+  },
+});
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+// 4. EXPORTACIÓN
+module.exports = Celebrity;
