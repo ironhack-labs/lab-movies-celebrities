521951d7497e9f7f5ef4792a5cee7dfe3bdbf040
Leonardo <mr_kunkel@hotmail.com>
2022-02-09 15:40:11 -0600

    Iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,13 @@
 
+
+const mongoose = require('mongoose');
+
+const celebritySchema = mongoose.Schema({
+    name: String,
+    occupation: String,
+    catchPhrase: String
+});
+
+const Celebrity = mongoose.model('Celebrity', celebritySchema);
+
+module.exports = Celebrity;
