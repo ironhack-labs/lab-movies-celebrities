b181d53a0ead93a1daf14153269bf0c47f76ba4b
Alan Cruz <alanchis@hotmail.com>
2022-02-09 15:59:54 -0600

    iteration 1 and 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,30 @@
-//  Add your code here
+
+// 1. IMPORTACIONES
+const mongoose = require("mongoose")
+
+
+// 2. SCHEMA
+const userSchema = mongoose.Schema({
+
+	name: {
+		type: String,
+		trim: true, // No puedes guardar en base de datos si mandas un dato con espacios en blanco
+		required: true
+	},
+    occupation: {
+		type: String,
+		trim: true, 
+		required: true
+	},
+    catchPhrase: {
+		type: String,
+		trim: true,
+		}
+    }, 	{timestamps: true}
+)
+
+// 3. MODEL
+const User = mongoose.model("User", userSchema)
+
+// 4. EXPORTACIÓN
+module.exports = User
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,19 @@
+
+const express		= require("express")
+const router = require("express").Router();
+
+//const authController		= require("./../controllers/authController")
+
+//const routeGuard			= require("./../middlewares/route-guard")
+
+// all your routes here
+
+
+
+
+
+
+
+
+
+module.exports = router;
