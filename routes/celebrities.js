729e366412fa2e57e9e2bc865e5f3a17ee765e85
Alexis Ocampo <al3xisoc4mpo@gmail.com>
2022-02-09 17:02:57 -0600

    Iteration 3 completed

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,3 +1,6 @@
+const async = require("hbs/lib/async");
+const Celebrity = require("../models/Celebrity.model");
+
 const router = require("express").Router();
 
 /* GET celebrities */
@@ -6,8 +9,30 @@ router.get("/", (req, res, next) => {
 });
 
 /* GET new celebrity */
-router.get("/new-celebrity", (req, res, next) => {
-  res.render("celebrities/new-celebrity");
+router.get("/new-celebrity", async (req, res, next) => {
+  try {
+    res.render("celebrities/new-celebrity");
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+/* POST new celebrity */
+router.post("/new-celebrity", async (req, res, next) => {
+  try {
+    //   console.log(req.body);
+    const { name, occupation, catchPhrase } = req.body;
+    // console.log(`name:${name},occupation:${occupation},catchPhrase:${catchPhrase}`)
+    const newCelebrity = await Celebrity.create({
+      name: name,
+      occupation: occupation,
+      catchPhrase: catchPhrase,
+    });
+    console.log(newCelebrity);
+    return res.redirect("/");
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 module.exports = router;
