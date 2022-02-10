ecb241dadbc1325638fcad083bb0eafa6bbb76b5
Leonardo <mr_kunkel@hotmail.com>
2022-02-10 11:44:30 -0600

    Iteration Bonus

--- controllers/celebrities.controller.js
+++ controllers/celebrities.controller.js
@@ -29,3 +29,33 @@ exports.createForm = async (req, res) => {
     }
 
 }
+
+exports.getCelebrity = async (req, res) => {
+    const { id } = req.params;
+    const foundCeleb = await Celebrity.findById(id);
+    res.render('celebrities/celebrity-detail', {celeb: foundCeleb});
+}
+
+exports.editCelebrity = async (req, res) => {
+    const { id } = req.params;
+    const foundCelebrity = await Celebrity.findById(id);
+    res.render('celebrities/edit-celebrity', {celebrity: foundCelebrity});
+}
+
+exports.editCelebrityForm = async (req, res) => {
+    const { id } = req.params;
+    const { name, occupation, catchPhrase } = req.body;
+    const updateCelebrity = await Celebrity.findByIdAndUpdate(
+        id,
+        { name, occupation, catchPhrase },
+        { new: true }
+    )
+    console.log(updateCelebrity);
+    return res.redirect('/celebrities')
+}
+
+exports.deleteCelebrity = async (req, res) => {
+    const { id } = req.params;
+    await Celebrity.findByIdAndDelete(id);
+    return res.redirect('/celebrities');
+}
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -8,4 +8,12 @@ router.get('/create', celebCtrl.create);
 
 router.post('/create', celebCtrl.createForm);
 
+router.get('/:id', celebCtrl.getCelebrity);
+
+router.get('/:id/edit', celebCtrl.editCelebrity);
+
+router.post('/:id/edit', celebCtrl.editCelebrityForm);
+
+router.post('/:id/delete', celebCtrl.deleteCelebrity);
+
 module.exports = router;
