fadaf6fda919d3cbc50d6f201d2d90c1814c01d3
ricomasgu <ricard.comas.gomez@gmail.com>
2022-02-12 13:51:52 +0100

    Iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,21 @@
 //  Add your code here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const celebritySchema = new Schema(
+    {
+        name: String,
+        occupation: {
+            type: String,
+            enum: [ "actor" , "singer" , "comedian" , "other" ]
+        },
+        catchPhrase: {
+            type: String,
+            required: true
+        }
+    }
+);
+
+const Celebrity = mongoose.model('Celebrity', celebritySchema);
+ 
+module.exports = Celebrity;
