6186279daa2d694396e840084f0ca9a083176858
ricomasgu <ricard.comas.gomez@gmail.com>
2022-02-12 14:41:30 +0100

    Iteration 3 and 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,34 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model');
 
 // all your routes here
+router.get('/celebrities', (res, req, next) => {
+    Celebrity.find()
+            .then(resFromDB => {
+                console.log("Celebrities retrieved", resFromDB);
+                req.render('celebrities/celebrities', { celebrities: resFromDB } );
+            })
+            .catch(err => {
+                console.log("Error while finding the Celebrites", err);
+            });
+});
+
+router.get('/celebrities/create', (res , req , next) => {
+    req.render('celebrities/new-celebrity');
+});
+
+router.post('/celebrities/create', (res , req , next) => {
+    const { name, occupation, catchPhrase } = res.body;
+    Celebrity.create({ name, occupation, catchPhrase })
+        .then( resFromDB => {
+            console.log("Celebrity created", resFromDB);
+            req.redirect('/celebrities');
+        })
+        .catch(err => {
+            console.log("Error while creating a new Celebrity", err);
+            req.redirect('/celebrities/new-celebrity?error=true');
+        });
+});
 
 module.exports = router;
