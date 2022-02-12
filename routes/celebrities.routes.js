93a847c239e693fbc7481d41f96323b7895bd887
guilhermefill <guilhermefill@gmail.com>
2022-02-12 16:43:39 +0100

    iteraton 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,19 @@
 const router = require('express').Router();
+const Celebrity = require('../models/Celebrity.model')
 
+router.get('/create', async (req, res) => {
+    res.render('celebrities/new-celebrity');
+});
 
+router.post('/create', async (req, res) => {
+    try {
+        const { name, occupation, catchPhrase } = req.body;
+        const newCelebrity = await Celebrity.create({ name, occupation, catchPhrase });
+        res.redirect('/celebrities')
+    } catch (err) {
+        console.error(err);
+    }
+});
 
 
 
