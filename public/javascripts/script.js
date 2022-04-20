5617d59c484ff0546157d5184c4c669c76857cf9
Andrew Lamagna <andrew.lamagna@LEGO.com>
2022-04-20 11:03:51 -0400

    Tweaking nav detect pg

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,18 +1,30 @@
 document.addEventListener(
   "DOMContentLoaded",
   () => {
-
-    const curPage = window.location.pathname;
-    if(curPage.substring(1,12) == 'celebrities'){
-      document.querySelector("#nav-celeb").classList.toggle('active');
-    }else if(curPage.substring(1,7) == 'movies'){
-      document.querySelector("#nav-movie").classList.toggle('active');
-    }else if(curPage == '/' ){
-      document.querySelector("#nav-home").classList.toggle('active');
-    }
-  
+    setCurrentPg();
   },
   false
-
-
 );
+
+function setCurrentPg(){
+  let ele = 'navbar-brand'; //placeholder
+  const onPage = window.location.pathname;
+  if(onPage.includes('movie')){
+    if(onPage.includes('create')){
+      document.getElementById('nav-drop').classList.toggle('active');
+      ele = 'nav-drop-movie';
+    }else
+      ele = 'nav-movie';
+  }else if(onPage.includes('celeb')){
+    if(onPage.includes('create')){
+      document.getElementById('nav-drop').classList.toggle('active');
+      ele = 'nav-drop-celeb';
+    }else
+      ele = 'nav-celeb';
+  }else{
+      ele = 'nav-home';
+  }
+  document.getElementById(ele).classList.toggle('active');
+  document.getElementById(ele).innerHTML += `<span class="sr-only">(current)</span>`;
+  return true;
+}
