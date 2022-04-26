ebdd3dd2ccc99ad04c88aa3567b51a21b48790de
PJPalma <102538053+PJPalma@users.noreply.github.com>
2022-04-26 18:29:11 +0100

    first commit

--- routes/index.js
+++ routes/index.js
@@ -5,4 +5,18 @@ router.get("/", (req, res, next) => {
   res.render("index");
 });
 
+router.use('/celebrities', require('./celebrities.routes'))
+
+router.use('/movies', require('./movies.routes'))
+
+router.get('/create', (req,res) => {
+  Celebrity
+      .find()
+      .then(celebrities => {
+          res.render('movies/new-movie', {celebrities})
+      })
+      .catch(err => console.log(err))
+})
+
+
 module.exports = router;
