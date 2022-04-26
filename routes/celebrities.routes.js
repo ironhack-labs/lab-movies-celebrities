c56839b7945dd04052cbd4a15c9367850ae3c589
jidekickpush <jeandenisdrane@gmail.com>
2022-04-26 19:19:52 +0200

    Iteration 2 create celebrity model and link in the route

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,6 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model")
 
 // all your routes here
 
