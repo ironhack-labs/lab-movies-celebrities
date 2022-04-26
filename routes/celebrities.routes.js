37450973d19351eb7c7d27bcc1a14029fdec5fd2
jidekickpush <jeandenisdrane@gmail.com>
2022-04-26 20:10:04 +0200

    Iteration 3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,6 +2,33 @@
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model")
 
-// all your routes here
+// 
+// GET route to display the form for new celebrity => Show a form to create a celebrity
+router.get('/celebrities/create', (req, res) => {
+    res.render('celebrities/new-celebrity')
+});
+
+//POST route to save a new celebrity to the database in the celebrities collection
+router.post('/celebrities/create', (req, res, next) => {
+
+    const newCelebrity = {
+        name :req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase,
+    }
+
+    Celebrity.create(newCelebrity)
+        .then(newCelebrity=>{
+            console.log("new celebrity created into the DB as:", newCelebrity.name)
+            res.redirect("/celebrities/celebrities")
+        })
+        .catch(err=>{
+            ("Something went wrong creating the newCelebrity into the DB",err)
+            res.redirect("/celebrities/new-celebrity")
+            next(err);
+        })
+  });
+
+
 
 module.exports = router;
