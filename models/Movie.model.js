e95e1764c53fc89d80fbe4703f022c2f18d5b39f
Marija Strahinjic <marija.strahinjic@hotmail.com>
2022-04-26 20:15:26 +0200

    create movie model

---
+++ models/Movie.model.js
@@ -0,0 +1,15 @@
+const {Schema, model} = require("mongoose");
+
+const movieSchema = new Schema(
+    {
+        title: String,
+    genre: String,
+    plot: String,
+    cast: [String]
+    }
+);
+
+
+const Movie = model("Movie", movieSchema);
+
+module.exports = Movie;
