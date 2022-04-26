03576d7cd86f7c87ae6994772e25f050bfaea941
jidekickpush <jeandenisdrane@gmail.com>
2022-04-26 21:34:10 +0200

    Create Movie model

---
+++ models/Movie.model.js
@@ -0,0 +1,23 @@
+//  Add your code here
+
+const { default: mongoose } = require("mongoose");
+const {Schema, model} = require("mongoose");
+
+const movieSchema = new Schema(
+    {
+        title: {
+            type:String,
+            required: true,
+        },
+        genre: String,
+        plot: String,
+        cast: [{
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Celebrity"
+        }]
+    }
+);
+
+const Movie = model("Celebrity", movieSchema);
+
+module.exports = Movie;
