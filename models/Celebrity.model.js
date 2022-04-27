217c29b9c86342e902e8ff1046971fe62ba8be37
Georgina Aceves <aceves.georgina@hotmail.com>
2022-04-27 15:45:03 +0200

    done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,11 @@
-//  Add your code here
+const { Schema, model } = require("mongoose")
+
+const CelebritySchema = new Schema({
+    name: String,
+    occupation: String,
+    catchPhrase: String
+}, { timestamps: true })
+
+
+const modelo = model("Celebrity", CelebritySchema);
+module.exports = modelo;
