ac77805dc663051cd1e6efddf035c56524d60acc
Emmanuel Alejandro Borrayo Cortes <alexborrayo21@hotmail.com>
2022-04-27 16:21:04 -0500

    Done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,15 @@
 //  Add your code here
+
+const { Schema, model } = require("mongoose")
+
+const CelebritySchema = new Schema({
+    name: String,
+    occupation: String,
+    catchPhrase: String
+}, { timestamps: true })
+
+
+const modelo = model("Celebrity", CelebritySchema);
+module.exports = modelo;
+
+//Lo de encima es lo mismo que  module.exports = model("Celebrity", CelebritySchema);
