909695cd63ea7bc657bd6bc37d81a5c425c0cd14
Catarina Salvado <catarinapsalvado@gmail.com>
2022-05-10 19:51:43 +0100

    done until 7

---
+++ routes/movies.routes.js
@@ -0,0 +1,32 @@
+
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
+
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("movies/new-movie", { celebrities });
+    })
+    .catch((err) => res.redirect("/movies/create"));
+});
+
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.create({ title, genre, plot, cast })
+    .then((mov) => res.redirect("/movies"))
+    .catch((err) => res.redirect("/movies/create"));
+});
+
+router.get("/movies" ,
+  (req, res, next) => {
+    const { title, genre, plot, cast } = req.body;
+    Movie.find()
+      .then((movies) => {
+        res.render("movies/movies", { movies });
+      })
+      .catch((err) => next(err));
+  });
+
+module.exports = router;
