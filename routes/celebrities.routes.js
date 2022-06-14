c432a900c8ccd752245cbd4df8cd389d685ae1d5
Vale Quiroga <valentinqramos@outlook.com>
2022-06-14 12:00:03 -0500

    Iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,21 +1,30 @@
 const router = require('express').Router();
 const Celebrity = require('../models/Celebrity.model');
 
-
+// ---create---
 router.get('/celebrities/create', (req, res, next) => {
   res.render('../views/celebrities/new-celebrity.hbs')
 })
-
+//---create---
 router.post('/celebrities/create',(req, res, next) => {
   const {...allInfo} = req.body
-
   Celebrity.create(allInfo)
     .then(() => res.redirect('/celebrities'))
-    .catch(err => {
-      console.log('Error while creating a celebrity:',err)
+    .catch(err => {console.log('Error while creating: ',err)
       next()
     })
 })
+//---read---
+router.get('/celebrities',(req, res, next) =>{
+  Celebrity.find()
+    .then(celebrities =>{
+      res.render('../views/celebrities/celebrities.hbs',{celebrities})
+    })
+    .catch(err => {console.log('Error while rendering: ', err)
+      next()
+    })
+})
+
 
 
 
