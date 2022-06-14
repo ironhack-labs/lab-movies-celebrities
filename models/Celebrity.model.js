daf67077abff7ac4d44c9625fba4c39632f945d6
Vale Quiroga <valentinqramos@outlook.com>
2022-06-14 10:09:26 -0500

    Iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,5 @@
-//  Add your code here
+const {Schema, model} =  require('mongooose')
+
+const celebritySchema = new Schema({ name:String, occupation:String, catchPhrase:String });
+
+module.exports = model('Celebrity', celebritySchema);
