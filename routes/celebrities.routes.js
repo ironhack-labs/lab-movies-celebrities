87be1cf23707e3be5b86696bdc778778922f2701
Vale Quiroga <valentinqramos@outlook.com>
2022-06-15 14:53:15 -0500

    bonus

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 const Celebrity = require('../models/Celebrity.model');
 
-
+//create
 router.get('/celebrities/create', (req, res, next) => {
-  res.render('../views/celebrities/new-celebrity.hbs')
+  res.render('celebrities/new-celebrity')
 })
-
+//create
 router.post('/celebrities/create',(req, res, next) => {
   const {...allInfo} = req.body
   Celebrity.create(allInfo)
@@ -14,15 +14,54 @@ router.post('/celebrities/create',(req, res, next) => {
     next()
   })
 })
-
+//read
 router.get('/celebrities',(req, res, next) =>{
   Celebrity.find()
-    .then(celebrities =>{
-      res.render('../views/celebrities/celebrities.hbs',{celebrities})
-    })
+    .then(celebrities => res.render('celebrities/celebrities',{celebrities}))
     .catch(err => {console.log('Error while rendering: ', err)
     next()
   })
+})
+//read
+router.get('/celebrities/:id',(req, res, next) => {
+  const {id} = req.params
+  Celebrity.findById(id)
+    .then(celebrity =>res.render('celebrities/celebrity-detail',{celebrity}))
+    .catch(err => {
+      console.log('Error ',err)
+      next()
+    })
+})
+//update
+router.get('/celebrities/:id/edit',(req, res, next) => {
+  const {id} = req.params
+  Celebrity.findById(id)
+    .then(celebrityEdit => res.render('celebrities/edit-celebrity',{celebrity: celebrityEdit}))
+    .catch(err => {
+      console.log(`Error`, err)
+      next()
+    })
+})
+//update
+router.post('/celebrities/:id/edit',(req, res, next) => {
+  const {id} = req.params
+  const {...allInfo} = req.body
+  Celebrity.findByIdAndUpdate(id, allInfo)
+    .then(() => res.redirect(`/celebrities/${id}`))
+    .catch(err =>{
+      console.log(`Error while editing celebrity`,err)
+      next()
+    })
+})
+//delete
+router.post('/celebrities/:id/delete',(req, res, next) =>{
+  const {id} = req.params
+  Celebrity.findByIdAndDelete(id)
+    .then(() => res.redirect('/celebrities'))
+    .catch(error => {
+      console.log('Error while deleting:',error)
+      next()
+    })
 })
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,14 +2,14 @@ const router = require('express').Router()
 const Celebrity = require('../models/Celebrity.model')
 const Movie = require('../models/Movie.model')
 
-
+//create
 router.get('/movies/create',(req, res, next) => {
   Celebrity.find()
-    .then(celebrityInfo => {
-      res.render('../views/movies/new-movie.hbs',{celebrityInfo})
-    })
+    .then(celebrityInfo => res.render('movies/new-movie',{celebrityInfo}))
+    .catch(err => {console.log('Error while creatin Movie',err) 
+      next()})
 })
-
+//create
 router.post('/movies/create',(req, res, next) => {
   const {...movieInfo} = req.body
   Movie.create(movieInfo)
@@ -18,30 +18,28 @@ router.post('/movies/create',(req, res, next) => {
     next()
   })
 })
-
+//read
 router.get('/movies',(req, res, next)=>{
   Movie.find()
-    .then(movies =>{
-      res.render('../views/movies/movies.hbs',{movies})
-    })
+    .then(movies => res.render('movies/movies',{movies}))
     .catch(err => {console.log('Error while rendering movies',err)
     next()
   })
 })
-
+//read
 router.get('/movies/:movieId',(req, res, next) =>{
   const {movieId} = req.params
   Movie.findById(movieId)
     .populate('cast')
     .then(theMovie => {
-      res.render('../views/movies/movie-details.hbs',theMovie)
+      res.render('movies/movie-details',theMovie)
     })
     .catch(err => {
       console.log('Error while rendering details:',err)
       next()
     })
 })
-
+//delete
 router.post('/movies/:id/delete',(req, res, next) =>{
   const {id} = req.params
   Movie.findByIdAndDelete(id)
@@ -51,21 +49,19 @@ router.post('/movies/:id/delete',(req, res, next) =>{
       next()
     })
 })
-
+//update
 router.get('/movies/:id/edit',(req, res, next) =>{
   const {id} = req.params
   Movie.findById(id)
     .then(movieData => {Celebrity.find()
-        .then(celebData => {
-          res.render('../views/movies/edit-movie.hbs', {movieData, celebData})
-        })
+        .then(celebData => res.render('movies/edit-movie', {movieData, celebData}))
         .catch(error => {
           console.log('Error while editing',error)
           next()
         })
     })
 })
-
+//update
 router.post('/movies/:id/edit',(req, res, next) => {
   const {id} = req.params
   const {name, genre, plot, cast} = req.body
@@ -76,8 +72,6 @@ router.post('/movies/:id/edit',(req, res, next) => {
     })
     .catch(error => {console.log(error)})
 })
-
-
 
 module.exports = router;
 
