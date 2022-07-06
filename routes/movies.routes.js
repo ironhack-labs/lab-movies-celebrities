a6366042c8d393592d60fbf5873434654a58f3fb
Val Mejia <valemegaestrellalectora@gmail.com>
2022-07-06 10:43:07 -0500

    mas cambios

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,24 +1,63 @@
+
 const router = require("express").Router();
-const Movie = require("../models/Movies.models")
 
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require('../models/Movies.model')
 
-router.get("/movies/create", (req, res) => {
-    res.render('movies/new-movie')
+router.get('/movies/create', (req, res) => {  
+
+    Celebrity
+    .find()
+    .then((celebrities) => {
+        res.render ('movies/new-movie', { celebrities })
+        })
+        .catch(err => console.log(err))
 })
 
 router.post('/movies/create', (req, res) => {
-   const {title, } = req.body
-   Movie
-    .create({name, occupation, catchPhrase})
-    .then(movie => res.redirect('/movies'))
-    .catch(err => res.redirect('/movies/redirect'))
+
+    const { title, genre, plot, cast } =  req.body
+
+    Movie
+        .create({ title, genre, plot, cast })
+        .then(() => res.redirect(`/movies`))
+        .catch(err => {
+            console.log(err)
+            res.redirect('create')
+        })
 })
 
 router.get('/movies', (req, res) => {
+
+        Movie
+        .find()
+        .then(movies => {
+            res.render('movies/movies', { movies })
+        })
+        .catch(err => console.log(err))
+})
+
+router.get('/movies/:id', (req, res) => {
+
+    const { id } = req.params
+
     Movie
-    .find()
-    .then(movie => res.render('movies/movies', {movie}))
-    .catch(err => console.log(err))
+        .findById(id)
+        .populate('cast') 
+        .then(movie => {
+            res.render('movies/movie-details', movie)
+        })
+        .catch(err => console.log(err))
+})
+
+router.post('/movies/:id/delete', (req, res) => {
+
+    const { id } = req.params
+
+    Movie
+        .findByIdAndDelete(id)
+        .then(() => res.redirect(`/movies`))
+        .catch(err => console.log(err))
 })
 
 module.exports = router;
