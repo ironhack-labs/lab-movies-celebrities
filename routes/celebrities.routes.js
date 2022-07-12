c5b4f1e84484e18fb2edd7ded55f976fcd1dae85
Christoph86 <ChrstphBch@web.de>
2022-07-12 16:58:31 +0200

    add celebrity routes

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,4 +3,14 @@ const router = require("express").Router();
 
 // all your routes here
 
+// CREATE: Render form
+router.get("/celebrities/create", (req, res) => {
+    res.render("celebrities/new-celebrity"); 
+})
+  
+// CREATE: Process form
+router.post("/celebrities/create", (req, res) => {
+    res.send("i am the post to create a new celbrity")
+  })
+
 module.exports = router;
