889cd271866ce00e4e156db2da0d3ed737ba8186
Tobias Conrad <max.tobias.conrad@gmail.com>
2022-07-13 10:16:17 +0200

    lab done to it 9

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -14,16 +14,71 @@ router.get("/movies/create", (req, res) => {
     })
 })
 
-// router.post("/movies/create", (req, res) => {
-//     const newMovie = {
-//         title: req.body.title,
-//         genre: req.body.genre,
-//         plot: req.body.plot,
+router.post("/movies/create", (req, res) => {
+    console.log(req.body)
 
-//     }
-// })
+    const newMovie = {
+        title: req.body.title,
+        genre: req.body.genre,
+        plot: req.body.plot,
+        cast: req.body.cast
+    }
+    Movie.create(newMovie)
+    .then( () => {
+        res.redirect("/movies")
+    })
+})
 
+router.get("/movies", (req, res) => {
+    Movie.find()
+    .populate("cast")
+    .then( (moviesFromDB) => {
+        const moviesList = {
+            movies: moviesFromDB
+        }
+        console.log(moviesList)
+        res.render("movies/movies", moviesList)
+    })   
+})
 
+router.get("/movies/:id", (req, res) => {
+    const id = req.params.id
+    console.log(id)
+    Movie.findById(id)
+    .populate("cast")
+    .then( (foundMovie) => {
+        const movieFromApi = {
+                movie: foundMovie
+        }
+        console.log(movieFromApi)
+        res.render("movies/movie-details", movieFromApi)
+    })
+    .then( () => {
+        console.log("Movie found and displayed")
+    })
+    .catch( (err) => {
 
+        console.log("Error occurred while displaying movie details", err)
+    })
+}) 
+
+router.post("/movies/:id/delete", (req, res) => {
+    const id = req.params.id
+    Movie.findByIdAndDelete(id)
+    .then( () => {
+        res.redirect("/movies")
+    })
+    .then( (err) => {
+        console.log(err)
+    })
+})
+
+router.get("/movies/:id/edit", (req, res) => {
+    const id = req.params.id
+    Movie.findById(id)
+    .then ((foundMovie) => {
+        Celebrity.find()
+    })
+})
 
 module.exports = router;
