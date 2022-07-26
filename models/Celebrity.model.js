ad480860c1529a76fb1ad2ae7b20a5b48ed00004
violeta atkinson <violeta.atkinson@gmail.com>
2022-07-26 21:26:53 +0200

    iteration 1 & 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,18 @@
 //  Add your code here
+const mongoose = require('mongoose');
+
+const celebrity = new mongoose.Schema({
+    name:{
+        type: String,
+    },
+    occupation:{
+        type: String,
+    },
+    catchPhrase:{
+        type: String,  
+    }
+})
+
+const Celebrity = mongoose.model('Celebrity', CelebritySchema);
+
+module.exports = Celebrity;
