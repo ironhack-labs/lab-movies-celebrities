3851cd8265d15963f9e5f0413d200087e347759e
Kira Soba <kira.soba@gmx.de>
2022-08-07 19:22:35 +0200

    Iteration 6 - Done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
+const movieModel = require("../models/Movie.model");
+const celebrityModel = require("../models/Celebrity.model");
 
 // all your routes here
+router.get("/create", (req, res) => {
+  celebrityModel
+    .find()
+    .then((celebArr) => {
+      res.render("movies/new-movie", { celebArr });
+    })
+    .catch((err) => console.log("Ups, that didn't work", err));
+});
+
+router.post("/create", (req, res) => {
+  console.log(req.body);
+  movieModel
+    .create(req.body)
+    .then(() => res.redirect("/movies"))
+    .catch((err) => console.log("Oh, adding a movie did not work.", err));
+});
 
 module.exports = router;
