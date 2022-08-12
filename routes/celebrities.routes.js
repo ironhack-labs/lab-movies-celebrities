6b6d1ca2d4ad7082d03aa006fd5f0c59c49da5f0
tiffany_chang <changyunhsuan.fr@gmail.com>
2022-08-12 23:22:29 +0200

    finish iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,6 +2,16 @@ const router = require("express").Router();
 const CelebrityModel = require("../models/Celebrity.model");
 
 // all your routes here
+// iteration 4: Listing all celebrities
+router.get("/", (req, res) => {
+  CelebrityModel.find()
+    .then((allCelebrities) =>
+      res.render("celebrities/celebrities", { allCelebrities })
+    )
+    .catch((err) => console.log(err));
+});
+
+// iteration 3: Create a new celebrity
 router.get("/create", (req, res) => {
   res.render("celebrities/new-celebrity");
 });
