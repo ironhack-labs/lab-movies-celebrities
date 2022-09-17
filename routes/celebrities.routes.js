59d6d9529ac3000ba9aec379ba6adf9177ef12f1
hvkst <bernd@hvkst.de>
2022-09-17 21:20:34 +0200

    Iteration 10

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,28 +1,30 @@
 const router = require('express').Router();
 const { Celebrity } = require('../models/Celebrity.model');
 
-// all your routes here
-
-router.get('/celebrities', (req, res, next) => {
-  Celebrity.find().then((data) => {
-    res.render('celebrities/celebrities', { celeb: data });
-  });
-});
-
+// Iteration #3
+// Show celebrities form
 router.get('/celebrities/create', (req, res, next) => {
   res.render('celebrities/new-celebrity');
 });
 
+// Create  celebrities
 router.post('/celebrities/create', (req, res, next) => {
   Celebrity.create(req.body)
     .then(() => {
       res.redirect('/celebrities');
-      console.log('celeb created!!!');
     })
     .catch((err) => {
       console.log(err);
       res.redirect('/celebrities/create');
     });
+});
+
+// Iteration #4
+// Listing all celebrities
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find().then((data) => {
+    res.render('celebrities/celebrities', { celeb: data });
+  });
 });
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,25 +3,13 @@ const { Celebrity } = require('../models/Celebrity.model');
 const { Movie } = require('../models/Movie.model');
 
 // all your routes here
-router.get('/movies', (req, res, next) => {
-  Movie.find({}, 'title').then((data) => {
-    // console.log(data);
-    res.render('movies/movies', { movies: data });
-  });
-});
 
+// Iteration 6
+// Show movies form
 router.get('/movies/create', (req, res, next) => {
-  Celebrity.find({}, 'name _id').then((data) => {
-    console.log(data);
-    res.render('movies/new-movie', { celebrities: data });
-  });
-});
-
-router.post('/movies/create', (req, res, next) => {
-  Movie.create(req.body)
-    .then(() => {
-      res.redirect('/movies');
-      console.log('Movie created!!!');
+  Celebrity.find({}, 'name _id')
+    .then((data) => {
+      res.render('movies/new-movie', { celebrities: data });
     })
     .catch((err) => {
       console.log(err);
@@ -29,11 +17,31 @@ router.post('/movies/create', (req, res, next) => {
     });
 });
 
+router.post('/movies/create', (req, res, next) => {
+  Movie.create(req.body)
+    .then(() => {
+      res.redirect('/movies');
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/movies/create');
+    });
+});
+
+// Iteration 7
+// Listing all movies
+router.get('/movies', (req, res, next) => {
+  Movie.find({}, 'title').then((data) => {
+    res.render('movies/movies', { movies: data });
+  });
+});
+
+// Iteration 8
+// Movies detail page
 router.get('/movies/:id', (req, res, next) => {
   Movie.findById(req.params.id)
     .populate('cast')
     .then((data) => {
-      console.log(data);
       res.render('movies/movie-detail', { movies: data });
     })
     .catch((err) => {
@@ -42,6 +50,8 @@ router.get('/movies/:id', (req, res, next) => {
     });
 });
 
+// Iteration 9
+// Delete movies
 router.post('/movies/:id/delete', (req, res, next) => {
   Movie.findByIdAndDelete(req.params.id)
     .then(() => {
@@ -53,13 +63,34 @@ router.post('/movies/:id/delete', (req, res, next) => {
     });
 });
 
-module.exports = router;
+// Iteration 10
+// Edit movies
+router.get('/movies/:id/edit', async (req, res, next) => {
+  const movieData = await Movie.findById(req.params.id).populate('cast', '_id name'); // <option value="second" selected>Second Value</option>
+  const celebrityData = await Celebrity.find({}, '_id name');
 
-// router.get('/movies', (req, res, next) => {
-//   Movie.find()
-//     .populate('cast')
-//     .then((data) => {
-//       console.log(data);
-//       res.render('movies/movies', { movies: data });
-//     });
-// });
+  // This was painful ;D
+  // https://stackoverflow.com/questions/34901593/how-to-filter-an-array-from-all-elements-of-another-array/34901916#34901916
+  const filteredArray = celebrityData.filter((celebrityData_el) => {
+    return (
+      movieData.cast.filter((movieData_el) => {
+        return movieData_el.name == celebrityData_el.name;
+      }).length == 0
+    );
+  });
+
+  res.render('movies/edit-movie', { movie: movieData, celebrities: filteredArray });
+});
+
+router.post('/movies/:id', (req, res, next) => {
+  Movie.findByIdAndUpdate(req.params.id, req.body)
+    .then(() => {
+      res.redirect('/movies');
+    })
+    .catch((err) => {
+      console.log(err);
+      // res.redirect('/movies');
+    });
+});
+
+module.exports = router;
