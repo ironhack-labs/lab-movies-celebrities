3d106c5ff186c9b77b49651d2ff3004dfcf264b3
hvkst <bernd@hvkst.de>
2022-09-17 13:14:46 +0200

    Iteration 1

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,19 @@
-const router = require("express").Router();
+const router = require('express').Router();
 
 /* GET home page */
-router.get("/", (req, res, next) => {
-  res.render("index");
+router.get('/', (req, res, next) => {
+  res.render('index');
 });
+
+// I don´t have to do this, when I do it in app.js? -> Google
+/* GET celebrities */
+// router.get('/celebrities', (req, res, next) => {
+//   res.render('celebrities');
+// });
+
+// /* GET movies */
+// router.get('/movies', (req, res, next) => {
+//   res.render('movies');
+// });
 
 module.exports = router;
