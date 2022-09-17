e1a87004723af46f64a41182c0f6dd9399e4b38e
hvkst <bernd@hvkst.de>
2022-09-17 17:16:38 +0200

    Iteration 8

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,15 +4,13 @@ const { Movie } = require('../models/Movie.model');
 
 // all your routes here
 router.get('/movies', (req, res, next) => {
-  Movie.find()
-    .populate('cast')
-    .then((data) => {
-      console.log(data);
-      res.render('movies/movies', { movies: data });
-    });
+  Movie.find({}, 'title').then((data) => {
+    // console.log(data);
+    res.render('movies/movies', { movies: data });
+  });
 });
 
-router.get('/movies/create', async (req, res, next) => {
+router.get('/movies/create', (req, res, next) => {
   Celebrity.find({}, 'name _id').then((data) => {
     console.log(data);
     res.render('movies/new-movie', { celebrities: data });
@@ -31,4 +29,26 @@ router.post('/movies/create', (req, res, next) => {
     });
 });
 
+router.get('/movies/:id', (req, res, next) => {
+  Movie.findById(req.params.id)
+    .populate('cast')
+    .then((data) => {
+      console.log(data);
+      res.render('movies/movie-detail', { movies: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/movies/create');
+    });
+});
+
 module.exports = router;
+
+// router.get('/movies', (req, res, next) => {
+//   Movie.find()
+//     .populate('cast')
+//     .then((data) => {
+//       console.log(data);
+//       res.render('movies/movies', { movies: data });
+//     });
+// });
