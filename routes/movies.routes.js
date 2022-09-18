a7910096202f86affc033f382e44c0a091f3410e
jdlms <lamorey.joshua@gmail.com>
2022-09-18 17:25:56 +0200

    sunday push

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,32 +2,40 @@ const express = require("express");
 const router = express.Router();
 
 const { Movie } = require("../models/Movie.model");
+const { Celeb } = require("../models/Celebrity.model");
 
-router.get('movies')
+router.get("/movies/create", async (req, res) => {
+  try {
+    const celebs = await Celeb.find({}, { name: 1 });
+    console.log(celebs);
+    res.render("movies/new-movie", { celebs });
+  } catch (error) {
+    res.render("error");
+  }
+});
 
-// const { Celeb } = require("../models/Celebrity.model");
+router.post("/movies/create", async (req, res) => {
+  try {
+    const newMovie = new Movie({ ...req.body });
+    await newMovie.save();
+    res.redirect("/movies/movies");
+  } catch (error) {
+    res.render("error");
+  }
+});
 
-// router.get("/celebrities/create", (req, res) => {
-//   res.render("celebrities/new-celebrity");
-// });
+router.get("/movies/movies", (req, res) => {
+  res.render("movies/movies");
+});
 
 // router.post("/celebrities/create", async (req, res) => {
-//   try {
-//     const newCeleb = new Celeb({ ...req.body });
-//     await newCeleb.save();
-//     res.redirect("/celebrities");
-//   } catch (error) {
-//     res.render("error");
-//   }
-// });
-
-// router.get("/celebrities", async (req, res) => {
-//   try {
-//     const celebrities = await Celeb.find({});
-//     res.render("celebrities/celebrities", { celebrities });
-//   } catch (error) {
-//     res.render("error");
-//   }
-// });
+//     try {
+//       const newCeleb = new Celeb({ ...req.body });
+//       await newCeleb.save();
+//       res.redirect("/celebrities");
+//     } catch (error) {
+//       res.render("error");
+//     }
+//   });
 
 module.exports = router;
