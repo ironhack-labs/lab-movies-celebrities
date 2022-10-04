dae35757b7c74348139b26b7f34250a2faf7652e
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-04 18:04:42 +0200

    iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,26 +1,39 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-const Celebrity = require("../models/Celebrity.model")
+const Celebrity = require("../models/Celebrity.model");
 
 router.get("/celebrities/create", (req, res, next) => {
-    res.render("celebrities/new-celebtity")
+  res.render("celebrities/new-celebrity");
+});
 
-})
-
-/*router.post("/celebrities/create", (req, res, nex) => {
-    const newCelebrity = {
-        name: req.body.name,
-        occupation: req.body.occupation,
-        catchPhrase: req.body.catchPhrase
-    }
-    Celebrity.create(newCelebrity)
-    .then( () => {
-        res.redirect("celebrities/celebrities")
-
+router.post("/celebrities/create", (req, res, nex) => {
+  const newCelebrity = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
+  Celebrity.create(newCelebrity)
+    .then((response) => {
+      res.redirect("/celebrities");
     })
-    .catch(()=>{
-        res.render("celebrities/new-celebtity")
-    })
-}) */
+    .catch((err) => {
+      console.log("There was an error creating new celebrity", err);
+      res.render("celebrities/new-celebrity");
+      next();
+    });
+});
 
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("celebrities/celebrities", { celebrities });
+    })
+    .catch((err) => {
+      console.log(
+        "There was an error getting all the celebrities from DB",
+        err
+      );
+      next();
+    });
+});
 module.exports = router;
