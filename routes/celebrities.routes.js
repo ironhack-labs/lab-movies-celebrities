7f0ef1473ab11e7f9448db302b93bd7c73019268
Marc Logan <marclogan90@gmail.com>
2022-10-04 17:18:18 +0200

    finished iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,4 +2,29 @@ const Celebrity = require("../models/Celebrity.model");
 
 const router = require("express").Router();
 
+//CREATE: display form
+router.get("/celebrities/create", (req, res, next) => {
+    res.render("celebrities/new-celebrity");
+})
+
+//CREATE: process form
+router.post("/celebrities/create", (req, res, next) => {
+  const celebritiesDetails = {
+      name: req.body.name,
+      occupation: req.body.occupation,
+      catchPhrase: req.body.catchPhrase,
+  }
+
+  Celebrity.create(celebritiesDetails)
+  .then( celebritiesDetails => {
+      res.redirect("/celebrities")
+  })
+  .catch(err => {
+      console.log("error creating new celebrity in DB", err);
+      next();
+  })
+
+})
+
+
 module.exports = router;
