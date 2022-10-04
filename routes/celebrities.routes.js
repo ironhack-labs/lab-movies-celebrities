ee4ea39fbe0a4586c9fa5448b7920a403b5df020
Julia Spriggs <ironhack@julia-spriggs.fr>
2022-10-04 21:39:13 +0200

    finished iteration 2

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,4 +1,6 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const Celebrity = require('../models/Celebrity.model');
+
 const router = require("express").Router();
 
 // all your routes here
