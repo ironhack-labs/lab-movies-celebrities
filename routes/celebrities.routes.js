54c5135582392fc78ca8067c924d802ae17d9518
Saskia Peach <saskiapeach@gmail.com>
2022-10-04 16:24:47 +0100

    complete iterations 1,2,3

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,32 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const Celebrity = require("../models/Celebrity.model");
+const router = require("express").Router();
+
+// all your routes here
+router.get("/celebrities/create", (req,res,next)=> {
+res.render("celebrities/new-celebrity");
+})
+
+router.post("/celebrities/create", (req,res,next)=> {
+ const celebrityDetails = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchphrase: req.body.catchphrase,
+ }
+
+ Celebrity.create(celebrityDetails)
+ .then(()=> {res.redirect("/celebrities")
+ })
+ .catch((err)=> {res.redirect("/celebrities/new-celebrity")})
+
+})
+
+
+
+
+
+
+
+
+
+module.exports = router;
