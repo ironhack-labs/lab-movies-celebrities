6278f93c86be00c788a63fe765693773a72b1db9
julianBrandon <p0lterg3istt@gmail.com>
2022-10-04 17:46:12 +0200

    Add iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,9 +3,26 @@ const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+
+//READ: List all celebs
+router.get("/celebrities", (req, res, next) => {
+    Celebrity.find()
+    .then(celebsFromDB => {
+        res.render("celebrities/celebrities", {celebrities: celebsFromDB});
+    })
+    .catch((err) => {
+        console.log("Error getting authors from DB...", err);
+        next(err);
+      });
+})
+
+//Create a Celebrity
+
 router.get("/celebrities/create", (req, res, next) => {
     res.render("celebrities/new-celebrity");
 })
+
+//Process Celebrity creation
 
 router.post("/celebrities/create", (req, res, next) => {
   const celebrityData = {
