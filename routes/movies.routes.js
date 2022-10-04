e2fb76572943966dd9a49b09154d1a8f2ee3baf6
Gabriel Gomez <ggomez.box@gmail.com>
2022-10-04 18:45:51 +0200

    iteration 6 done

---
+++ routes/movies.routes.js
@@ -0,0 +1,36 @@
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+// all your routes here
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("movies/new-movie", { celebrities });
+    })
+    .catch((err) => {
+      console.log("There was an error getting all the movies from DB", err);
+      next();
+    });
+});
+
+router.post("/movies/create", (req, res, next) => {
+  const newMovie = {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot,
+    celebrity: req.body.celebrity,
+  };
+  Movie.create(newMovie)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => {
+      console.log("There was an error creating new movie", err);
+      next();
+    });
+});
+
+module.exports = router;
