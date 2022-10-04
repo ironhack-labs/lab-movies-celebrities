dd5c406f96c767be9397110aa3d631c087fbfb01
Marc Logan <marclogan90@gmail.com>
2022-10-04 18:06:32 +0200

    finished iteration 6

--- routes/movies.route.js
+++ routes/movies.route.js
@@ -1,5 +1,52 @@
-const Author = require("../models/Author.model");
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
 const router = require("express").Router();
+
+// router.get("/books", (req, res, next) => {
+//     Book.find()
+//     .populate("author")
+//       .then( booksFromDB => {
+//           res.render("books/books-list", {books: booksFromDB})
+//       })
+//       .catch( err => {
+//           console.log("error getting books from DB", err);
+//           next();
+//       })
+//   });
+
+
+  //CREATE: display form
+router.get("/movies/create", (req, res, next) => {
+    Celebrity.find()
+    .then((celebArr) => {
+        res.render("movies/new-movie", {celebArr});
+    })
+    .catch(err => {
+        console.log("error getting celebrities from DB", err);
+        next(err);
+      })
+})
+
+//CREATE: process form
+router.post("/movies/create", (req, res, next) => {
+  const moviesDetails = {
+      title: req.body.title,
+      genre: req.body.genre,
+      plot: req.body.plot,
+  }
+
+  Movie.create(moviesDetails)
+  .then( moviesDetails => {
+      res.redirect("/movies")
+  })
+  .catch(err => {
+      console.log("error creating new movie in DB", err);
+      next();
+  })
+
+})
+  
+
 
 module.exports = router;
