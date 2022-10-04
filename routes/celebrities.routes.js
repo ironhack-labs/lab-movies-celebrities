238059684286d851c7fbf9abe3ddbb29d3912a64
Serhii Kvasha <seregeykvasha@gmail.com>
2022-10-04 18:18:08 +0200

    Iteration 5 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,27 +1,9 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
-
 router.get("/celebrities/create", (req, res, next) => {
-<<<<<<< HEAD
   res.render("celebrities/new-celebrity");
 });
-=======
-    res.render("celebrities/new-celebrity")
-
-})
-
-router.post("/celebrities/create", (req, res, nex) => {
-    const newCelebrity = {
-        name: req.body.name,
-        occupation: req.body.occupation,
-        catchPhrase: req.body.catchPhrase
-    }
-    Celebrity.create(newCelebrity)
-    .then( () => {
-        res.redirect("celebrities/celebrities")
->>>>>>> a8d736447b66d3e29f4512acf1aedc162452f4d5
-
 router.post("/celebrities/create", (req, res, nex) => {
   const newCelebrity = {
     name: req.body.name,
@@ -32,20 +14,12 @@ router.post("/celebrities/create", (req, res, nex) => {
     .then((response) => {
       res.redirect("/celebrities");
     })
-<<<<<<< HEAD
     .catch((err) => {
       console.log("There was an error creating new celebrity", err);
       res.render("celebrities/new-celebrity");
       next();
     });
 });
-=======
-    .catch(()=>{
-        res.render("celebrities/new-celebtity")
-    })
-}) 
->>>>>>> a8d736447b66d3e29f4512acf1aedc162452f4d5
-
 router.get("/celebrities", (req, res, next) => {
   Celebrity.find()
     .then((celebrities) => {
