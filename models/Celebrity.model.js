c1db905e3301f710bc1e29be76743dcdc96c1c5b
Helena Saborit <helena.sb91@gmail.com>
2022-10-18 16:40:45 +0200

    it 3 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,15 @@
 //  Add your code here
+const mongoose = require('mongoose');
+
+const celebritySchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    occupation: String,
+    catchPhrase: String,
+})
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,27 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
 
-// all your routes here
+//GET /celebrities/create form to create celebrity
+router.get("/create", (req, res, next) => {
+    res.render("celebrities/new-celebrity.hbs")
+})
+
+//POST /celebrities/create data from form to create celeb and save it in DB
+router.post("/create", async (req, res, next) => {
+   const {name, occupation, catchPhrase} = req.body;
+
+    try {
+    await Celebrity.create({name, occupation, catchPhrase})
+    res.redirect("/celebrities")
+
+    } catch (error) {
+        res.render("celebrities/new-celebrity")
+        next(error)
+    }
+})
+
+
+
 
 module.exports = router;
