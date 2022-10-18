a6e59af7689913a30fe4a0d4e8d9c919728d6dac
MoniIgnacio <moniignacio02@gmail.com>
2022-10-18 18:04:50 +0400

    Add It-1 done

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,16 @@
 const router = require("express").Router();
 
+
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
 });
+
+
+const moviesRoutes = require ('./movies.routes.js')
+router.use('/movies', moviesRoutes);
+
+const celebritiesRoutes = require ('./celebrities.routes.js')
+router.use('/celebrities', celebritiesRoutes);
 
 module.exports = router;
