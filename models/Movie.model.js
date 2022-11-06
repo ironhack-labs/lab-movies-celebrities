772759cd27892f77340d10cc37f53f22d6797fe5
Shania <shaniaantezana@gmail.com>
2022-11-06 15:46:19 -0400

    iteration 5 done

---
+++ models/Movie.model.js
@@ -0,0 +1,10 @@
+const {Schema,model} = require('mongoose')
+const movieSchema =new Schema({
+   title:{type:String,required:true},
+   genre:[String],
+   plot:String,
+   cast:[{type: Schema.Types.ObjectId,ref:'Celebrity'}]
+})
+const Movie = model('Movie',movieSchema)
+
+module.exports = Movie 
