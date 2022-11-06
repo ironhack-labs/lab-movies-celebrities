2e4bd2a91a4b68b04bfde5c72180cef4e4d922bb
Shania <shaniaantezana@gmail.com>
2022-11-06 15:30:24 -0400

    iteration 4 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,14 @@
 const router = require('express').Router()
 const Celebrity = require('../models/Celebrity.model')
 
+router.get('/celebrities',async(req,res)=>{
+try{
+  let celebrities = await Celebrity.find()
+  res.render('celebrities/celebrities',celebrities)
+}catch(error){
+  console.log(error)
+}
+})
 router.get('/celebrities/create',(req,res)=>{
  res.render('celebrities/new-celebrity')
 })
@@ -8,15 +16,13 @@ router.post('/celebrities/create',async(req,res)=>{
   const celebrity = req.body
   const {name,occupation,catchPhrase} = celebrity
   !name || !occupation || !catchPhrase ? res.render('celebrities/new-celebrity') : null
-  const foundedCelebrity = Celebrity.findOne(celebrity)
+  const foundedCelebrity = await Celebrity.findOne(celebrity)
   foundedCelebrity ? res.render('celebrities/new-celebrity') : null
   try{
-   await Celebrity.create(celebrity)
-   //const celebrities= Celebrity.find()
-   //res.render('celebrities/celebrities',celebrities)
+   res.redirect('/celebrities')
   }catch(error){
     console.log(error)
-    res.render('celebrities/new-celebrity')
+    res.redirect('celebrities/new-celebrity')
   }
 })
 
