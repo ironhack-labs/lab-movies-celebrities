c7dbabdb553aecda9c8e40a67077a252db9d5f15
Gonzalo García Corrales <115544633+gongarco@users.noreply.github.com>
2022-11-12 18:43:27 +0100

    iteration_3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,25 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
-const router = require("express").Router();
+const express = require('express');
+const router = express.Router();
 
-// all your routes here
+const CelebrityModel = require('../models/Celebrity.model')
+
+router.get('/celebrities/create', (req, res, next) => {
+    res.render('celebrities/new-celebrity')
+    // console.log('la ruta desde celebrities funciona')
+});
+
+router.post('/celebrities/create', (req, res, next) => {
+    const { name, occupation, cathPhrase } = req.body
+
+    CelebrityModel
+        .create({ name, occupation, cathPhrase })
+        .then(newCelebrity => {
+            res.redirect('/celebrities')
+        })
+        .catch(err => {
+            console.log(err)
+            res.redirect('celebrities/new-celebrity')
+        })
+})
 
 module.exports = router;
