6a3a11c2b9b4197455a556f36ea195c883011366
davidmunoz626 <115696661+davidmunoz626@users.noreply.github.com>
2022-11-13 12:44:34 +0100

    done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -36,6 +36,60 @@ router.get("/celebrities", (req, res) => {
         .catch(err => (err))
 })
 
+router.get('/celebrities/celebritie-details/:celeb_id', (req, res) => {
+
+    const { celeb_id } = req.params
+
+    Celebrity
+        .findById(celeb_id)
+        .then(celebFromDb => {
+            res.render('celebrities/celebritie-details', celebFromDb)
+        })
+        .catch(err => console.log(err))
+})
+
+
+router.get('/celebrities/:celeb_id/update-celebrities', (req, res) => {
+
+    const { celeb_id } = req.params
+
+    Celebrity
+        .findById(celeb_id)
+        .then(celebFromDb => {
+            const celebrities = {
+                celebArray: celebFromDb
+            }
+            res.render('celebrities/update-celebrities', celebrities)
+
+
+        })
+        .catch(err => console.log(err))
+})
+
+
+
+router.post('/celebrities/:celeb_id/update-celebrities', (req, res) => {
+    const { name, occupation, catchPhrase } = req.body
+    const { celeb_id } = req.params
+
+    Celebrity
+        .findByIdAndUpdate(celeb_id, { name, occupation, catchPhrase })
+        .then(() => res.redirect('/celebrities'))
+        .catch(err => console.log(err))
+
+
+})
+
+
+
+router.post('/celebrities/:celeb_id/delete', (req, res) => {
+    const { celeb_id } = req.params
+    Celebrity
+        .findByIdAndDelete(celeb_id)
+        .then(() => res.redirect('/celebrities'))
+        .catch(err => console.log(err))
+})
+
 
 
 module.exports = router;
