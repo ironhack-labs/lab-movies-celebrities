03d0e060d558fa18ef385afd2ecebe4e30cc7f9b
cursedxp <anilozsoy@gmail.com>
2022-11-14 15:21:34 +0100

    Iteration #2: The Celebrity model

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,13 @@
+const mongoose = require("mongoose");
+
 //  Add your code here
+const { Schema, model } = mongoose;
+
+const celebritySchema = new Schema({
+  name: String,
+  occupation: { type: String, default: "unknown" },
+  catchPhrase: String,
+});
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+module.exports = Celebrity;
