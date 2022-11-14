79abfe290e86a728c3b6d4641b7b52f7e67600b2
cursedxp <anilozsoy@gmail.com>
2022-11-14 17:07:40 +0100

    Iteration #3: Adding New Celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,10 +1,27 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrities = require("../models/Celebrity.model");
 
 // all your routes here
 
 router.get("/", (req, res) => {
   res.send("Celebrities");
+});
+
+//Create new celebrity
+router.get("/create", (req, res) => {
+  res.render("./celebrities/new-celebrity");
+});
+
+router.post("/create", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrities.create({ name, occupation, catchPhrase })
+    .then(() => {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      res.render("./celebrities/new-celebrity");
+    });
 });
 
 module.exports = router;
