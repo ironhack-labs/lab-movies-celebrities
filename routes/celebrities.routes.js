0c7e13c2667784c234aff10556e489191b42672f
Daniela <danielacardona2203@gmail.com>
2022-11-16 11:04:33 +0100

    almost done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -14,7 +14,7 @@ router.post("/celebrities/create", (req, res, next) => {
   Celebrity.create({ name, ocupation, catchPhrase })
     // .then(bookFromDB => console.log(`New book created: ${bookFromDB.title}.`))
     .then(() => res.redirect("/celebrities/create"))
-    .catch((error) => res.redirect("celebrities/new-celebrity"));
+    .catch((error) => res.render("celebrities/new-celebrity"));
 });
   
 router.get("/celebrities", (req, res, next) => {
@@ -25,6 +25,65 @@ router.get("/celebrities", (req, res, next) => {
     .catch((error) => {
 console.log(error)    });
 });
+
+
+// Get celebrity details
+router.get("/celebrities/:id", (req, res) => {
+  const id = req.params.id
+
+  Celebrity.findById(id)
+  .then(celebrity => {
+      res.render("celebrities/celebrity-details", { celebrity })
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
+// Delete celebrity
+router.post("/celebrities/:id/delete", (req, res) => {
+  const id = req.params.id
+
+  Celebrity.findByIdAndRemove(id)
+  .then(deletedCelebrity => {
+      res.redirect("/celebrities")
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
+// Edit celebrity
+router.get("/celebrities/:id/edit", (req, res) => {
+  const id = req.params.id
+
+  Celebrity.findById(id)
+  .then(celebrity => {
+      res.render("celebrities/edit-celebrity", { celebrity })
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
+router.post("/celebrities/:id/edit", (req, res) => {
+  const id = req.params.id
+  const { name, occupation, catchPhrase } = req.body
+
+  const celebrity = {
+      name,
+      occupation,
+      catchPhrase
+  }
+
+  Celebrity.findByIdAndUpdate(id, celebrity)
+  .then(createdCelebrity => {
+      res.redirect(`/celebrities/${id}`)
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
 
 module.exports = router;
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -37,4 +37,78 @@ router.get("/movies", (req, res, next) => {
 console.log(error)    });
 });
 
+
+// Get movie details
+router.get("/movies/:id", (req, res) => {
+  const id = req.params.id
+
+  Movie.findById(id)
+  .populate("cast")
+  .then(movie => {
+      res.render("movies/movie-details", { movie })
+  })
+  .catch(err => {
+      console.log(err)
+  })
+})
+
+// Delete movie
+router.post("/movies/:id/delete", (req, res, next) => {
+  const id = req.params.id
+
+  Movie.findByIdAndRemove(id)
+  .then(deletedMovie => {
+      res.redirect("/movies")
+  })
+  .catch(err => {
+      next(err)
+  })
+})
+
+// Edit movie
+router.get("/movies/:id/edit", async (req, res, next) => {
+  const id = req.params.id
+
+  try {
+      const movie = await Movie.findById(id).populate("cast")
+      const celebrities = await Celebrity.find()
+      const celebritiesNotInCast = filterCelebritiesNotInCast(movie, celebrities)
+
+      res.render("movies/edit-movie", { movie, celebritiesNotInCast })
+  } catch (error) {
+      console.log(error)
+  }
+})
+
+router.post("/movies/:id/edit", (req, res, next) => {
+  const id = req.params.id
+  const { title, genre, plot, cast } = req.body
+
+  const movie = {
+      title,
+      genre,
+      plot,
+      cast
+  }
+
+  Movie.findByIdAndUpdate(id, movie)
+  .then(createdMovie => {
+      res.redirect(`/movies/${id}`)
+  })
+  .catch(err => {
+      next(err)
+  })
+})
+
+function filterCelebritiesNotInCast(movie, celebrities) {
+  return celebrities.filter(celebrity => {
+      movie.cast.forEach(movieCelebrity => {
+          if (movieCelebrity.name === celebrity.name) {
+              return false
+          }
+      })
+      return true
+  })
+}
+
 module.exports = router;
