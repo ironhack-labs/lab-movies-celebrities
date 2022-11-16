b7072c98fd0d1b82533616a29c15064e02a0c925
sijincodes <sijinmathews03@gmail.com>
2022-11-16 19:54:04 +0100

    bonus-update done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -10,9 +10,7 @@ router.get("/celebrities/create", (req, res, next) => {
 router.post("/celebrities/create", (req, res, next) => {
   const { name, occupation, catchPhrase } = req.body;
   Celeb.create({ name, occupation, catchPhrase })
-    .then((celebfromDb) =>
-      res.redirect("/celebrities/")
-    )
+    .then((celebfromDb) => res.redirect("/celebrities/"))
     .catch((error) => res.render("celebrities/new-celebrity.hbs"));
 });
 // GET All
@@ -23,5 +21,43 @@ router.get("/celebrities/", (req, res, next) => {
     )
     .catch((error) => next(error));
 });
+//BONUS
+router.get("/celebrities/:celebId/", (req, res) => {
+  const { celebId } = req.params;
+  Celeb.findById(celebId)
+    .then((celebsFromDB) => {
+      res.render("celebrities/celebrities-details.hbs", {
+        celeb: celebsFromDB,
+      });
+    })
+    .catch((error) => {
+      console.log("Error while retrieving movie details: ", error);
+    });
+});
+//update
+router.get("/celebrities/:celebId/edit", (req, res) => {
+  const { celebId } = req.params;
+  Celeb.findById(celebId)
+    .then((theCeleb) => {
+      res.render("celebrities/celebrities-edit.hbs", { celeb: theCeleb });
+    })
+    .catch((error) => {
+      console.log("Error while retrieving movie details: ", error);
+    });
+});
+router.post("/celebrities/:celebId/", (req, res, next) => {
+  const { celebId } = req.params;
+  const { name, occupation, catchPhrase } = req.body;
 
+  Celeb.findByIdAndUpdate(
+    celebId,
+    { name, occupation, catchPhrase },
+    { new: true }
+  )
+    .then((updatedCeleb) => {
+      updatedCeleb.save();
+    })
+    .then(() => res.redirect("celebrities/celebrities.hbs"))
+    .catch((error) => next(error));
+});
 module.exports = router;
