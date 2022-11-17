0aac7c0582cac49782fc81c9746f72510f0aa9f1
cursedxp <anilozsoy@gmail.com>
2022-11-17 11:46:26 +0100

    Iteration #10: Editing Movies

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -22,7 +22,6 @@ router.post("/create", (req, res) => {
   console.log("Celebrities", cast);
   Movie.create({ title: title, genre: genre, plot: plot, cast: cast })
     .then(() => {
-      console.log("Saved to Db");
       res.redirect("/movies");
     })
     .catch((err) => {
@@ -38,7 +37,6 @@ router.get("/:id", (req, res) => {
     .populate({ path: "cast", model: "Celebrity" })
     .then((data) => {
       res.render("./movies/movie-details", { data: data });
-      console.log(data);
     })
     .catch((err) => {
       console.log(err);
@@ -50,12 +48,37 @@ router.post("/:id/delete", (req, res) => {
 
   Movie.findByIdAndRemove(id)
     .then(() => {
-      console.log("Movie Deleted");
       res.redirect("/movies");
     })
     .catch((err) => {
       console.log(err);
     });
+});
+
+router.get("/:id/edit", (req, res) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .then((movieData) => {
+      Celebrity.find().then((celebrityData) => {
+        res.render("./movies/edit-movie", { movieData, celebrityData });
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+router.post("/:id/edit", (req, res) => {
+  const { id } = req.params;
+  const { title, genre, plot, cast } = req.body;
+  Movie.findByIdAndUpdate(id, {
+    title: title,
+    genre: genre,
+    plot: plot,
+    cast: cast,
+  }).catch((err) => {
+    res.redirect("/movies/movie-details");
+  });
 });
 
 module.exports = router;
