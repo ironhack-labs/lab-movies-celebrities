65c61821b69001aa8c664b7451ec41ae883df384
Alfie Collins <alfiecollins44@hotmail.co.uk>
2022-11-18 15:21:41 +0000

    i 1-3

--- routes/celebrities.route.js
+++ routes/celebrities.route.js
@@ -2,5 +2,22 @@
 const router = require("express").Router();
 
 // all your routes here
+/* GET create-celeb page */
+router.get("/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
+
+//POST create-celeb page
+
+router.post("/create", async (req, res, next) => {
+
+  const { name, occupation, catchPhrase } = req.body;
+  
+  Celebrity.create({ name, occupation, catchPhrase });
+  res.redirect(`/celebrities`)
+  .catch(err => console.log(err)
+  .then(res.redirect("celebrities/new-celebrity")))
+});
+
 
 module.exports = router;
