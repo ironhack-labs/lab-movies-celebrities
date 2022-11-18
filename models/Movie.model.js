96aed5b653c93b3512bb0c2df2f78796341e6f35
Antonio Gloria <angloria87@gmail.com>
2022-11-18 15:26:54 -0600

    Solved the lab

---
+++ models/Movie.model.js
@@ -0,0 +1,26 @@
+//  Add your code here
+const { Schema, model } = require('mongoose');
+
+const movieSchema = new Schema(
+  {
+    title: {
+      type: String,
+      trim: true,
+      required: [true, 'Title is required.'],
+      unique: true
+    },
+    genre: {
+      type: String,
+      trim: true,
+    },
+    plot: {
+      type: String,
+    },
+    cast: [{type: Schema.Types.ObjectId, ref: 'Celebrity'}],
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = model('Movie', movieSchema);
