6add1aef3fc7636cc90a57e745551662fc25de11
Jandson Costa <jandsoncosta55@gmail.com>
2022-11-25 21:27:17 -0300

    lab incomplete, terminar depois

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const projectName = 'lab-movies-celebrities';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
-
+// terminar depois, lab incompleto
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
