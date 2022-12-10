654d04dfcb77b00936efc786b436790dc0039e71
Cristian Gomez <gmz.mrs.cristian@gmail.com>
2022-12-09 22:44:06 -0600

    Ya quedo hasta el listado de peliculas

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,20 @@
 //  Add your code here
+const { Schema, model } = require('mongoose');
+
+const celebritySchema = new Schema({
+    name:{
+        type: String,
+        require: true
+    },
+    occupation:{
+        type: String
+    },
+    catchPhrase:{
+        type: String
+    }
+},
+{
+    timestamps: true
+});
+
+module.exports = model('Celebrity', celebritySchema);
---
+++ routes/movies.routes.js
@@ -0,0 +1,43 @@
+const router = require('express').Router();
+
+const Celebrity = require('../models/Celebrity.model');
+const Movie = require('../models/Movie.model');
+
+router.get('/list', async (req,res,next)=>{
+    try {
+        const moviesList = await Movie.find().populate('_cast','name')
+        res.render('movies/movies', { moviesList })
+        //res.json(moviesList)
+    } catch (error) {
+        next(error)
+    }
+})
+
+
+router.get('/create', async (req,res,next)=>{
+    try {
+        const listCelebrities = await Celebrity.find()
+        res.render('movies/new-movie', {listCelebrities})
+    } catch (error) {
+        next(error)
+    }
+})
+
+router.post('/create', async (req,res,next)=>{
+    try {
+        const {movieTitle,movieGenre,moviePlot,CelebritiesIds} = req.body
+        await Movie.create({title:movieTitle,genre:movieGenre,plot:moviePlot,_cast:CelebritiesIds})
+        res.redirect('list')
+    } catch (error) {
+        next(error);
+    }
+})
+
+
+
+
+
+
+
+
+module.exports = router;
