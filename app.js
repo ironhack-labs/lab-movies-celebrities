67efd73ba76b492c4bb859d4daf192db4b6b12b9
Everton Pudim Jackson <everton@debian-BULLSEYE-live-builder-AMD64>
2022-12-17 10:55:26 -0300

    done

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('dotenv/config');
 // ℹ️ Connects to the database
 require('./db');
 
+
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require('express');
