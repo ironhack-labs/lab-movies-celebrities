bbf20fae0f29b348bf89ccfb090eb0031fd12cf0
Rudolf Teibtner <rudolfteibtner@mailbox.org>
2023-01-02 21:11:44 +0100

    add iteration 6, not done yet

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,59 +1,49 @@
-const Celebrity = require("../models/Celebrity.model");
+const CelebrityModel = require("../models/Celebrity.model");
+
+const MONGO_URI =
+  process.env.MONGODB_URI ||
+  "mongodb://127.0.0.1/lab-movies-celebrities" ||
+  "mongodb://localhost:27017/lab-movies-celebrities";
 
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
-// all your routes here
-
 //GET list all Celebs
 router.get("/celebrities", (req, res, next) => {
-    Celebrity.find()
-       
-        .then(celebsFromDB => {
+  CelebrityModel.find()
 
-            console.log(celebsFromDB)
+    .then((celebsFromDB) => {
+      console.log(celebsFromDB);
 
-            res.render("celebrities/celebrities", { celebrities: celebsFromDB })
-        })
-        .catch(err => {
-            console.log("error getting celebs from DB", err);
-            next();
-        })
+      res.render("celebrities/celebrities", { celebrities: celebsFromDB });
+    })
+    .catch((err) => {
+      console.log("error getting celebs from DB", err);
+      next();
+    });
 });
-
-
-
 
 //GET celebrity create form
 router.get("/celebrities/create", (req, res, next) => {
-    
-        res.render("celebrities/new-celebrity")
-    
-            
-       
-        
-        })
-    
-    //POST submit the celebrity create form
-    router.post("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
 
-        const celebDetails = {
-            name: req.body.name,
-            occupation: req.body.occupation,
-            catchPhrase: req.body.catchPhrase
-           
-        }
-    
-        Celebrity.create(celebDetails)
-            .then(celebDetails => {
-                res.redirect("/celebrites");
-            })
-            .catch(err => {
-                res.render("/celebrites/new-celebrity");
-                next();
-            })
-    
+//POST submit the celebrity create form
+router.post("/celebrities/create", (req, res, next) => {
+  const celebDetails = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
+
+  Celebrity.create(celebDetails)
+    .then((celebDetails) => {
+      res.redirect("/celebrites");
+    })
+    .catch((err) => {
+      res.render("/celebrites/new-celebrity");
+      next();
     });
-
+});
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,56 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const express = require("express");
+
+const Movies = require("../models/Movies.model");
+
+const MONGO_URI =
+  process.env.MONGODB_URI ||
+  "mongodb://127.0.0.1/lab-movies-celebrities" ||
+  "mongodb://localhost:27017/lab-movies-celebrities";
 
 // all your routes here
+//GET movies create form
+router.get("/movies", (req, res, next) => {
+  Celebrity.find()
+    .then(() => {
+      res.render("movies/new-movie");
+    })
+    .catch((err) => {
+      console.log("Error getting movies from database...", err);
+      next();
+    });
+});
+
+//POST submit the celebrity create form
+router.post("/movies/create", (req, res, next) => {
+  const moviesDetails = {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot,
+    cast: req.body.cast, //potentially wrong
+  };
+
+  Movies.create(moviesDetails)
+    .then((moviesDetails) => {
+      res.redirect("movies/movies");
+    })
+    .catch((err) => {
+      res.render("movies/new-movie");
+      next();
+    });
+});
 
 module.exports = router;
+
+// GET view all movies
+router.get("/movies", (req, res, next) => {
+  Movies.find()
+    .then(() => {
+      res.render("movies/movies");
+    })
+    .catch((err) => {
+      console.log("Error deleting book...", err);
+      next();
+    });
+});
