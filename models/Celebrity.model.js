8845cf78b7f32859aa6d829ec8ae7043f2cdc5bf
Thomas Augot <thomas.augot@hotmail.fr>
2023-01-02 18:44:58 +0100

    iteration 2 completed

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,16 @@
 //  Add your code here
+
+const { Schema, model } = require('mongoose');
+
+const celebritySchema = new Schema(
+    {
+        name: String,
+        occupation: String,
+        catchPhrase: String
+    },
+    {
+        timestamps: true
+    }
+);
+
+module.exports = model('Celebrity', celebritySchema);
