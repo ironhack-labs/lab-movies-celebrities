3a12fd800184ccadbfae94065333d9de19209e28
Luluyan2022 <emmymagao@gmail.com>
2023-01-02 20:43:17 +0100

    iteration 1-7

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,11 +3,11 @@ const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model")
 
 //Show a form to create a celebrity
-router.get("/celebrities/create"), (req, res, next) => {
+router.get("/celebrities/create", (req, res, next) => {
    res.render("celebrities/new-celebrity")
-};
+});
 //Send the data from the form to this route to create the celebrity and save it to the database
-router.post("/celebrities/create"), (req, res, next) => {
+router.post("/celebrities/create", (req, res, next) => {
    const {name, occupation, catchPhrase} = req.body;
    Celebrity.create({name, occupation, catchPhrase})
      .then(celebritiesDetails => {
@@ -16,24 +16,20 @@ router.post("/celebrities/create"), (req, res, next) => {
      .catch(err => {
         res.render("celebrities/new-celebrity") 
      })
-};
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+});
+//display a list of all the celebrities
+router.get("/celebrities", (req, res, next) => {
+   Celebrity.find()
+     .then((celebritiesFromDB) => {
+       console.log(celebritiesFromDB);
+ 
+       res.render("celebrities/celebrities", { celebrities: celebritiesFromDB });
+     })
+     .catch((err) => {
+       console.log("error getting celebrities from DB", err);
+       next();
+     });
+ });
 
 
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,39 @@
 const router = require("express").Router();
 
+const Movie = require("../models/Movie.model")
+const Celebrity = require("../models/Celebrity.model");
+
 // all your routes here
+//Show a form to create a movie
+router.get("/movies/create", (req, res, next) => {
+
+    let celebrities;
+  
+    Celebrity.find()
+        .then( (celebritiesFromDB) => {
+            celebrities = celebritiesFromDB;
+            
+            console.log(celebritiesFromDB);
+  
+            res.render("movies/new-movie", { celebrities: celebrities });
+        })
+        .catch(err => {
+            console.log("Error creating movies details from DB...", err);
+            next();
+        });
+  });
+ //Send the data from the form to this route to create the movie and save it to the database
+ router.post("/movies/create", (req, res, next) => {
+
+    const {name, title, genre, plot, cast} = req.body;
+    Movie.create({name, title, genre, plot, cast})
+      .then(moviesDetails => {
+          res.redirect("/movies")
+      })
+      .catch(err => {
+         res.render("movies/new-movie") 
+      })
+ });
 
 module.exports = router;
+
