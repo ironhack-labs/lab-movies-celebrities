dc019c489ca483ee9cf16a68ca9cf7e1061d7536
omarali <o.ali94@gmail.com>
2023-01-07 16:18:59 +0000

    Iteration 1-4 complete

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,13 @@
-//  Add your code here
+const { Schema, model } = require("mongoose");
+
+const celebritiesSchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+  },
+  { timestamps: true }
+);
+
+const Celebrity = model("Celebrity", celebritiesSchema);
+module.exports = Celebrity;
---
+++ routes/celebrities.routes.js
@@ -0,0 +1,31 @@
+// const app = require('../app');
+const Celebrity = require("../models/Celebrity.model");
+const router = require("express").Router();
+
+router.get("/celebrities/create", (req, res) => {
+  console.log("working");
+  res.render("celebrities/new-celebrity");
+});
+
+router.post("/celebrities/create", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then((newCeleb) => {
+      console.log(newCeleb);
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+        console.log(`This is your error: ${error}`);
+        next(error)
+    })
+})
+
+router.get('/celebrities', (req,res,next) => {
+    Celebrity.find()
+    .then(allCelebs => {
+        console.log(allCelebs);
+        res.render('celebrities/celebrities', {allCelebs})
+    })
+} )
+
+module.exports = router;
