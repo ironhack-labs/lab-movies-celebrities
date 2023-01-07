bf084da5b3f8e320a59e0b286cd77200470333ab
Thor Löfgren <lofgrenthor@gmail.com>
2023-01-07 17:18:36 +0100

    Complete Iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,24 +1,39 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-const CelebrityModel = require("../models/Celebrity.model")
+const CelebrityModel = require("../models/Celebrity.model");
 
 // all your routes here
-router.get("/celebrities/create", (req,res) =>{
-    res.render("celebrities/new-celebrity")
-})
 
-router.post("/celebrities/create", (req,res) => {
-    const {name, occupation, catchPhrase} = req.body 
-    CelebrityModel.create({name, occupation, catchPhrase})
-        .then((result)=>{
-            console.log("new Celebrity was created: " + result)
-            res.redirect("/celebrities")
-        })
-        .catch((error) => {
-            console.log("An error occured while creating a New Celebrity: " + err)
-            res.render("/celebrities/new-celebrity")
-        })
+router.get("/celebrities", (req, res) => {
+  CelebrityModel.find()
+    .then((celebrities) => {
+      console.log("Celebrities found in db: ", celebrities);
+
+      res.render("celebrities/celebrities", { celebrities });
     })
+    .catch((error) => {
+      console.log(
+        "Something went wrong while getting celebrities list: ",
+        error
+      );
+    });
+});
 
+router.get("/celebrities/create", (req, res) => {
+  res.render("celebrities/new-celebrity");
+});
+
+router.post("/celebrities/create", (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  CelebrityModel.create({ name, occupation, catchPhrase })
+    .then((result) => {
+      console.log("new Celebrity was created: " + result);
+      res.redirect("/celebrities");
+    })
+    .catch((error) => {
+      console.log("An error occured while creating a New Celebrity: " + err);
+      res.render("/celebrities/new-celebrity");
+    });
+});
 
 module.exports = router;
