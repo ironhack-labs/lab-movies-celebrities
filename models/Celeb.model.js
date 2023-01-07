e50cec8c5c7c0fac1cd15ae7a66fa884b028beae
Jessica-Maria Freund <jessicamariafreund@gmail.com>
2023-01-07 16:53:39 +0100

    Iteration 1 & 2

---
+++ models/Celeb.model.js
@@ -0,0 +1,11 @@
+const {Schema, model} = require('mongoose')
+
+const celebSchema = new Schema(
+    {
+        name: String,
+        occupation: String,
+        catchPhrase: String,
+    }
+)
+
+module.exports = model('Celebs', celebSchema)
--- routes/celeb.routes.js
+++ routes/celeb.routes.js
@@ -1,6 +1,6 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-
+const celeb = require("../models/Celeb.model")
 // all your routes here
 
 module.exports = router;
