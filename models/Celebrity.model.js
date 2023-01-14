70044d970d27026122c21c130feb49da7785ac3f
Carlos Sosa <csosa@simpletg.com>
2023-01-14 14:52:49 -0500

    Iteration 2 Complete

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,10 @@
 //  Add your code here
+const { model, Schema } = require('mongoose');
+
+const celebritySchema = new Schema({
+  name: { type: String, required: true },
+  occupation: String,
+  catchPhrase: String,
+});
+
+module.exports = model('Celebrity', celebritySchema);
