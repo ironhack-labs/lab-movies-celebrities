0896cb792669eb4787fd9da73e3eac2d7e2b5845
pianattero <mariapianattero@gmail.com>
2023-01-25 12:41:25 +0100

    Re-organized structure

--- app.js
+++ app.js
@@ -1,32 +1,37 @@
-// ℹ️ Gets access to environment variables/settings
-// https://www.npmjs.com/package/dotenv
+
+const express = require("express");
+const logger = require("morgan");
+const cookieParser = require("cookie-parser");
+const favicon = require("serve-favicon");
+const path = require("path");
+
 require('dotenv/config');
-
-// ℹ️ Connects to the database
-require('./db');
-
-// Handles http requests (express is node js framework)
-// https://www.npmjs.com/package/express
-const express = require('express');
-
-// Handles the handlebars
-// https://www.npmjs.com/package/hbs
-const hbs = require('hbs');
+require('./config/db.config');
+require('./config/hbs.config');
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most middlewares
-require('./config')(app);
+app.use(logger("dev"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.use(cookieParser());
+
+app.set("views", path.join(__dirname, ".", "views"));
+app.set("view engine", "hbs");
+
+app.use(express.static(path.join(__dirname, ".", "public")));
+app.use(favicon(path.join(__dirname, ".", "public", "images", "favicon.ico")));
+
+// 👇 Start handling routes here
+const index = require('./routes/index');
+app.use('/', index);
 
 // default value for title local
 const projectName = 'lab-movies-celebrities';
 const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
-
-// 👇 Start handling routes here
-const index = require('./routes/index');
-app.use('/', index);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
