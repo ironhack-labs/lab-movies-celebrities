8ab6e2819fdde81b55d5281eb2b5cd3d1ca40625
Manuel Da Silva <ktireds2803@gmail.com>
2023-02-07 18:17:53 +0100

    Lab done

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -26,3 +26,42 @@ module.exports.doCreate = (req, res, next) => {
       console.error(err)
   })
 }
+
+module.exports.detail = (req, res, next) => {
+  Movie.findById(req.params.id)
+  .populate('cast')
+  .then((movie) => {
+      res.render('movies/movie-details', { movie })
+  })
+  .catch(err => console.error(err))
+}
+module.exports.doDelete = (req, res, next) => {
+  Movie.findByIdAndDelete(req.params.id)
+  .then((movie) => {
+      res.redirect('/movies')
+      console.info(`${movie.title} has been deleted`)
+  })
+  .catch(err => console.error(err))
+}
+
+module.exports.edit = (req, res, next) => {
+  Promise.all([
+      Movie.findById(req.params.id),
+      Celebrity.find()
+  ])
+  .then((response) => {
+      const [movie, celebrities] = response; 
+   
+      res.render('movies/edit-movie', { celebrities, movie})
+  })
+  .catch(err => console.error(err))
+};
+
+module.exports.doEdit = (req, res, next) => {
+  Movie.findByIdAndUpdate(req.params.id, req.body)
+  .then((movie) => {
+      res.redirect('/movies')
+      console.info(`${movie.title} has been updated`)
+  })
+  .catch(err => console.error(err))
+}
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,5 +4,9 @@ const moviesController = require("../controllers/movies.controller");
 router.get("/", moviesController.list);
 router.get("/create", moviesController.create);
 router.post("/create", moviesController.doCreate);
+router.get("/:id", moviesController.detail);
+router.post("/:id/delete", moviesController.doDelete);
+router.get("/:id/edit", moviesController.edit);
+router.post("/:id/edit", moviesController.doEdit);
 
 module.exports = router;
