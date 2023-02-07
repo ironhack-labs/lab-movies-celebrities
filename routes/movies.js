c7a632c3e8d4a6ce7841dab1f1a186e739fe3bf0
AGUSTINA PATT <agustinapatt@gmail.com>
2023-02-07 19:48:37 +0100

    almost almost done

--- routes/movies.js
+++ routes/movies.js
@@ -5,24 +5,62 @@ const Celebrity = require("../models/Celebrity.model")
 
 // all your routes here
 
+router.get("/movies", (req, res, next) => {
+    Movie.find()
+    .then(MoviesFromDB => {
+        res.render("movies/movies", {movies: MoviesFromDB});
+    })
+    .catch(err => next(err))
+})
+
 router.get("/movies/create", (req, res, next) => {
 	
 	Celebrity.find()
 		.then(celebritiesFromDB => {
-            console.log('esto',celebritiesFromDB)
 			res.render("movies/new-movie", { cast: celebritiesFromDB})
 		})
 		.catch(err => next(err))
-})
-
-router.post("/movies/create", (req, res, next) => {
-    const {title, genre, plot, cast} = req.body
-  
+    })
+    
+    router.post("/movies/create", (req, res, next) => {
+        const {title, genre, plot, cast} = req.body
+        
     Movie.create({title, genre, plot, cast})
     .then(createdMovie => {
-        res.render("movies/movies")
+        res.redirect("/movies")
     })
     .catch(err => next(err))
-    
 })
+
+//NOT FINISHED
+router.get("/movies/:id", (req, res, next) => {
+    const movieId = req.params.id
+    
+    Movie.findById(movieId)
+    .populate("cast")
+    .then(MovieFromDB => {
+        res.render("movies/movie-details", {movie: MovieFromDB})
+    })
+    .catch(err => next(err))
+})
+
+router.post("/movies/:id/delete", (req, res, next) => {
+    const movieId = req.params.id
+    Movie.findByIdAndDelete(movieId)
+    .then(() => {
+        res.redirect("/movies")
+    })
+    .catch(err => next(err))
+})
+
+router.get("/movies/:id/edit", (req, res, next) => {
+    const movieId = req.params.id
+    Movie.findById(movieId)
+    Celebrity.find()
+    .then(() => {
+        res.render("movies/edit-movie")
+    })
+})
+
+
 module.exports = router;
