c15117a340effc2ccb19e4f1ecdb3cb3dfc78f01
Tobias Krauss <tobias.tk.krauss@gmail.com>
2023-02-07 15:39:06 +0100

    Iteration #3: Adding New Celebrities

--- routes/celebrities.js
+++ routes/celebrities.js
@@ -1,6 +1,34 @@
+
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+
+router.get("/celebrities/create", (req, res, next) => {
+    Celebrity.create()
+        .then(celebrity => {
+            res.render("celebrities/new-celebrity", { celebrity })
+        })
+        .catch(err => next(err))
+})
+
+router.post("/celebrities/create", (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body
+    Celebrity.create({name, occupation, catchPhrase})
+        .then(createdCelebrity => {
+            console.log(createdCelebrity)
+            res.redirect("/celebrities")
+        })
+        .catch(err => {next(err)
+        res.render("celebrities/new-celebrity")})
+})
 
 
+router.get("/celebrities", (req, res, next) => {
+    Celebrity.find()
+        .then(celebritiesFromDB => {
+            res.render("celebrities/celebrities", { celebrities: celebritiesFromDB })
+        })
+        .catch(err => next(err))
+})
 
 
 module.exports = router;
