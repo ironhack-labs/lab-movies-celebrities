dfbe36d4072cb2b6295c935b12b7ac9bee143c45
Pauline <sisal.dynamo-0r@icloud.com>
2023-02-09 22:58:03 +0100

    edit app.js and add Schema

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,13 @@
 //  Add your code here
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const celebSchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+
+const Celeb = mongoose.model("Celeb", celebSchema);
+module.exports = Celeb;
