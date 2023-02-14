7d8ebaabb46fe31399881f36e320bfbaf41ead08
Araceli Galiano Salas <araceligalianosalas@gmail.com>
2023-02-14 16:08:18 +0100

    Iteración 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,13 @@
 //  Add your code here
+const mongoose = require("mongoose");
+
+const celebritySchema = new mongoose.Schema({
+    
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+})
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema)
+
+module.exports = Celebrity
