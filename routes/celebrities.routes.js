d8faca943678d2627b25344751b15bd2d05f1fc4
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-18 17:01:21 +0100

    Solved iteration 3

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,23 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require('express').Router();
 
-// all your routes here
+const Celebrity = require('../models/Celebrity.model');
+
+router.get('/celebrities/create', (req, res, next) => {
+	res.render('celebrities/new-celebrity');
+});
+
+router.post('/celebrities/create', (req, res, next) => {
+	const { name, occupation, catchPhrase } = req.body;
+
+	Celebrity.create({ name, occupation, catchPhrase })
+		.then((newCeleb) => {
+			res.redirect('/celebrities');
+		})
+		.catch((err) => {
+			console.log('Detected error, redirecting');
+			res.render('celebrities/new-celebrity');
+		});
+});
 
 module.exports = router;
