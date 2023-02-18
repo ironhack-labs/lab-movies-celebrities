c02604ac54883879261ba03348d42b749d7e613b
ApollineStev <apollinecanard@gmail.com>
2023-02-18 16:58:53 +0100

    404 error fixed

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,12 @@
 //  Add your code here
+const { Schema, model} = require('mongoose')
+
+const Celebrity = new Schema(
+    {
+        name: String,
+        occupation: String,
+        catchPhrase: String
+    }
+)
+
+module.exports = model('celebrity', Celebrity)
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,6 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-
+const Celebrity = require('../models/Celebrity.model')
 // all your routes here
 
 router.get('/celebrities/create', (req, res, next) =>{
