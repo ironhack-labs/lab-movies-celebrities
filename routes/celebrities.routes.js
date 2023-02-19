e7c62d983b383bac70f302b9f7650e2f29e56ea5
Wilkins Morales <wilkinscom@gmail.com>
2023-02-19 18:34:14 +0200

    minor changes to create new celebrity

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,18 +1,25 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-const Celebrity = require('../models/Celebrity.model')
+const Celebrity = require("../models/Celebrity.model.js");
 // all your routes here
 
-router.get('/celebrities/create', (req, res, next) =>{
-    res.render('celebrities/new-celebrity')
-})
+router.get("/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
 
-router.post('/celebrities/create', (req, res, next) => {
-    const { name, occupation, catchPhrase } = req.body
-    res.redirect('/celebrities').catch(error => {
-        next(error)
-    })
+router.post("/create", (req, res, next) => {
+  const { name, occupation, catchPhrase } = req.body;
 
-})
+  //create new celebrity inside the database
+
+  Celebrity.create({ name, occupation, catchPhrase })
+  /*.then((celebrityDB) => console.log(`New celebrity created ${celebrityDB.name}`)*/
+      .then(() => res.redirect("/celebrities"))
+      .catch((error) => next(error))
+});
+
+router.get("/celebrities/", (req, res, next) => {
+  res.render("celebrities/celebrities", {Celebrity});
+});
 
 module.exports = router;
