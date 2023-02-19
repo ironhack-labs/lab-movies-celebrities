662030398bd7dd029f5365ddff6f0600f6af4040
Johannes Chorzempa <jchorzempa@posteo.org>
2023-02-19 16:07:17 +0100

    Workjing on iteration 9

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -7,12 +7,6 @@ const Celebrity = require('../models/Celebrity.model');
 
 // all your routes here
 
-router.get('/movies', (req, res, next) => {
-	Movie.find().then((moviesFromDB) => {
-		res.render('movies/movies', { movies: moviesFromDB });
-	});
-});
-
 router.get('/movies/create', (req, res, next) => {
 	Celebrity.find().then((allCelebrities) => {
 		res.render('movies/new-movie', { allCelebrities: allCelebrities });
@@ -38,6 +32,42 @@ router.post('/movies/create', (req, res, next) => {
 			console.log('Detected error, redirecting');
 			res.render('movies/new-movie');
 		});
+});
+
+router.get('/movies/:id/edit', (req, res, next) => {
+	const { id } = req.params;
+
+	Movie.findById(id).then((specificMovie) => {
+		Celebrity.find().then((allCastMembers) => {
+			res.render('movies/edit-movie', { movieToEdit: specificMovie, castToEdit: allCastMembers });
+		});
+	});
+});
+
+router.post('/movies/:id/edit', (req, res, next) => {
+	const { id } = req.params;
+	const { title, genre, plot } = req.body;
+
+	Movie.findByIdAndUpdate(id, { title, genre, plot }, { new: true })
+		.then((updatedMovie) => {
+			console.log(updatedMovie);
+			res.redirect(`/movies/${updatedMovie.id}`);
+		})
+		.catch((err) => console.log('DONKEY', err));
+});
+
+router.post('/movies/:id/delete', (req, res, next) => {
+	const { id } = req.params;
+
+	Movie.findByIdAndDelete(id)
+		.then(() => res.redirect('/movies'))
+		.catch((err) => console.log('FLONKEY!', err));
+});
+
+router.get('/movies', (req, res, next) => {
+	Movie.find().then((moviesFromDB) => {
+		res.render('movies/movies', { movies: moviesFromDB });
+	});
 });
 
 router.get('/movies/:id', (req, res, next) => {
