429507daef3cad33ba16daca6c7975eede2fd836
Maria Fernanda Echeverria Colorado <fernanda.ecmf@gmail.com>
2023-02-19 23:58:28 -0600

    Done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,14 @@
 //  Add your code here
+const { Schema, model } = require('mongoose');
+
+const celebritySchema = new Schema(
+    {
+        name: String,
+        ocuppation: String,
+        catchPhrase: { type: String, required: true },
+        participateMovies : [{type: Schema.Types.ObjectId, ref:'Movie'}]
+    }
+)
+
+const Celebrity = model('Celebrity', celebritySchema);
+module.exports = Celebrity;
