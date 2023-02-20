9c682501db622e88aa89b790a2eb5372ddb90ae6
Wilkins Morales <wilkinscom@gmail.com>
2023-02-20 18:51:25 +0200

    fix merge last try

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,23 +3,27 @@ const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model.js");
 // all your routes here
 
-router.get("/create", (req, res, next) => {
+router.get("/create-celebrity", (req, res, next) => {
   res.render("celebrities/new-celebrity");
 });
 
-router.post("/create", (req, res, next) => {
+router.post("/create-celebrity", (req, res, next) => {
   const { name, occupation, catchPhrase } = req.body;
-
-  //create new celebrity inside the database
-
   Celebrity.create({ name, occupation, catchPhrase })
-  /*.then((celebrityDB) => console.log(`New celebrity created ${celebrityDB.name}`)*/
-      .then(() => res.redirect("/celebrities"))
-      .catch((error) => next(error))
+    .then(() => res.redirect("/celebrities"))
+    .catch((error) => next(error));
 });
 
 router.get("/celebrities/", (req, res, next) => {
-  res.render("celebrities/celebrities", {Celebrity});
+  Celebrity.find().then((allCelebrities) => {
+    res.render("../views/celebrities/celebrities.hbs", {
+      celebrity: allCelebrities,
+    });
+  });
+});
+
+router.get("/celebrities/", (req, res, next) => {
+  res.render("celebrities/celebrities", { Celebrity });
 });
 
 module.exports = router;
