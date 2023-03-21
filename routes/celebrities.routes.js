e213dd588fd1ec323878b0b63544e4fd29cb00f1
Cristian Palao <cristianpalaocamarasa@gmail.com>
2023-03-21 17:07:17 +0100

    [IT. 3] Implemented functionality to add celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,7 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
-// ROUTES CODE HERE
+// require the models
+const Celebrity = require('../models/Celebrity.model');
 
+/* ROUTES HANDLING: */
+
+// GET /celebrities/create
+router.get('/celebrities/create', (req, res, next) => {
+    res.render('celebrities/new-celebrity');
+});
+
+// POST /celebrities/create
+router.post('/celebrities/create', (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+
+    Celebrity.create({name, occupation, catchPhrase})
+        .then(() => {
+            res.redirect('/celebrities');
+        })
+        .catch(err => {
+            res.render('/celebrities/new-celebrity', { errorMessage: 'There was an error creating the celebrity. Try again!'});
+        });
+})
 
 module.exports = router;
