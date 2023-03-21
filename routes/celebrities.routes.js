fdfc1156ebb7d96e535880e6499fb574b18c0fca
drose123456 <acg_danial@hotmail.com>
2023-03-21 16:48:55 +0000

    Pushing code

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const celebrity = require('../models/Celebrity.model');
+const router = express.Router();
+
+router.get("/celebrities/create", (req, res) => {
+    res.render("celebrities/new-celebrity");
+});
+
+router.post("/celebrities/create", (req, res) => {
+    const celebDetails = {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase
+    }
+celebrity.create(celebDetails)
+    .then(celebFromDB => {
+        res.redirect("/celebrities/new-celebrity");
+    })
+    .catch(e => {
+        console.log("Error trying to create a  new celebrity", e)
+        next(e);
+    });
+});
+
+module.exports = router;
