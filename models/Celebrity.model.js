ac1f10c01b9995949e412daf6cb06fc7ef1c9928
ABenat1988 <ale.benat@gmail.com>
2023-03-21 16:56:24 +0100

    Co-authored-by: Peter Sekan <sekan.pete@gmail.com>

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,18 @@
 //  Add your code here
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema(
+    {
+        name: String,
+        occupation: {
+            type: String,
+            enum: ["unknown", "actor", "singer", "comedian"]
+        },
+        catchPhrase: String,
+    }
+
+)
+
+
+const Celebrity = model("Celebrity", celebritySchema);
+module.exports = Celebrity;
