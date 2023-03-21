328663670c43070cb689268608af7e161f6ffa68
Pim Blom <pim.blom@icloud.com>
2023-03-21 20:39:11 +0100

    create page working and iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,24 +1,41 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
-const express = require("express")
+const express = require("express");
 
-
-router.get("/celebrities", (req, res, next) => {
-    res.send(" does it show")
-  
-});
 
 
 //create
 
-router.get('/celebrities/create' , (req, res, next) =>{
-    res.render('celebrities/new-celebrity')
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
 });
 
-router.post('/celebrities/create' ,  (req, res, next) =>{
-    Celebrity.create(req.body)  
-    res.render("/celebrities") 
-});
+router.post("/celebrities/create", (req, res, next) => {
+  const celebrityDetails = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
 
+  Celebrity.create(celebrityDetails)
+    .then((celebrityFromDB) => {
+      res.render("celebrities/new-celebrity");
+    })
+    .catch((e) => {
+      console.log(`Something wrong with creating celebrity`, e);
+    });
+});
 module.exports = router;
+
+//display
+
+router.get("/celebrities", (req, res, next) => {
+    Celebrity.find()
+    .then(celebsArr=>{
+        const data={
+            celebrities: celebsArr
+        }
+        res.render("celebrities/celebrities", data)
+    })
+  });
