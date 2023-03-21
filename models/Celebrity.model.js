0aede774c16536545684a1af577f54340198940f
Ismael Freitas <ismael.freitas@sapo.pt>
2023-03-21 15:57:08 +0000

    Implement Iteration 3

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,4 +1,4 @@
-const {Schema} = require("mongoose");
+const {Schema, model} = require("mongoose");
 
 
 const celebritySchema = new Schema(
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,30 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+router.get("/celebrities/create",(req, res, next) => {
+    res.render("celebrities/new-celebrity")
+});
+
+
+//POST
+router.post("/celebrities/create", (req, res, next) => {
+    const celebrityDetails = {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase,
+    };
+
+    Celebrity.create(celebrityDetails)
+        .then((result) => {
+            console.log(result);
+            res.redirect("/celebrities");
+        }).catch((err) => {
+            console.error(err);
+            next(err);
+        });
+    
+});
 
 module.exports = router;
