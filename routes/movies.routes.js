417abc595342e3340c9e3c711a860860065040d1
Ismael Freitas <ismael.freitas@sapo.pt>
2023-03-21 17:01:46 +0000

    Implement Iteration 7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -15,10 +15,34 @@ router.get("/movies/create", (req, res, next) => {
         });
 });
 
+router.get("/movies", (req, res, next) => {
+
+    Movie.find()
+        .then((moviesArr) => {
+
+            res.render("movies/movies", {movies: moviesArr});
+
+        }).catch((err) => {
+            next(err);
+        });
+});
+
 
 router.post("/movies/create", (req, res, next) => {
-    console.log(req.body);
-    res.redirect("/");
+    const movieDetails = {
+        title: req.body.title,
+        genre: req.body.genre,
+        plot: req.body.plot,
+        cast: req.body.cast
+    };
+
+    Movie.create(movieDetails)
+        .then(() => {
+            res.redirect("/movies");
+        }).catch((err) => {
+            next(err);
+        });
+
 });
 
 module.exports = router;
