39907e1b4678ffd5ce720eea700a35a17cfc1b08
denefi <denefi@posteo.de>
2023-03-21 18:07:55 +0100

    wip it.6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,14 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find({}).then((celebrityArr) => {
+    const data = { actors: celebrityArr };
+    res.render("movies/new-movie", data);
+  });
+});
 
 module.exports = router;
