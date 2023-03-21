a94e95b1f17260d12c36b2dc5742bd1c9d87ed07
Peter Sekan <sekan.pete@gmail.com>
2023-03-21 17:27:42 +0100

    IT.3 add functionality to add celebrity

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,25 @@
 const router = require("express").Router();
-const Celebrity = require('../models/Celebrity.model');
+const Celebrity = require("../models/Celebrity.model");
 
+// GET the create form
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
+
+// POST create a new celebrity
+router.post("/celebrities/create", (req, res, next) => {
+  // console.log(req.body);
+
+  Celebrity.create(req.body)
+    .then((result) => {
+      // console.log(result);
+      res.redirect("/celebrities");
+    })
+    .catch((err) => {
+      console.log("Could not create new celebrity. ", err);
+      console.error(err);
+      res.redirect("/celebrities/create");
+    });
+});
 
 module.exports = router;
