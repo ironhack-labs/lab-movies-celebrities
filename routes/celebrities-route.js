6055fbc41d3af146056ef32e8bd73a03a71ad9f2
valyy151 <marin.valenta1@gmail.com>
2023-03-21 17:34:54 +0100

    Commit before pull

--- routes/celebrities-route.js
+++ routes/celebrities-route.js
@@ -1,23 +1,19 @@
-const router = require("express").Router();
-const Celebrity = require("../models/Celebrity.model")
+const router = require('express').Router();
+const Celebrity = require('../models/Celebrity.model');
 
-router.get("/celebrities",(req,res)=>{
-    res.render("celebrities/list")
-})
+router.get('/celebrities', (req, res) => {
+	res.render('celebrities/list');
+});
 
+router.get('/celebrities/create', (req, res) => {
+	res.render('celebrities/create');
+});
 
-router.get("/celebrities/create",(req,res)=>{
-    res.render("celebrities/create")
-})
-
-
-
-router.post("/celebrities/create",(req, res) => {
-    const {name,occupation,catchPhrase}= req.body
-    Celebrity.create(name,occupation,catchPhrase).then(()=>res.redirect/celebrities).catch((err)=>console.log(err))
-
-})
-
-
+router.post('/celebrities/create', (req, res) => {
+	const { name, occupation, catchPhrase } = req.body;
+	Celebrity.create(name, occupation, catchPhrase)
+		.then(() => res.redirect / celebrities)
+		.catch((err) => console.log(err));
+});
 
 module.exports = router;
