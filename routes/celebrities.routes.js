ac4ade8570937ca5d9331ab8bfd91e09a3e3c469
Pim Blom <pim.blom@icloud.com>
2023-03-21 19:06:21 +0100

    create form doesn't proseed to next step

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,37 +1,24 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
+const express = require("express")
+
 
 router.get("/celebrities", (req, res, next) => {
-  Celebrity.find({})
-    .then((celebritiesArr) => {
-      res.render("celebrities/celebrities", { celebrity: celebritiesArr });
-    })
-    .catch((e) => {
-      console.log("error creating new celebrity", e);
-      next(e);
-    });
-});
-
-router.get("/celebrities/create", (req, res, next) => {
+    res.send(" does it show")
   
-  res.render("celebrities/new-celebrity");
 });
 
-router.post("celebrities/create", (req, res, next) => {
-  const celebrityDetails = {
-    name: req.body.name,
-    occupation: req.body.occupation,
-    catchPhrase: req.body.catchPhrase,
-  };
-  Celebrity.create(celebrityDetails)
-    .then((celebritiesFromDB) => {
-      res.redirect("/celebrities");
-    })
-    .catch((e) => {
-      console.log("error creating new celebrity", e);
-      next(e);
-    });
+
+//create
+
+router.get('/celebrities/create' , (req, res, next) =>{
+    res.render('celebrities/new-celebrity')
+});
+
+router.post('/celebrities/create' ,  (req, res, next) =>{
+    Celebrity.create(req.body)  
+    res.render("/celebrities") 
 });
 
 module.exports = router;
