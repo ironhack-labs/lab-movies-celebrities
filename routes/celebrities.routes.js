be994f2992dfd014c5e1541550c9c963fcf079a3
amina247 <aminakhedr7@gmail.com>
2023-03-21 17:22:30 +0100

    iteration 3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,14 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require('../models/Celebrity.model');
 
-// all your routes here
+router.get('/celebrities/create' , (req, res, next) =>{
+    res.render('celebrities/new-celebrity')
+});
+
+router.post('/celebrities/create' ,  (req, res, next) =>{
+    Celebrity.create(req.body)  
+    res.send("Created new Celebrity") 
+});
 
 module.exports = router;
