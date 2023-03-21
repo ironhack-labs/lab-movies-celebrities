620453882aa112136aa9ee5ed6a512a4ab7efc08
valyy151 <marin.valenta1@gmail.com>
2023-03-21 18:30:55 +0100

    Edit method implemented for movies

--- routes/movies-route.js
+++ routes/movies-route.js
@@ -24,4 +24,25 @@ router.get('/movies/:movieId', (req, res) => {
 		.catch((err) => console.log(err));
 });
 
+router.post('/movies/:movieId/delete', (req, res) => {
+	Movie.findByIdAndDelete(req.params.movieId)
+		.then(() => res.redirect('/movies'))
+		.catch((err) => console.log(err));
+});
+
+router.get('/movies/:movieId/edit', (req, res) => {
+	Movie.findById(req.params.movieId)
+		.then((movie) => res.render('movies/edit', { movie }))
+		.catch((err) => console.log(err));
+});
+
+router.post('/movies/:movieId/edit', (req, res) => {
+	const { Title, Genre, Cast, Plot } = req.body;
+	Movie.findByIdAndUpdate(req.params.movieId, { Title, Genre, Cast, Plot }, { new: true })
+		.then(() => {
+			res.redirect('/movies');
+		})
+		.catch((err) => console.log(err));
+});
+
 module.exports = router;
