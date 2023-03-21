e566d1b25a35f76899af869a7968166d4e56720b
Ismael Freitas <ismael.freitas@sapo.pt>
2023-03-21 16:39:36 +0000

    Implement Iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,24 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
+router.get("/movies/create", (req, res, next) => {
+
+    Celebrity.find()
+        .then((celebritiesArr) => {
+
+            res.render("movies/new-movie", { celebrities: celebritiesArr });
+        }).catch((err) => {
+            
+        });
+});
+
+
+router.post("/movies/create", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
+});
 
 module.exports = router;
