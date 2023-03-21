8862602f63c683d7efac51c87d46f7714da395f5
ABenat1988 <ale.benat@gmail.com>
2023-03-21 18:04:09 +0100

    iteration 7 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,23 +3,38 @@ const Celebrity = require("../models/Celebrity.model");
 
 // GET the create form
 router.get("/celebrities/create", (req, res, next) => {
-  res.render("celebrities/new-celebrity");
+    res.render("celebrities/new-celebrity");
 });
 
 // POST create a new celebrity
 router.post("/celebrities/create", (req, res, next) => {
-  // console.log(req.body);
+    // console.log(req.body);
 
-  Celebrity.create(req.body)
-    .then((result) => {
-      // console.log(result);
-      res.redirect("/celebrities");
-    })
-    .catch((err) => {
-      console.log("Could not create new celebrity. ", err);
-      console.error(err);
-      res.redirect("/celebrities/create");
-    });
+    Celebrity.create(req.body)
+        .then((result) => {
+            // console.log(result);
+            res.redirect("/celebrities");
+        })
+        .catch((err) => {
+            console.log("Could not create new celebrity. ", err);
+            console.error(err);
+            res.redirect("/celebrities/create");
+        });
 });
 
+
+router.get("/celebrities", (req, res, next) => {
+    Celebrity.find()
+        .then((response) => {
+            const data = {
+                celebritiesArray: response
+            };
+            res.render("celebrities/celebrities", data)
+
+        })
+        .catch((err) => {
+            console.log("Could not find celebrities. ", err);
+            console.error(err);
+        });
+});
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,4 +1,54 @@
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+
+
+// GET the create form
+router.get("/movies/create", (req, res, next) => {
+    Celebrity.find()
+        .then((response) => {
+            const data = {
+                celebritiesArray: response
+            };
+
+            res.render("movies/new-movie", data);
+        })
+        .catch((err) => {
+            console.log("error when finding celebreties list. ", err);
+            console.error(err);
+        });
+});
+
+// POST create a new movie
+router.post("/movies/create", (req, res, next) => {
+    // console.log(req.body);
+    Movie.create(req.body)
+        .then((result) => {
+            // console.log(result);
+            res.redirect("/movies");
+        })
+        .catch((err) => {
+            console.log("Could not create new movie. ", err);
+            console.error(err);
+            res.redirect("/movies/create");
+        });
+});
+
+router.get("/movies", (req, res, next) => {
+    Movie.find()
+        .then((response) =>{
+            const data ={
+                moviesArray : response
+            };
+            res.render('movies/movies', data);
+        })
+        .catch((err) => {
+            console.log("Could not find movies from DB. ", err);
+            console.error(err);
+        });
+
+})
+
 
 
 module.exports = router;
