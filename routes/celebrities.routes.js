f12c08541f427ee2f5bcde8abca2fb6b76392598
Ismael Freitas <ismael.freitas@sapo.pt>
2023-03-21 19:04:39 +0000

    Implement Bonus Iterations Finally

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,7 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model")
 
 // all your routes here
 router.get("/celebrities/create",(req, res, next) => {
@@ -18,6 +19,30 @@ router.get("/celebrities", (req, res, next) => {
             next(err);
         });
 });
+
+router.get("/celebrities/:celebrityId", (req, res, next) => {
+    const {celebrityId} = req.params;
+
+    Celebrity.findById(celebrityId)
+        .then((celebrity) => {
+            res.render("celebrities/celebrity-details", {celebrity});
+        }).catch((err) => {
+            next(err);
+        });
+});
+
+router.get("/celebrities/:celebrityId/edit", (req, res, next) => {
+    const {celebrityId} = req.params;
+
+    Celebrity.findById(celebrityId)
+        .then((celebrity) => {
+            res.render("celebrities/edit-celebrity", {celebrity});
+        }).catch((err) => {
+            next(err)
+        });
+
+});
+
 
 
 //POST
@@ -37,5 +62,52 @@ router.post("/celebrities/create", (req, res, next) => {
         });
     
 });
+
+router.post("/celebrities/:celebrityId/edit", (req, res, next) => {
+    const { celebrityId } = req.params;
+    const { name, occupation, catchPhrase } = req.body;
+
+    Celebrity.findByIdAndUpdate(celebrityId, { name, occupation, catchPhrase }, { new: true })
+    .then(() => {
+        res.redirect("/celebrities");
+    })
+    .catch(error => next(error));
+});
+
+//DELETE
+router.post("/celebrities/:celebrityId/delete", (req, res, next) => {
+    async function deleteCelebrity(){
+        try{
+
+            const {celebrityId} = req.params;
+            //Get all movies
+            const moviesArr = await Movie.find();
+
+            //Remove the celebrity from its movies
+            const celebMovies = moviesArr.map((movie) => {
+
+                movie.cast.forEach(objectId => {
+                    if(toString(objectId) === toString(celebrityId)) {
+                        const idPos = movie.cast.indexOf(objectId);
+                        movie.cast.splice(idPos, 1);
+                    }
+                });
+            });
+
+            //delete celebrity document
+            await Celebrity.findByIdAndDelete(celebrityId);
+            console.log("Celebrity deleted!");
+
+            res.redirect("/celebrities");
+
+
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    deleteCelebrity();
+});
+
 
 module.exports = router;
