1f3530c6dbd1ceaa42203939be0d291b059d4007
denefi <denefi@posteo.de>
2023-03-21 17:45:10 +0100

    sove it. 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,14 +1,25 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-const Celebrity = require('../models/Celebrity.model');
+const Celebrity = require("../models/Celebrity.model");
 
-router.get('/celebrities/create' , (req, res, next) =>{
-    res.render('celebrities/new-celebrity')
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
 });
 
-router.post('/celebrities/create' ,  (req, res, next) =>{
-    Celebrity.create(req.body)  
-    res.send("Created new Celebrity") 
+router.post("/celebrities/create", (req, res, next) => {
+  Celebrity.create(req.body);
+  res.send("Created new Celebrity");
+});
+
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find({})
+    .then((celebritiesArr) => {
+      const data = {
+        celebrities: celebritiesArr,
+      };
+      res.render("celebrities/celebrities", data);
+    })
+    .catch((err) => next(err));
 });
 
 module.exports = router;
