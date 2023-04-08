9e8319c3a10cad07278e110481a54001e17b0049
“Zuzana <zuzana@getyourguide.com>
2023-04-08 16:20:41 +0200

    Iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,19 +1,31 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const CelebrityModel = require("../models/Celebrity.model");
 
-// all your routes here
+// route to display the new celebrity form / creation 
 router.get("/create", (req,res) => {
     res.render("celebrities/new-celebrity");
 });
 
+// route to post /submit the new celebrity 
 router.post("/create", async (req,res) => {
     try {
         const newCelebrity = await CelebrityModel.create(req.body);
-        res.redirect("/celebrities");
+        res.redirect("/celebrities/celebrities");
+    }
+    catch (err) {
+        console.log(err);
+        res.redirect("/celebrities/new-celebrity")};
+});
+
+// route to disaplay the list of created celebrities 
+router.get("/celebrities", async (req,res) => {
+    try {
+        const allCelebrities = await CelebrityModel.find();
+        res.render("celebrities/celebrities", {allCelebrities});
     }
     catch (err) {
         console.log(err)};
-        res.redirect("celebrities/new-celebrity");
 });
 
 module.exports = router;
