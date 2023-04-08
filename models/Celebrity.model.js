b723b82c7ce05c13e9be6901322ca0756cbb0b43
olgam <olgamartyniuk88@gmail.com>
2023-04-08 14:51:21 +0200

    it. 1 and 2 done

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,13 @@
 //  Add your code here
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+ 
+const celebSchema = new Schema({
+  name: String,
+  occupation: String,
+  catchPhrase: String,
+});
+ 
+const CelebModel = mongoose.model('celeb', celebSchema);
+ 
+module.exports = CelebModel;
