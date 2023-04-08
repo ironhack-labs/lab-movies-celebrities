a9e50df987d5d0526cac93d08403f6a763060586
olgam <olgamartyniuk88@gmail.com>
2023-04-08 15:51:57 +0200

    it. 3 done

--- routes/celebs.routes.js
+++ routes/celebs.routes.js
@@ -1,5 +1,20 @@
+const CelebModel = require("../models/Celebrity.model");
+
 const router = require("express").Router();
 
-// all your routes here
+router.get("/create", (req, res) => {
+  res.render("celebs/new-celeb");
+});
+
+router.post("/create", async (req, res) => {
+    try {
+        const newCelebCreated = await CelebModel.create(req.body);
+        res.redirect('/celebs');
+    } catch {
+        res.redirect('/create');
+    }
+   
+
+  });
 
 module.exports = router;
