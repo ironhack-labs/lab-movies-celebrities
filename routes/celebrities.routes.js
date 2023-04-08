0b06434b402c03bc6f1a9d2625ad087ae58d77cd
Lena Imdahl <mail@lenaimdahl.de>
2023-04-08 16:25:55 +0200

    router post for create celebtrity

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,11 +1,12 @@
 const router = require("express").Router();
+const CelebrityModel = require("../models/Celebrity.model");
 
 router.get("/create", (req, res) => {
   res.render("celebrities/new-celebrity");
 });
 
 router.post("/create", async (req, res) => {
-  const newCelebrity = await celebrityModel.create(req.body);
+  const newCelebrity = await CelebrityModel.create(req.body);
   console.log("New celebrity Created ", newCelebrity);
   res.redirect(`celebrities/new-celebrity${newCelebrity._id}`);
 });
