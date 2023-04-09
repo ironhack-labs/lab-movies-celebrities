12592174b948a4f5189e7c193d8bc1b93993bdfd
Michel Saber <michel-saber@hotmail.com>
2023-04-09 17:44:31 +0200

    day2

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,43 @@
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+require("../db");
 
-// all your routes here
+router.get("/movies/create", async (req, res) => {
+  const allCelebrities = await Celebrity.find();
+  res.render("movies/new-movies", { allCelebrities });
+});
+
+router.get("/movies", async (req, res) => {
+  try {
+    const allMovies = await Movie.find();
+    res.render("movies/movies", { allMovies });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+router.post("/movies/create", async (req, res) => {
+  try {
+    const { title, genre, plot, cast } = req.body;
+    await Movie.create({ title, genre, plot, cast });
+    res.redirect("/movies");
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//creating a movie details route
+
+router.get("/movies/:id", (req, res) => {
+  const { id } = req.params;
+  Movie.findById(id)
+    .populate({
+      path: "cast",
+      select: "-_id name occupation catchPhrase",
+    })
+    .then((movie) => res.render("movies/movie-detail", movie))
+    .catch((err) => console.error(err));
+});
 
 module.exports = router;
---
+++ views/movies/movie-detail.hbs
@@ -0,0 +1,1 @@
+<h1>Movie Detail</h1>
