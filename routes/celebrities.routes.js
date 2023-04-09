63904723f9dc2c67d803131357ad0c2e15636b45
“Zuzana <zuzana@getyourguide.com>
2023-04-09 18:13:29 +0200

    more styling and fixed movie form

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -2,33 +2,70 @@
 const router = require("express").Router();
 const CelebrityModel = require("../models/Celebrity.model");
 
-// route to display the new celebrity form / creation 
-router.get("/create", (req,res) => {
-    res.render("celebrities/new-celebrity");
+// route to display the new celebrity form / creation
+router.get("/create", (req, res) => {
+  res.render("celebrities/new-celebrity");
 });
 
-// route to post /submit the new celebrity 
-router.post("/create", async (req,res) => {
-    try {
-        const newCelebrity = await CelebrityModel.create(req.body);
-        res.redirect("/celebrities/celebrities");
-    }
-    catch (err) {
-        console.log(err);
-        res.redirect("/celebrities/new-celebrity")};
+// route to post /submit the new celebrity
+router.post("/create", async (req, res) => {
+  try {
+    const newCelebrity = await CelebrityModel.create(req.body);
+    res.redirect("/celebrities/celebrities");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/celebrities/new-celebrity");
+  }
 });
 
-// route to disaplay the list of created celebrities 
-router.get("/celebrities", async (req,res) => {
-    try {
-        const allCelebrities = await CelebrityModel.find();
-        res.render("celebrities/celebrities", {allCelebrities});
-    }
-    catch (err) {
-        console.log(err)};
+// route to display the list of created celebrities
+router.get("/celebrities", async (req, res) => {
+  try {
+    const allCelebrities = await CelebrityModel.find();
+    res.render("celebrities/celebrities", { allCelebrities });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// route to show each celebrity details
+router.get("/:id", async (req, res) => {
+  try {
+    const oneCelebrity = await CelebrityModel.findById(req.params.id);
+    console.log(oneCelebrity);
+    res.render("celebrities/celebrity-details", { oneCelebrity });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// route to delete a celebrity
+router.post("/:id/delete", (req, res, next) => {
+  const { id } = req.params;
+  CelebrityModel.findByIdAndDelete(id)
+    .then(() => res.redirect("/celebrities/celebrities"))
+    .catch((error) => next(error));
+});
+
+// route to edit celebrity page
+router.get("/:id/edit", async (req, res) => {
+  try {
+      const oneCelebrity = await CelebrityModel.findById(req.params.id);
+      res.render("celebrities/edit-celebrity", { oneCelebrity });
+  } catch (err) {console.log(err)}
+});
+
+// route to display edited celebrity
+router.post("/:id", async (req, res) => {
+  try {
+      const newCelebValues = req.body;
+      const { id } = req.params;
+      const editedCelebrity = await CelebrityModel.findByIdAndUpdate(id, newCelebValues, {new: true});
+    res.redirect(`/celebrities/${id}`);
+  } catch (err) {
+      console.log(err);
+      res.redirect("/celebrities/celebrity-details");
+  }
 });
 
 module.exports = router;
-
-
-
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -3,92 +3,73 @@ const router = require("express").Router();
 const MovieModel = require("../models/Movie.model");
 const CelebrityModel = require("../models/Celebrity.model");
 
+// route to display the new celebrity form / creation
+router.get("/create", async (req, res) => {
+  const celebrities = await CelebrityModel.find();
+  res.render("movies/new-movie", { celebrities });
+});
 
-// route to display the new celebrity form / creation 
-router.get("/create", async (req,res) => {
+// route to post /submit the new movie
+router.post("/create", async (req, res) => {
+  try {
+    const newMovie = await MovieModel.create(req.body);
+    res.redirect("/movies/movies");
+  } catch (err) {
+    console.log(err);
+    res.redirect("/movies/new-movie");
+  }
+});
+
+// route to display the list of created movie
+router.get("/movies", async (req, res) => {
+  try {
+    const allMovies = await MovieModel.find();
+    console.log(allMovies);
+    res.render("movies/movies", { allMovies });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// route to show each movie details
+router.get("/:id", async (req, res) => {
+  try {
+    const eachMovie = await MovieModel.findById(req.params.id).populate("cast");
+    console.log(eachMovie);
+    res.render("movies/movie-details", { eachMovie });
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// route to delete a movie
+router.post("/:id/delete", (req, res, next) => {
+  const { id } = req.params;
+  MovieModel.findByIdAndDelete(id)
+    .then(() => res.redirect("/movies/movies"))
+    .catch((error) => next(error));
+});
+
+// route to edit movie page
+router.get("/:id/edit", async (req, res) => {
+  try {
+    const eachMovie = await MovieModel.findById(req.params.id);
     const celebrities = await CelebrityModel.find();
-    res.render("movies/new-movie", {celebrities});
+    res.render("movies/edit-movie", { eachMovie, celebrities });
+  } catch (err) {console.log(err)}
 });
 
-// route to post /submit the new celebrity 
-router.post("/create", async (req,res) => {
-    try {
-        const newMovie = await MovieModel.create(req.body);
-        res.redirect("/movies/movies");
-    }
-    catch (err) {
-        console.log(err);
-        res.redirect("/movies/new-movie")};
+// route to display edited movie
+router.post("/:id", async (req, res) => {
+  try {
+      const newValues = req.body;
+      const { id } = req.params;
+      const editedMovie = await MovieModel.findByIdAndUpdate(id, newValues, {new: true});
+      res.redirect(`/movies/${id}`);
+  } catch (err) {
+      console.log(err);
+      res.redirect("/movies/movie-details");
+  }
 });
-
-
-// route to disaplay the list of created celebrities 
-router.get("/movies", async (req,res) => {
-    try {
-        const allMovies = await MovieModel.find();
-        console.log(allMovies);
-        res.render("movies/movies", {allMovies});
-    }
-    catch (err) {
-        console.log(err)};
-});
-
-
-
-router.get("/:id", async (req,res) => {
-    try {
-        const eachMovie = await MovieModel.findById(req.params.id).populate("cast");
-        console.log(eachMovie);
-        res.render("movies/movie-details", {eachMovie});
-    }
-    catch (err) {
-        console.log(err)};
-});
-
-// router.post("/:id/delete", async (req,res,next) => {
-//     const {id} = req.params;
-//     try {
-//         await MovieModel.findByIdAndRemove(id)
-//         // console.log(eachMovie);
-//         res.redirect("movies/movies");
-//     }
-//     catch (err) {
-//         console.log(err)};
-// });
-
-router.post('/:id/delete', (req, res, next) => {
-    const { id } = req.params;
-    MovieModel.findByIdAndDelete(id)
-    .then(() => res.redirect('/movies/movies'))
-    .catch(error => next(error));
-    });
-
-
-
-
-router.get("/:id/edit", async (req,res) => {
-        try {
-            const eachMovie = await MovieModel.findById(req.params.id);
-            const celebrities = await CelebrityModel.find();
-            res.render("movies/edit-movie", {eachMovie, celebrities});
-        }
-        catch (err) {console.log(err)};
-    });
-    
-router.post("/:id", async (req,res) => {
-        try {
-                const newValues = req.body;
-                const {id} = req.params;
-                const editedMovie = await MovieModel.findByIdAndUpdate(id, newValues, {new: true});
-                res.redirect(`/movies/${id}`);
-        }
-        catch (err) {
-            console.log(err);
-            res.redirect("/movies/movie-details")};
-    });
-
 
 module.exports = router;
-
-
-
