fa285f4e1a10a25f2428f80c67f34d8b2f9c278c
Krisztian Kiss <kisskrisztian215@gmail.com>
2023-04-10 23:26:01 +0200

    Added Route for Celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,23 +1,30 @@
 const router = require("express").Router();
+const celebrityModel = require("../models/Celebrity.model");
 const CelebrityModel = require("../models/Celebrity.model");
 
 router.get("/create", (req, res) => {
   res.render("celebrities/new-celebrity");
 });
 
-router.get("/all", (req, res) => {
-  res.render("celebrities/celebrities");
+router.get("/all", async (req, res) => {
+  try{
+    const allCelebrities = await CelebrityModel.find();
+    res.render("celebrities/celebrities", {allCelebrities});
+  }catch(err){
+     console.log("There was an error", err);
+  }
 });
 
 router.post("/create", async (req, res) => {
   try {
     const newCelebrity = await CelebrityModel.create(req.body);
     console.log("New celebrity Created ", newCelebrity);
-    res.redirect("/celebreties/all");
+    res.redirect("/celebrities/all");
   } catch (err) {
     console.log("there was an error", err);
-    res.redirect("/celebrities/create");
+    res.redirect("/celebrities/new-celebrity");
   }
 });
+
 
 module.exports = router;
