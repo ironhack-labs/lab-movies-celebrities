b4c010333f846c145df349bd9e3ea55ffa085f3d
olgam <olgamartyniuk88@gmail.com>
2023-04-13 18:17:24 +0200

    movie detail page doesn't display details

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -5,8 +5,14 @@ const router = require("express").Router();
 
 // all your routes here
 router.get("/movieslist", async (req, res) => {
-  const allMovies = await MovieModel.find();
-  res.render("movies/movies", { allMovies });
+  try {
+    const allMovies = await MovieModel.find();
+    res.render("movies/movies", { allMovies });
+
+  } catch (err) {
+    console.log("movie list page error", err);
+  }
+
 });
 
 router.get("/create", async (req, res) => {
@@ -23,6 +29,43 @@ router.post("/create", async (req, res) => {
     res.redirect("/movies/create");
     console.log("movie post error", err);
   }
+});
+
+router.get("/:id", async (req, res) => {
+
+    const { movieID } = req.params;
+    const currentMovie = await MovieModel.findById(movieID).populate("cast");
+    console.log("movie from ID page", currentMovie); 
+    res.render("movies/movie-details", { currentMovie });
+  }); 
+  
+
+router.post("/:id/delete", async (req, res) => {
+  try {
+
+    const { movieID } = req.params;
+    await MovieModel.findByIdAndDelete(movieID);
+    res.redirect("/movies/movieslist");
+  
+  } catch (err) {
+    console.log("movie post error", err);
+    res.send ("Oops, problem while deleting, try again.");
+  }
+});
+
+router.get("/:id/edit", async(req,res) => {
+
+  const { movieID } = req.params;
+  const movieToEdit = await MovieModel.findById(movieID).populate("cast");
+  const allCelebs = await CelebModel.find();
+
+  res.render("movies/edit-movie", { movieToEdit, allCelebs} );
+});
+
+router.post("/:id", async(req,res) => {
+  const { movieID } = req.params;
+  const updatedMovie = await MovieModel.findByIdAndUpdate( { _id: movieID }, req.body);
+  res.redirect(`/movies/${movieID}`);
 });
 
 //   router.get("/celebs", async (req,res) => {
