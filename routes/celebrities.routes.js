ba5a045a20dd80f71feeb6e1d6233261b6d6c8fb
Nicolas_M <nicolas.meot@live.fr>
2023-04-15 15:01:54 +0200

    3rd iteration

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,7 +1,33 @@
+const Celebrity = require("../models/Celebrity.model");
+
 const router = require("express").Router();
 
-router.get('/celebrities/celebrities', (req, res, next) => res.render('celebrities'));
+router.get('/celebrities', (req, res, next) => {
+    Celebrity
+        .find()
+        .then(listFromDB => {
+            const data = {celebrity : listFromDB} 
+            console.log(data)
+            res.render('celebrities/celebrities',data)
+        })
+        .catch(err => console.log(`an error happened ${err}`));
+})
 
-router.get('/', (req, res, next) => res.render('new-celebrity'));
+router.get('/celebrity/create' , (req, res, next) =>{
+    res.render('celebrities/new-celebrity')
+});
+
+router.post('/celebrity/create', (req, res, next) => {
+    const {name, occupation, catchphrase} = req.body
+    Celebrity.create({ name, occupation, catchphrase })
+        .then(() => {
+            res.redirect('/celebrities')
+            console.log({name, occupation, catchphrase})
+        })
+        .catch(err => {
+            res.render('celebrities/new-celebrity', { errorMessage: 'There was an error creating the celebrity. Try again!'});
+        });
+    //res.render('new-celebrity'));
+} ),
 
 module.exports = router;
