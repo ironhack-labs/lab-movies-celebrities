11400460f3e9c4d6f49f1117d90b2c88c6c038b9
AZRGUI-MOHAMED <azrgui.m@gmail.com>
2023-04-15 12:42:59 +0200

    iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,10 @@
-//  Add your code here
+const {Schema, model} = require('mongoose');
+
+const celebritySchema = new Schema ({
+    name: String,
+    occupation: String, 
+    catchPhrase: String,
+});
+
+const celebrity = model("Celebrity", celebritySchema);
+module.exports = celebrity;
