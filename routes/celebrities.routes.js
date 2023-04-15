08b215b66a61c7ebf8af9f0c112c57a275386f46
Cedric Aim <cedric.aim@gmail.com>
2023-04-15 15:07:56 +0200

    iteration 4 et 5

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,17 +1,26 @@
 const router = require("express").Router();
-const Celebrity = require("../models/Celebrity.model.js")
+const Celebrity = require("../models/Celebrity.model.js");
+
+router.get("/", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("celebrities/celebrities", { celebrities });
+    })
+    .catch((error) => {
+      console.log("Error when listing celebrities", error);
+      next(error);
+    });
+});
 
 router.get("/create", (req, res, next) => {
-    res.render("celebrities/new-celebrity");
+  res.render("celebrities/new-celebrity");
 });
-router.post('/create', (req, res, next) => {
-    const { name, occupation, catchphrase } = req.body;
-    console.log(name, occupation, catchphrase);
-    Celebrity.create({ name, occupation, catchphrase })
-        .then(celebrityFromDB => res.render('celebrities/celebrities'))
-        .catch(error => res.render('celebrities/new-celebrity'));
-})
+router.post("/create", (req, res, next) => {
+  const { name, occupation, catchphrase } = req.body;
 
-
+  Celebrity.create({ name, occupation, catchphrase })
+    .then((celebrityFromDB) => res.redirect("/celebrities"))
+    .catch((error) => res.render("celebrities/new-celebrity"));
+});
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,35 @@
 const router = require("express").Router();
+const Movie = require("../models/Movie.model.js");
+const Celebrity = require("../models/Celebrity.model.js");
 
-// all your routes here
+router.get("/", (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render("movies/movies", { movies });
+    })
+    .catch((error) => {
+      console.log("Error when listing movies", error);
+      next(error);
+    });
+});
+
+router.get("/create", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("movies/new-movie", { celebrities });
+    })
+    .catch((error) => {
+      console.log("Error when searching celebrities", error);
+      next(error);
+    });
+});
+
+router.post("/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+
+  Movie.create({ title, genre, plot, cast })
+    .then((movieFromDB) => res.redirect("/movies"))
+    .catch((error) => res.render("movies/new-movie"));
+});
 
 module.exports = router;
