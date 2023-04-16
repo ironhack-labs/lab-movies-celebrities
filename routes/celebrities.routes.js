dcd297c2050a634a244c0ae8b129bc50ad34196c
Jon <jon_ander_23@protonmail.com>
2023-04-16 09:06:43 +0200

    First iteration

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,7 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+// all your routes here
+router.get("/celebrities", (req, res, next) => {});
+
+module.exports = router;
---
+++ routes/movies.routes.js
@@ -0,0 +1,7 @@
+// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
+const router = require("express").Router();
+
+// all your routes here
+router.get("/movies", (req, res, next) => {});
+
+module.exports = router;
