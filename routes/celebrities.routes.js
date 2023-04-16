6898a034293987f9480b33cde1c80c9fa66ab20d
Ayoze Martin <ayozemt@gmail.com>
2023-04-16 19:56:30 +0200

    Iteration 7

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,30 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+const Celebrity = require('../models/Celebrity.model');
+
 // all your routes here
+router.get('/celebrities/create', (req, res, next) => res.render('celebrities/new-celebrity.hbs'));
+
+router.post('/celebrities/create', (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+
+    Celebrity.create({ name, occupation, catchPhrase })
+    // .then(celebrityFromDB => console.log(`New celebrity created: ${celebrityFromDB.name}.`))
+    .then(() => res.redirect('/celebrities'))
+    .catch(error => next(error));
+});
+
+router.get('/celebrities', (req, res, next) => {
+    Celebrity.find()
+    .then(allTheCelebritiesFromDB => {
+        console.log('Retrieved celebrities from DB:', allTheCelebritiesFromDB);
+        res.render('celebrities/celebrities.hbs', { celebrities: allTheCelebritiesFromDB});
+    })
+    .catch(error => {
+        console.log('Error while getting the celebrities from the DB: ', error);
+        next(error);
+    });
+});
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,36 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+const Celebrity = require('../models/Celebrity.model');
+const Movie = require('../models/Movie.model');
+
 // all your routes here
+router.get('/movies/create', (req, res, next) => {
+    Celebrity.find()
+    .then(allTheCelebritiesFromDB => {
+        res.render('movies/new-movie.hbs', { celebrities: allTheCelebritiesFromDB });
+    })
+    .catch(error => next(error));
+});
+
+router.post('/movies/create', (req, res, next) => {
+    const { title, genre, plot, cast } = req.body;
+
+    Movie.create({ title, genre, plot, cast })
+    .then(() => res.redirect('/movies'))
+    .catch(error => next(error));
+});
+
+router.get('/movies', (req, res, next) => {
+    Movie.find()
+    .then(allTheMoviesFromDB => {
+        console.log('Retrieved movies from DB:', allTheMoviesFromDB);
+        res.render('movies/movies.hbs', { movies: allTheMoviesFromDB });
+    })
+    .catch(error => {
+        console.log('Error while getting the movies from the DB: ', error);
+        next(error);
+    });
+});
 
 module.exports = router;
