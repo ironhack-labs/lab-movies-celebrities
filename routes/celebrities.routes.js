4f97397fcb4feeaedae84bd2ceef6bc640e0f441
Jon <jon_ander_23@protonmail.com>
2023-04-16 10:19:09 +0200

    Third iteration

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,7 +1,25 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+// require Celebrity model in order to use it
+const Celebrity = require("../models/Celebrity.model");
+
 // all your routes here
-router.get("/celebrities", (req, res, next) => {});
+router.get("/celebrities", (req, res, next) => {
+  res.render("celebrities/celebrities");
+});
+
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity");
+});
+
+router.post("/create", (req, res, next) => {
+  const { celebrity } = req.body;
+  Celebrity.create({ celebrity })
+    .then(() => res.redirect("/celebrities"))
+    .catch((err) =>
+      console.log(`Error while creating a new celebrity: ${err}`)
+    );
+});
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,6 +2,20 @@
 const router = require("express").Router();
 
 // all your routes here
-router.get("/movies", (req, res, next) => {});
+router.get("/movies", (req, res, next) => {
+  res.render("movies.hbs");
+});
+
+router.get("/new-movie", (req, res, next) => {
+  res.render("new-movie.hbs");
+});
+
+router.get("/movie-details", (req, res, next) => {
+  res.render("movie-details.hbs");
+});
+
+router.get("/edit-movie", (req, res, next) => {
+  res.render("edit-movie.hbs");
+});
 
 module.exports = router;
