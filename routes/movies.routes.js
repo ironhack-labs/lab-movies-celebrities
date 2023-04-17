318283419eddd72e787427cbb665d72c5c312afb
Rayane <alirayane14@gmail.com>
2023-04-17 20:55:12 +0200

    iteration 7

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -4,32 +4,40 @@ const Celebrity = require("../models/Celebrity.model");
 const Movie = require("../models/Movie.model");
 
 router.get("/create", (req, res, next) => {
-    Celebrity.find()
-    .then((celebritiesFromDb)=>{
-        res.render("movies/new-movie", {allCelebrities: celebritiesFromDb});
+  Celebrity.find()
+    .then((celebritiesFromDb) => {
+      res.render("movies/new-movie", { allCelebrities: celebritiesFromDb });
     })
-    .catch((err)=>{
-        console.log("erreur",err)
-        next(err);
-    })
-  });
+    .catch((err) => {
+      console.log("erreur", err);
+      next(err);
+    });
+});
 
-  router.post("/create", (req, res, next) => {
-    const movies = req.body
-    Movie.create({
-      title: req.body.title,
-      genre: req.body.genre,
-      plot: req.body.plot,
-      cast: req.body.cast,
+router.post("/create", (req, res, next) => {
+  const movies = req.body;
+  Movie.create({
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot,
+    cast: req.body.cast,
+  })
+    .then((celebrityFromDB) => {
+      res.redirect("/movies");
     })
-      .then((celebrityFromDB) => {
-        res.redirect("/movies");
-      })
-      .catch((err) => {
-        next(err);
-      });
-  });
-  
+    .catch((err) => {
+      next(err);
+    });
+});
 
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((moviesFromDB) => {
+      res.render("movies/movies", { allMovies: moviesFromDB });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
