0b6a5ac2571411643d8f8b6570d25ce013f98f65
José Antonio Escudero García <jaeg90@gmail.com>
2023-04-17 15:43:17 +0200

    Iteration 6 in progress

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,3 +1,36 @@
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+
+//GET Movies
+router.get("/", async (req, res) => {
+  const movies = await Movie.find();
+  res.render("movies/movies", { movies });
+});
+
+// GET create
+router.get("/create", (req, res) => {
+  res.render("movies/new-movie");
+});
+
+// POST create
+router.post("/create", (req, res) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.findOne({ title, genre, plot, cast })
+    .then((result) => {
+      if (!result) {
+        Movie.create({ title, genre, plot, cast }).then(() =>
+          res.redirect("/movies")
+        );
+      } else {
+        res.render("movies/new-movies", {
+          message: "It seems it is already created",
+        });
+        return;
+      }
+    })
+    .catch((err) => {
+      console.log(`Error while creating a new user: ${err}`);
+    });
+});
 
 module.exports = router;
