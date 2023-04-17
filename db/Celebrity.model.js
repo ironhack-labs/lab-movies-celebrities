3336438b63c84ea87fca1d5a34ef05ec8c523c0f
José Antonio Escudero García <jaeg90@gmail.com>
2023-04-17 12:29:44 +0200

    Iteration 2 done

---
+++ db/Celebrity.model.js
@@ -0,0 +1,16 @@
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Celebrity = model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
