800081212569e2472278ef042fcc096fe0aebc8d
Ayoze Martin <ayozemt@gmail.com>
2023-04-17 13:04:14 +0200

    Solved lab

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,61 +1,87 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
-const Celebrity = require('../models/Celebrity.model');
-const Movie = require('../models/Movie.model');
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 
 // all your routes here
-router.get('/movies/create', (req, res, next) => {
-    Celebrity.find()
-    .then(allTheCelebritiesFromDB => {
-        res.render('movies/new-movie.hbs', { celebrities: allTheCelebritiesFromDB });
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find()
+    .then((allTheCelebritiesFromDB) => {
+      res.render("movies/new-movie.hbs", {
+        celebrities: allTheCelebritiesFromDB,
+      });
     })
-    .catch(error => next(error));
+    .catch((error) => next(error));
 });
 
-router.post('/movies/create', (req, res, next) => {
-    const { title, genre, plot, cast } = req.body;
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
 
-    Movie.create({ title, genre, plot, cast })
-    .then(() => res.redirect('/movies'))
-    .catch(error => next(error));
+  Movie.create({ title, genre, plot, cast })
+    .then(() => res.redirect("/movies"))
+    .catch((error) => next(error));
 });
 
-router.get('/movies', (req, res, next) => {
-    Movie.find()
-    .then(allTheMoviesFromDB => {
-        console.log('Retrieved movies from DB:', allTheMoviesFromDB);
-        res.render('movies/movies.hbs', { movies: allTheMoviesFromDB });
+router.get("/movies/:movieId/edit", (req, res, next) => {
+  const { movieId } = req.params;
+  let movie;
+
+  Movie.findById(movieId)
+    .then((theMovie) => {
+        movie = theMovie;
+        return Celebrity.find()
     })
-    .catch(error => {
-        console.log('Error while getting the movies from the DB: ', error);
-        next(error);
+    .then((allTheCelebritiesFromDB) => res.render("movies/edit-movie.hbs", { movie, celebrity: allTheCelebritiesFromDB }))
+    .catch((error) => next(error));
+});
+
+router.post("/movies/:movieId/edit", (req, res, next) => {
+  const { movieId } = req.params;
+  const { title, genre, plot, cast } = req.body;
+
+  Movie.findByIdAndUpdate(movieId, { title, genre, plot, cast }, { new: true })
+    .then((editedMovie) => res.redirect(`/movies/${editedMovie.id}`))
+    .catch((error) => next(error));
+});
+
+router.post("/movies/:movieId/delete", (req, res, next) => {
+  const { movieId } = req.params;
+
+  Movie.findByIdAndRemove(movieId)
+    .then(() => res.redirect("/movies"))
+    .catch((error) => {
+      console.log("Error while deleting movie: ", error);
+
+      next(error);
     });
 });
 
-router.get('/movies/:movieId', (req, res, next) => {
-    const { movieId } = req.params;
-  
-    Movie.findById(movieId)
-      .populate('cast')
-      .then((theMovie) => res.render('movies/movie-details.hbs', { movie: theMovie }))
-      .catch((error) => {
-        console.log('Error while retrieving movie details: ', error);
-  
-        next(error);
-      });
-  });
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    .then((allTheMoviesFromDB) => {
+      console.log("Retrieved movies from DB:", allTheMoviesFromDB);
+      res.render("movies/movies.hbs", { movies: allTheMoviesFromDB });
+    })
+    .catch((error) => {
+      console.log("Error while getting the movies from the DB: ", error);
+      next(error);
+    });
+});
 
-router.post('/movies/:movieId/delete', (req, res, next) => {
-    const { movieId } = req.params;
+router.get("/movies/:movieId", (req, res, next) => {
+  const { movieId } = req.params;
 
-    Movie.findByIdAndRemove(movieId)
-      .then(() => res.redirect('/movies'))
-      .catch((error) => {
-        console.log('Error while deleting movie: ', error);
+  Movie.findById(movieId)
+    .populate("cast")
+    .then((theMovie) =>
+      res.render("movies/movie-details.hbs", { movie: theMovie })
+    )
+    .catch((error) => {
+      console.log("Error while retrieving movie details: ", error);
 
-        next(error);
-      });
+      next(error);
+    });
 });
 
 module.exports = router;
