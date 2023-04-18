ae25541ed9234cc9bbfdbf20334f945e4f389dd6
Yvo de Rooij <110634893+YvodeRooij@users.noreply.github.com>
2023-04-18 16:43:39 +0200

    finished iterations 7,8

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,38 +1,35 @@
-const express=require('express')     //defined express seperately 
-const router = express.Router();     
+const express = require("express"); //defined express seperately
+const router = express.Router();
 
-const celebrityModel=require("../models/Celebrity.model")
+const celebrityModel = require("../models/Celebrity.model");
 
-router.get('/new-celebrity',(req,res)=>{
-    try{
-        res.render('celebrities/new-celebrity');
-       //res.send("newpage with new celebrities")
-    }
-    catch(err){
-        console.log("Error rendering new celebrity",err)
-    }
-})
+router.get("/new-celebrity", (req, res) => {
+  try {
+    res.render("celebrities/new-celebrity");
+    //res.send("newpage with new celebrities")
+  } catch (err) {
+    console.log("Error rendering new celebrity", err);
+  }
+});
 
-router.post('/new-celebrity',async(req,res)=>{
-    try{
-    await celebrityModel.create(req.body)
-    console.log("celebrity created ")
-    res.redirect('/celebrity/celebrities')
-    }
-    catch(err){
-        res.redirect('localhost:3000')
-    }
-    //res.render('/celebrities/new-celebrity')
-})
+router.post("/new-celebrity", async (req, res) => {
+  try {
+    await celebrityModel.create(req.body);
+    console.log("celebrity created ");
+    res.redirect("/celebrity/celebrities");
+  } catch (err) {
+    res.redirect("localhost:3000");
+  }
+  //res.render('/celebrities/new-celebrity')
+});
 
-router.get('/celebrities',async(req,res,next)=>{
-    try{
-        const celebrities=await celebrityModel.find()
-        res.render('celebrities/celebrities',{celebrities:celebrities}) 
-    }
-    catch(err){
-console.log('Error getting celebrity', err)
-    }
-})
+router.get("/celebrities", async (req, res, next) => {
+  try {
+    const celebrities = await celebrityModel.find();
+    res.render("celebrities/celebrities", { celebrities: celebrities });
+  } catch (err) {
+    console.log("Error getting celebrity", err);
+  }
+});
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,31 +1,48 @@
-const express=require('express')     //defined express seperately 
-const router = express.Router();     
+const express = require("express"); //defined express seperately
+const router = express.Router();
 
-const movieModel=require("../models/movie.model")
+const movieModel = require("../models/movie.model");
 
+// GET route to display the form for creating a new movie
+router.get("/new-movie", async (req, res) => {
+  try {
+    res.render("movies/new-movie");
+  } catch (err) {
+    console.log("Error rendering new movie form", err);
+  }
+});
 
+// POST route to create a new movie and save it to the database
+router.post("/new-movie", async (req, res) => {
+  console.log("Received data:", req.body);
+  try {
+    const createdMovie = await movieModel.create(req.body);
+    console.log("Movie created:", createdMovie);
+    res.redirect("/movies");
+  } catch (err) {
+    console.error("Error creating movie:", err);
+    res.render("movies/new-movie", { errorMessage: "Error creating movie: " + err.message });
+  }
+});
 
-router.get('/new-movie',async(req,res)=>{
-    try{
-    await movieModel.create(req.body)
-    //console.log("celebrity created ")
-    res.redirect('/movies/movies')
-    }
-    catch(err){
-        
-        res.render('movies/new-movie');
-    }
+// Route to display the list of movies
+router.get("/", async (req, res, next) => {
+  try {
+    const movies = await movieModel.find();
+    res.render("movies/movies", { movies: movies });
+  } catch (err) {
+    console.log("Error getting movies", err);
+  }
+});
 
-})
-
-router.post('/movies',async(req,res,next)=>{
-    try{
-        const movies=await movieModel.find()
-        res.render('movies/movies',{movies: movies}) 
-    }
-    catch(err){
-console.log('Error getting movies', err)
-    }
-})
+router.get("/movies/:movieID", async (req, res) => {
+  const { movieID } = req.params;
+  try {
+    const detailMovie = await movieModel.findById(movieID).populate("cast");
+    res.render("movies/movie-details", { movie: detailMovie });
+  } catch (err) {
+    console.log("Error getting movie details", err);
+  }
+});
 
 module.exports = router;
