ef16556d649f7feeb564bb204e8175544f11a8c6
raqueldfrutos <raqueldfrutos@gmail.com>
2023-04-18 09:43:52 +0200

    It 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,12 +1,23 @@
 const router = require("express").Router();
 const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
 router.get("/create", (req, res, next) => {
-    res.render("movies/new-movie")
+  Celebrity.find().then(cast => {
+    res.render("movies/new-movie", { cast });
+  });
 });
 
-router.post("create", (req, res, next) => {
-    
-})
+router.post("/create", (req, res, next) => {
+  const { body } = req;
+  Movie.create(body)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch(error => {
+      console.error(error);
+      res.render("movies/new-movie");
+    });
+});
 
 module.exports = router;
