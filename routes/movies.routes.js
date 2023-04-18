09e66a1fce70a5a864e837193cd4c213e1b41040
Silvia Flare <silviaguizan@gmail.com>
2023-04-18 18:12:26 +0200

    iteraction 9

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -49,4 +49,39 @@ router.get("/movies/:id", (req, res, next) => {
     .catch((err) => next(err));
 });
 
+router.get("/movies/:id/delete", (req, res, next) => {
+  MovieModel.findByIdAndDelete(req.params.id)
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((err) => next(err));
+});
+
+// EDIT
+
+/* router.get("/movies/edit/:id", (req, res, next) => {
+    const id = req.params.id;
+    MovieModel.findById(id)
+      .then((movies) => {
+        res.render("movies/edit", { movie: movies });
+      })
+      .catch((err) => next(err));
+  });
+  
+router.post("/movies/edit/:id", (req, res, next) => {
+const { name, genre, plot, cast } = req.body;
+
+MovieModel.findByIdAndUpdate(req.params.id, {
+    name,
+    genre,
+    plot,
+    cast,
+})
+    .then(() => {
+    res.redirect(`/movies/${req.params.id}`);
+    })
+    .catch((err) => next(err));
+});
+ */
+
 module.exports = router;
