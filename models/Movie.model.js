7b43a46f268c6b889dd72ff294d1cf29ae4ff86a
beckjosh <Joshbeck17@gmail.com>
2023-04-18 17:18:27 +0200

    "Second Commit"

---
+++ models/Movie.model.js
@@ -0,0 +1,23 @@
+// const mongoose = require('mongoose');
+// const Celebrities = require('./Celebrity.model');
+// const Schema = mongoose.Schema
+
+// const movieSchema = new Schema({
+
+//     title: {
+//         type: String,
+//         required: true,
+//         unique: true,
+//     },
+//     genre: {
+//         type: String,
+//         required: true
+//     },
+//     plot: {
+//         type: String
+//     },
+//     cast: { [String]},
+
+// })
+// const Movies = mongoose.model("movie", moviesSchema)
+// module.exports = Movies
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,19 +1,38 @@
+const Celebrities = require("../models/Celebrity.model");
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 router.get("/", (req, res, next) => {
     res.render("index");
-    
-  });
-  
-router.get("/celebrities/create", (req,res,next) => {
-    var newCelebrity = {
-        name: req.body.name,
-        occupation: req.body.occupation,
-        catchPhrase: req.body.catchPhrase
-    }
-    res.render("celebrities/new-celebrity")
-})
-router.post("/celebrities/create", (req,res,next) => {
 
+});
+// Route to get and list celebrities
+
+router.get("/celebrities", (req, res, next) => {
+    Celebrities.find()
+        .then(Celebrities => {
+            console.log(Celebrities)
+            res.render("/celebrities", { Celebrities: Celebrities })
+        })
+})
+
+// Route to create a new celebrity form
+
+router.get("/celebrities/create", (req, res, next) => {
+
+    res.render("celebrities/new-celebrity")
+
+})
+// Route to post the new celebrity form on to the webpage
+
+router.post("/celebrities/create", (req, res, next) => {
+    var { name, occupation, catchPhrase } = req.body
+    Celebrities.create({ name, occupation, catchPhrase })
+        .then(createdCelebrity => {
+            console.log(req.body)
+            res.render("celebrities/celebrities")
+        })
+        .catch(err => next(res.render("celebrities/new-celebrity")))
+    console.log("Error")
 })
 module.exports = router;
