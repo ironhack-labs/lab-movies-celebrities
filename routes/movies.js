cc7a9344ae9bdb9fb2bacc8fd65cd8ad04a9102f
Kristof R <kristofrombauts@me.com>
2023-04-18 17:39:52 +0200

    Iteration 10 done

--- routes/movies.js
+++ routes/movies.js
@@ -4,6 +4,8 @@ const Celebrity = require("../models/Celebrity.model")
 
 // all your routes here
 
+// Create a movie
+
 router.get("/movies/create", (req, res, next) => {
     Celebrity.find()
     .then(celebritiesFromDB => {
@@ -22,6 +24,8 @@ router.post("/movies", (req, res, next) => {
     .catch(err => res.redirect(`/movie/create`))
 })
 
+// Render movies page
+
 router.get("/movies", (req, res, next) => {
     Movie.find()
     .then((moviesFromDB) => {
@@ -30,6 +34,8 @@ router.get("/movies", (req, res, next) => {
     })
     .catch(err => next(err))
 })
+
+// Render movies details page
 
 router.get("/movies/:id", (req, res, next) => {
     const movieId = req.params.id
@@ -43,10 +49,43 @@ router.get("/movies/:id", (req, res, next) => {
     .catch(err => next(err))
 })
 
+// Delete a movie
+
 router.post("/movies/delete/:id", (req, res, next) => {
     Movie.findByIdAndDelete(req.params.id)
     .then(() => {
         res.redirect("/movies")
+    })
+    .catch(err => next(err))
+})
+
+// Edit a movie
+
+router.get("/movies/edit/:id", (req, res, next) => {
+    const id = req.params.id
+    Movie.findById(id)
+    .then(movieFromDB => {
+        console.log(movieFromDB)
+        Celebrity.findById(movieFromDB.cast)
+        .then(celebritiesFromDB => {
+            console.log(celebritiesFromDB)
+            res.render("movies/edit-movie", { movie: movieFromDB, celebrity: celebritiesFromDB } )
+        })
+    })
+    .catch(err => next(err))
+})
+
+router.post("/movies/edit/:id", (req, res, next) => {
+    const { title, genre, plot, cast } = req.body
+  
+    Movie.findByIdAndUpdate(req.params.id, {
+        title,
+        genre,
+        plot,
+        cast
+    })
+    .then(() => {
+        res.redirect(`/movies/${req.params.id}`)
     })
     .catch(err => next(err))
 })
