b23feab09715996e72ce2cad4dd6831fc605bee9
Belen Sanchez <belensezrami@gmail.com>
2023-05-06 13:50:45 +0200

    first commit, el crear está, iteración  3

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const router = require("express").Router();
+
+const Celebrity = require('./../models/Celebrity.model')
+
+
+router.get('/celebrities', (req, res, next) => {
+    
+    Celebrity
+    .find()
+    .then(celebrity => res.render('celebrities/celebrities', {celebrity} ))
+    .catch(err => console.log(err))
+
+})
+
+router.get('/celebrities/create', (req, res, next) => {
+    
+    res.render('celebrities/new-celebrity')
+
+})
+
+router.post('/celebrities/create', (req, res, next) => {
+
+    const { name, occupation, catchPhrase } = req.body
+
+    Celebrity
+    .create({ name, occupation, catchPhrase })
+    .then(celebrity => res.redirect('/celebrities'))
+    .catch(err => console.log(err))
+
+})
+
+
+
+
+
+
+
+
+
+
+module.exports = router;
