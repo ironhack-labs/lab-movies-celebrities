6a78cb1640af6d8fe7b57de987724442ada76095
Robson Batista <robson_batista@icloud.com>
2023-05-09 22:11:17 +0100

    [lis.wdft.apr23-Robson]lab-movies-celebrities

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,43 @@
+const Celebrity = require('../models/Celebrity.model');
+const router = require('express').Router();
+
+router.get('/create', (req, res, next) => {
+
+        res.render("celebrities/new-celebrity");
+});
+
+router.post('/create', (req, res, next) => {
+
+    Celebrity.create(req.body)
+
+    .then(() => {
+        console.log("celebrity creada");
+        res.redirect('/celebrities'); 
+    })
+
+    .catch((err) => {
+
+        res.redirect('/celebrities/new-celebrity');
+    })
+    
+});
+
+router.get('/', (req, res, next) => {
+
+    Celebrity.find()
+    .then((allCelebs) => {
+        res.render('celebrities/celebrities.hbs', {
+            allCelebs: allCelebs
+        })
+    })
+
+    .catch((err) => {
+        next(err)
+    })
+
+})
+
+
+
+
+module.exports = router;
