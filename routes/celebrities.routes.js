5968b5520c16d3b01bdfd35ea4138e566854a4f3
MATHEUS CHAVES ARAUJO <002919@CESED.LOCAL>
2023-05-09 10:21:47 -0300

    Iterations 4 to 10 made and lab done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -3,8 +3,9 @@ const router = require("express").Router();
 const Celebritie = require('../models/Celebrity.model');
 
 router.post('/celebrities/create', async (req, res, next) => {
+    const { name, occupation, catchPhrase } = req.body;
+
     try {
-        const { name, occupation, catchPhrase } = req.body;
         const newCelebritie = await Celebritie.create({ name, occupation, catchPhrase });
         res.status(200).json(newCelebritie);
         
@@ -12,6 +13,16 @@ router.post('/celebrities/create', async (req, res, next) => {
         next(error);
     }
     
+})
+
+router.get('/celebrities', async (req, res, next) => {
+    const { page, limit } = req.query;
+    try {
+        const allCelebrities = await Celebritie.find().limit(limit).skip(limit * (page - 1));
+        res.status(200).json(allCelebrities);    
+    } catch (error) {
+        next(error);
+    }
 })
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,4 +1,65 @@
 const router = require("express").Router();
 
+const Movie = require('../models/Movie.model');
+
+router.post('/movies/create', async(req, res, next) => {
+    const { title, genre, plot, cast } = req.body;
+
+    try {
+        const newMovie = await Movie.create({ title, genre, plot, cast });
+        res.status(200).json(newMovie);
+    } catch (error) {
+        next(error);        
+    }
+});
+
+router.get('/movies', async (req, res, next) => {
+    try {
+        const allMovies = await Movie.find();
+        res.status(200).json(allMovies);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.get('/movies/:id', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const movie = await Movie.findById(id, {}).populate();
+        res.status(200).json(movie);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/movies/:id/delete', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        await Movie.findByIdAndDelete (id);
+        res.status(204).json;
+    } catch (error) {
+        next(error);        
+    }
+});
+
+router.get('/movies/:id/edit', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const movie = await Movie.findById(id);
+        res.status(200).json(movie);
+    } catch (error) {
+        next(error);
+    }
+});
+
+router.post('/movies/:id', async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const movie = await Movie.findByIdAndUpdate(id, req.body, {new: true})
+        res.status(200).json(movie)
+    } catch (error) {
+        next(error);
+    }
+})
 
 module.exports = router;
