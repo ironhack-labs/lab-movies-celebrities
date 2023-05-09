755708fede2bf75211b7e8534506962afc6bf9ee
Raquel Barrio <r.barrio.f@gmail.com>
2023-05-09 18:46:21 +0200

    third iteration

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -2,12 +2,12 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-let celebritySchema = new Schema({
-    name: String,
+const celebritySchema = new Schema({
+    name: {type: String, required: true},
     occupation: String,
     catchPhrase: String
 })
 
-let Celebrity = mongoose.model("Celebrity", celebritySchema);
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
 
 module.exports = Celebrity
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -4,9 +4,29 @@ const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
 
 // all your routes here
-router.get("/celebrities/create", (req, res, next) => {
+
+router.get("/create", (req, res, next) => {
     res.render("./celebrities/new-celebrity")
 })
 
+router.post("/create", (req,res,next) => {
+    console.log(req.body)
+    let newCelebrity = {
+        name, occupation, catchPhrase
+    } = req.body
+    Celebrity.create(newCelebrity)
+    .then(response => {
+        res.redirect("/celebrities")
+    })
+    .catch(err => {
+        console.log(err)
+        res.render("./celebrities/new-celebrity")
+
+    })
+})
+
+router.get("/", (req,res,next)=>{
+    res.render("./celebrities/celebrities")
+})
 
 module.exports = router;
