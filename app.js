58aa4580f473734a22f50b6ac9fd9d944d3310d7
Raquel Barrio <r.barrio.f@gmail.com>
2023-05-09 20:35:58 +0200

    halfway iteration 6

--- app.js
+++ app.js
@@ -31,6 +31,9 @@ app.use('/', index);
 const celebritiesRoutes = require("./routes/celebrities.routes")
 app.use("/celebrities/", celebritiesRoutes)
 
+const moviesRoutes = require("./routes/movies.routes")
+app.use("/movies", moviesRoutes)
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,8 +1,11 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
 
 // all your routes here
-
+router.get("/create", (req, res, next) => {
+    res.render("./movies/new-movie")
+})
 
 
 module.exports = router;
