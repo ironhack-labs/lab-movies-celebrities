cd285ddb2995979e8e52263d34e52c5ad67b9f9b
Jonathan Bijl <sannoayo@gmail.com>
2023-05-26 11:32:23 -0400

    Iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,25 @@
 const router = require("express").Router();
 
 // All your routes here
+router.get('/create', async (req, res) => {
+    try {
+      const celebrities = await Celebrity.find();
+      res.render('movies/new-movie', { celebrities });
+    } catch (error) {
+      // Handle the error
+    }
+  });
+  
+  router.post('/create', async (req, res) => {
+    try {
+      const { title, genre, plot, cast } = req.body;
+      const movie = new Movie({ title, genre, plot, cast });
+      await movie.save();
+      res.redirect('/movies');
+    } catch (error) {
+      // Handle the error
+    }
+  });
+  
 
 module.exports = router;
