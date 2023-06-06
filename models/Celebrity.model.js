9b868a4141de573440dc02366b9df5ca199b8407
hymced <hymced@gmail.com>
2023-06-06 17:20:04 +0200

    it2 add Celebrity model

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,13 @@
-//  Add your code here
+const mongoose = require("mongoose")
+const celebritySchema = new mongoose.Schema({
+	name: String,
+	occupation: {
+		type: String,
+		default: "unknown"
+	},
+	catchPhrase: {
+		type: String,
+		required: true
+	},
+});
+module.exports = mongoose.model("Celebrity", celebritySchema);
