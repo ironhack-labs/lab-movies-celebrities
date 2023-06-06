6692ed63a05f68b3e1c5ade24ccf7a596939299a
Philippe Miossec <pmiossec@gmail.com>
2023-06-06 17:18:31 +0200

    It3: Adding New Celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,21 @@
+const Celebrity = require("../models/Celebrity.model");
+
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
 // all your routes here
+router.get('/celebrities/create', (req,res,next)=>{
+    res.render('celebrities/new-celebrity');
+})
+
+router.post("/celebrities/create", async (req, res, next) => {
+    try {
+        const celebrity =  await Celebrity.create({...req.body});
+        res.send("Celebrity created: " + celebrity);
+        // res.redirect("/celebrities");
+    } catch (error) {
+        next(err);
+    }
+})
 
 module.exports = router;
