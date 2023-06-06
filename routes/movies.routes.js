a4c855d1567dfe25619bbc33016c9da7c7dbc6ca
Chiara888 <carolin@family-klose.de>
2023-06-06 19:46:56 +0200

    in the middle of it 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,41 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+const Movie = require("../models/Movie.model");
+
 // all your routes here
 
 module.exports = router;
+
+// GET
+router.get("/movies/create", (req,res,next) => {
+    Movie.find()
+    .then(moviesFromDB => {
+        res.render("movies/new-movie", {moviesArr: moviesFromDB}); 
+    })
+    .catch( e => {
+        console.log("error show a form to create a movie",e);
+        next(e);
+    })
+})
+
+// POST
+router.post("/movies/create", (req,res,next) => {
+
+const newMovie = {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot,
+    cast: req.body.cast
+};
+
+Movie.create(newMovie)
+    .then(() => {
+        res.redirect("/movies");
+    })
+    .catch(e => {
+        console.log("error send the data from the form to this route to create the celebrity and save it to the database", e);
+        res.render("/movies/new-movie");
+        next(e);
+    })
+})
