7665849539c01fb53a14e4c490320c5e29e445db
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-06 18:00:24 +0200

    iteration 3

--- app.js
+++ app.js
@@ -31,6 +31,7 @@ const movies = require('./routes/movies.routes')
 app.use('/', index);
 app.use("/", celebrities);
 app.use("/", movies);
+
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
 
--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,23 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
 
-// all your routes here
+router.get("/celebrities/create", (req, res, next) => {
+  res.render("celebrities/new-celebrity.hbs");
+});
+
+router.post("/celebrities/create", (req, res, next) => {
+  const newCelebrity = {
+    name: req.body.name,
+    occupation: req.body.occupation,
+    catchPhrase: req.body.catchPhrase,
+  };
+  Celebrity.create(newCelebrity)
+    .then((newCelebrity) => {
+      req.redirect("/celebrities");
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
