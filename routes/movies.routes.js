5fadb29199cabece59299e2cc147819bc172904d
pedroxpaulos <paulos.pedro@gmail.com>
2023-06-06 22:29:13 +0100

    Lab done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -59,6 +59,36 @@ router.get('/movies/:id/delete', (req, res, next) => {
 		.catch((e) => console.log(e));
 });
 
+router.get('/movies/:id/edit', async (req, res, next) => {
+	const { id } = req.params;
+	try {
+		const movieInfo = await Movie.findById(id);
+		const allCelebrities = await Celebrity.find();
+
+		res.render('movies/edit-movie', {
+			movie: movieInfo,
+			celebrities: allCelebrities,
+		});
+	} catch (e) {
+		next(e);
+	}
+});
+
+router.post('/movies/:id/edit', (req, res, next) => {
+	const { id } = req.params;
+	const existingMovie = {
+		title: req.body.title,
+		genre: req.body.genre,
+		plot: req.body.plot,
+		cast: req.body.cast,
+	};
+
+	console.log('check this out', existingMovie);
+
+	Movies.findByIdAndUpdate(id, existingMovie)
+		.then((updatedMovies) => res.redirect('/movies')) // go to the details page to see the updates
+		.catch((error) => next(error));
+});
 // 647f5ffb5782b614d6ad6e35
 
 module.exports = router;
