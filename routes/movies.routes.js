3cfcc50fb01392808bccc9879abaa57e11fcbc60
Mary Sue Meissner <marysue.meissner@outlook.com>
2023-06-06 22:09:12 +0200

    It#5 in process

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,4 +1,52 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+
+//Create movies from the DB
+//and read existing celebrities from the DB
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find()
+    .then((celebritiesFromDb) => {
+      res.render("movies/new-movie", { celebritiesArr: celebritiesFromDb });
+    })
+    .catch((e) => {
+      console.log(e, "error getting celebrities");
+    });
+});
+
+// Lists the movies in the DB
+router.get("/movies", (req, res, next) => {
+  Movie.find()
+    // .populate("cast")
+    .then((moviesFromDb) => {
+      const data = { movies: moviesFromDb };
+
+      console.log(data);
+      res.render("movies/movies.hbs", data);
+    })
+    .catch((e) => {
+      console.log(e, "error getting movies");
+    });
+});
+
+router.post("/movies/create", (req, res, next) => {
+  const newMovie = {
+    name: req.body.title,
+    occupation: req.body.genre,
+    catchPhrase: req.body.plot,
+    cast: req.body.cast,
+  };
+
+  Movie.create(newMovie)
+    .then((newMovie) => {
+      res.redirect("/movies");
+    })
+    .catch((e) => {
+      //console.log("error creating new celebrity", e);
+      // next(e);
+      res.render("movies/new-movie");
+    });
+});
 
 module.exports = router;
