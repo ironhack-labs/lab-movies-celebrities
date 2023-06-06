e555187c83bf26d44066835f33a55ef304589f73
Bernard Cardoso <be1.0@hotmail.com>
2023-06-06 20:23:05 +0100

    More Updates

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
 
+
 /* GET Celebrities page */
 router.get("/", (req, res, next) => {
   Celebrity.find()
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,8 +1,48 @@
 const router = require("express").Router();
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
 
 /* GET Movies page */
-router.get("/movies", (req, res, next) => {
-  res.render("movies");
+router.get("/", (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render("movies/movies", { movies });
+    })
+    .catch((error) => {
+      console.log("Error: ", error);
+      res.render("error", { error });
+    });
 });
+
+router.get("/create", (req, res, next) => {
+  Celebrity.find()
+    .then((celebrities) => {
+      res.render("movies/new-movie", { celebrities });
+    })
+    .catch((error) => {
+      console.log("Error: ", err);
+      res.render("error", { err });
+    });
+});
+
+router.post("/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  const newMovie = new Movie({
+    title,
+    genre,
+    plot,
+    cast
+  });
+  // Saving the new movie to the database
+  newMovie.save()
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch(error => {
+      console.log("Error: ", err),
+      res.render("movies/new-movie", { err });
+    });
+});
+
 
 module.exports = router;
