eb622b5a85e9a8ccadb3b6b76727cbf8a5775291
Emilio Moreno <emilio@Emilios-MacBook-Air.local>
2023-06-06 18:16:14 +0200

    iteration 3 completed

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -7,4 +7,25 @@ router.get("/celebrities/create", (req, res, next) => {
 })
 
 
+// iteration 3
+router.post("/celebrities/create", (req, res, next) =>{
+    
+    const newCeleb = {
+        name: req.body.name,
+        occupation: req.body.occupation, 
+        catchPhrase: req.body.catchPhrase
+    };
+    Celebrity.create(newCeleb)
+        .then((newCeleb)=>{ //not sure if newCeleb is what should go here...
+            res.redirect("/celebrities")
+        })
+        .catch( e => {
+            console.log("error creating new celeb", e);
+            res.render("celebrities/new-celebrity")
+            next(e);
+        });
+
+})
+
+
 module.exports = router;
