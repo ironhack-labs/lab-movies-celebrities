cf3115669b9e3a1f3c97101267864b80b4d45834
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-06 18:31:04 +0200

    iteration 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -2,9 +2,30 @@ const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
 const Movie = require("../models/Movie.model");
 
+router.get("/movies/create", (req, res, next) => {
+  Celebrity.find()
+    .then((celebritiesFromDB) => {
+      res.render("movies/new-movie", { celebritiesArr: celebritiesFromDB });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
-
-
-
+router.post("/movies/create", (req, res, next) => {
+  const newMovie = {
+    title: req.body.title,
+    genre: req.body.genre,
+    plot: req.body.plot,
+    cast: req.body.cast,
+  };
+  Movie.create(newMovie)
+    .then((newMovie) => {
+      req.redirect("/movies");
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
 
 module.exports = router;
