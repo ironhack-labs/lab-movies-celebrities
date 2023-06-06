21fcf64e5ba99582f5eab359aece6691d82d411b
linhvnde <linh.nh.tyty@gmail.com>
2023-06-06 20:56:44 +0200

    fixed bug when selecting multiple casts

--- models/Movie.model.js
+++ models/Movie.model.js
@@ -5,7 +5,7 @@ const movieSchema = new Schema(
     title: { type: String },
     genre: { type: String },
     plot: { type: String },
-    cast: {type: Schema.Types.ObjectId, ref: "Celebrity"},
+    cast: [{type: Schema.Types.ObjectId, ref: "Celebrity"}],
   },
   {
     timestamps: true,
