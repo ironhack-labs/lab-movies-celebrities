a4ba03679b1f5a02ac599745f187a39573e197b7
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-06 18:07:02 +0200

    iteration 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -20,4 +20,14 @@ router.post("/celebrities/create", (req, res, next) => {
     });
 });
 
+router.get("/celebrities", (req, res, next) => {
+  Celebrity.find()
+    .then((celebritiesFromDB) => {
+      res.render("celebrities/celebrities", { celebrities: celebritiesFromDB });
+    })
+    .catch((err) => {
+      next(err);
+    });
+});
+
 module.exports = router;
