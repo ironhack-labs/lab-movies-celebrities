d1528b178d5a32d531cde31ff2aca31648f1a717
Fabio Sdringola <fabio.sdringola@gmail.com>
2023-06-06 18:13:19 +0200

    iteration 5

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 
 router.get("/celebrities/create", (req, res, next) => {
   res.render("celebrities/new-celebrity.hbs");
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,6 +1,10 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
+const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 
-// all your routes here
+
+
+
+
 
 module.exports = router;
