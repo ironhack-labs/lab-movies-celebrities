ce4b2dea06789def19bf36f9f6f583409e8901a0
hymced <hymced@gmail.com>
2023-06-06 17:54:51 +0200

    it3 + it4 celebrities

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,32 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
-// all your routes here
+const CelebrityModel = require("../models/Celebrity.model")
+
+// CREATE: display form to create new celebrity
+router.get("/celebrities/create", (req, res, next) => {
+	res.render("celebrities/new-celebrity")
+})
+
+// CREATE: process form
+router.post("/celebrities/create", (req, res, next) => {
+	const {name, occupation, catchPhrase} = req.body
+	const newCelebrity = {name, occupation, catchPhrase}
+	CelebrityModel.create(newCelebrity)
+		.then((cebrityFromDB) => {
+			console.log(cebrityFromDB)
+			res.redirect("/celebrities")
+		})
+		.catch(e => next(e))
+})
+
+// READ: list celebrities
+router.get("/celebrities", (req, res, next) => {
+	CelebrityModel.find()
+		.then((celebritiesFromDB) => {
+			res.render("celebrities/celebrities", {celebrities: celebritiesFromDB})
+		})
+		.catch(e => next(e))
+})
 
 module.exports = router;
