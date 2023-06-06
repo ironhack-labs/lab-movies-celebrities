c7a5ea6438321b8a2c132f9e70e66d48a418d6bd
Philippe Miossec <pmiossec@gmail.com>
2023-06-06 18:15:50 +0200

    It8: movie details

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -6,36 +6,42 @@ const router = require("express").Router();
 
 // all your routes here
 
-router.get('/movies/create', async (req, res, next) => {
-    try {
-        const celebrities = await Celebrity.find();
-        res.render("movies/new-movie.hbs", {celebrities});
-    } catch (error) {
-        next(error);
-    }
-})
+router.get("/movies/create", async (req, res, next) => {
+  try {
+    const celebrities = await Celebrity.find();
+    res.render("movies/new-movie.hbs", { celebrities });
+  } catch (error) {
+    next(error);
+  }
+});
 
+router.post("/movies/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  const movieObj = { title, genre, plot, cast };
+  Movie.create(movieObj)
+    .then((movie) => {
+      res.send("movies/create:" + movie);
+      // res.redirect('/movies')
+    })
+    .catch((error) => next(error));
+});
 
-router.post('/movies/create', (req,res,next)=>{
-    const {title, genre, plot, cast} = req.body;
-    const movieObj = { title, genre, plot, cast };
-    Movie.create(movieObj)
-        .then(movie =>{
-            res.send('movies/create:' + movie)
-            // res.redirect('/movies')
-        }).catch(error=>next(error))
-})
+router.get("/movies", async (req, res, next) => {
+  try {
+    const movies = await Movie.find().populate("cast");
 
+    res.render("movies/movies.hbs", { movies });
+  } catch (error) {
+    next(error);
+  }
+});
 
-router.get('/movies', async (req, res, next) => {
-    try {
-        const movies = await Movie.find()
-            .populate("cast");
-        
-        res.render("movies/movies.hbs", {movies});
-    } catch (error) {
-        next(error);
-    }
-})
+router.get("/movies/:id", (req, res, next) => {
+  Movie.findById(req.params.id).populate('cast')
+    .then((movie) => {
+      res.render("movies/movie-details", movie);
+    })
+    .catch((error) => next(error));
+});
 
 module.exports = router;
