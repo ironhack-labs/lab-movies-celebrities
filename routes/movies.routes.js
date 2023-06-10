48cf1cb9f28d823c7dcb9fca383804fabc0b6e2c
churrundo <churrundo@gmail.com>
2023-06-10 16:01:59 -0600

    implement movie list and new movie form

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,3 +1,41 @@
 const router = require("express").Router();
 
+const Movie = require("../models/Movie.model");
+const Celebrity = require("../models/Celebrity.model");
+
+router.get('/', (req, res, next) => {
+    Movie.find()
+        .then(moviesFromDB => {
+            res.render('movies/movies', { movies: moviesFromDB });
+        })
+        .catch(error => {
+            console.log('Error while getting the movies from the DB: ', error);
+        });
+});
+
+router.get('/create', (req, res, next) => {
+    Celebrity.find()
+    .then((celebritiesFromDB) => {
+        res.render('movies/new-movie', { celebrities: celebritiesFromDB });
+    })
+    .catch((error) => {
+        next(error);
+    });
+});
+
+router.post("/create", (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  const newMovie = new Movie({ title, genre, plot, cast });
+
+  newMovie
+    .save()
+    .then(() => {
+      res.redirect("/movies");
+    })
+    .catch((error) => {
+      console.log("Error while creating new movie: ", error);
+      res.render("movies/new-movie");
+    });
+});
+
 module.exports = router;
