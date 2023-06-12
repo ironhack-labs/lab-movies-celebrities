06952dd50d3df50b515ed7a6adcd8c5c05f23944
Walter Bernal <wltrbm@gmail.com>
2023-06-12 00:55:14 -0700

    Bonus: Celebrity edit

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -29,12 +29,41 @@ router.get('/celebrities', (req, res, next) => {
     .catch(err => console.log('Error @ GET /celebrities: ', err))
 })
 
-// Get /celebrities/:celibrityId
+// GET /celebrities/:celibrityId
 router.get("/celebrities/:celebrityId", (req, res, next) => {
   const { celebrityId: celebrityId } = req.params
   Celebrity.findById(celebrityId)
     .then((celebrity) => res.render("celebrities/celebrity-details.hbs", { celebrity }))
     .catch((err) => console.log("Error @ GET /celebrities/:celebrityId", err))
+})
+
+// GET /celebrities/:celebrityId/edit
+router.get("/celebrities/:celebrityId/edit", (req, res, next) => {
+  const { celebrityId } = req.params
+  Celebrity.findById(celebrityId)
+    .then((celebrity) => {
+      res.render("celebrities/edit-celebrity.hbs", { celebrity })
+    })
+    .catch((err) => console.log("Error @ GET /celebrities/:celebrityId/edit", err))
+})
+
+// POST /celebrities/:celebrityId
+router.post("/celebrities/:celebrityId/edit", (req, res, next) => {
+  const { celebrityId } = req.params
+
+  Celebrity
+    .findById(celebrityId)
+    .then((celebProps) => {
+      let { name, occupation, catchPhrase } = req.body
+
+      if (!name) name= celebProps?.name
+      if (!occupation) occupation = celebProps?.occupation
+      if (!catchPhrase) catchPhrase = celebProps?.catchPhrase
+
+      Celebrity.findByIdAndUpdate(celebrityId, { name, occupation, catchPhrase })
+        .then(() => res.redirect("/celebrities"))
+        .catch((err) => console.log("Error @ POST /celebrities/:celebrityId/edit", err))
+    })
 })
 
 module.exports = router
