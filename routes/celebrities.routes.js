b0a54e469b690c52225723b6c17a10b8eca43b6b
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-06-12 19:48:40 -0600

    Iteration 3 done

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,22 @@
 // starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
 
+
 // all your routes here
+
+router.get('/celebrities/create', (req, res, next) => {
+    res.render('new-celebrity')
+});
+
+router.post('/celebrities/create', (req, res, next) => {
+    const {name , occupation, catchPhrase} = req.body;
+    const newCelebrity = new Celebrity ({name, occupation, catchPhrase})
+
+    newCelebrity.save()
+    .then(() => {
+        res.render('/celebrities')
+    })
+    .catch(err => res.render('/celebrities/new-celebritie', {err}))
+});
 
 module.exports = router;
