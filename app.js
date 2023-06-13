d8faca458dec7899925760fdb9c7e63ecafba028
KRcamara <113925794+KRcamara@users.noreply.github.com>
2023-06-12 19:25:32 -0600

    interaccion1 y2

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const hbs = require('hbs');
 
 const app = express();
 
+
+
 // ℹ️ This function is getting exported from the config folder. It runs most middlewares
 require('./config')(app);
 
@@ -27,6 +29,12 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+
+const celebritiesRoutes = require('./routes/celebrities.routes');
+const moviesRoutes = require('./routes/movies.routes');
+
+app.use('/celebrities', celebritiesRoutes);
+app.use('/movies', moviesRoutes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
