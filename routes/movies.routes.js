ea30835b4104f687c9cf3ccb4265f98d11387b17
KRcamara <113925794+KRcamara@users.noreply.github.com>
2023-06-12 21:07:11 -0600

    Interaccion  5 y 6

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,23 @@
 const router = require("express").Router();
+const Celebrity = require ("../models/Celebrity.model");
+const Movie = require ("../models/Movie.model");
 
 // all your routes here
+router.get("/create", (req, res, next) => {
+    Celebrity.find().then(allCelebrity => {
+        res.render('movies/new-movies',{celebrity: allCelebrity})
+    })
+    
+}) 
+
+router.post("/create",(req, res, next) =>{
+    const {title, genre, plot, cast} = req.body
+
+    Movie
+    .create({ title, genre, plot, cast})
+    .then(() => res.redirect("/movies"))
+    .catch(err => console.log('Error @ POST /craete:', err))
+})  
+
 
 module.exports = router;
