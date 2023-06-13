45af3482ff45dc935c7a8bc9dacd3078bbe832f0
Walter Bernal <wltrbm@gmail.com>
2023-06-12 18:47:45 -0700

    Iteration 3 & 4

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,25 @@
 const router = require("express").Router();
 
 // all your routes here
+const Celebrity = require("../models/Celebrity.model")
+
+router.get('/create', (req, res, next) => { 
+  res.render('celebrities/new-celebrity')
+})
+
+router.post('/create', (req, res, next) => { 
+  const { name, occupation, catchPhrase } = req.body
+  
+  Celebrity
+    .create({ name, occupation, catchPhrase })
+    .then(() => res.redirect('/celebrities'))
+    .catch(err => console.log('Error @ POST /create: ', err))
+})
+
+router.get('/', (req, res, next) => { 
+  Celebrity
+    .find()
+    .then(allCelebrities => res.render('celebrities/celebrities', {celebrity: allCelebrities}))
+})
 
 module.exports = router;
