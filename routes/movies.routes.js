e83f1af88d6e0f9d61feefd417182acc7567dafd
Andresfrla <125943749+Andresfrla@users.noreply.github.com>
2023-06-14 18:28:02 -0600

    done

--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -53,4 +53,36 @@ router.post('/movies/:movieId/delete', (req, res, next) => {
     .catch((err) => console.log("Error POST /movies/:movieId/delete", err))
 })
 
+router.get('/movies/:movieId/edit', (req, res, next) => {
+    const { movieId } = req.params
+    Movie
+      .findById(movieId)
+      .populate('cast')
+      .then(movie => {
+        Celebrity
+          .find()
+          .then(celebrities => {
+            res.render("movies/edit-movie.hbs", { movie, celebrity: celebrities })
+          })
+          .catch((err) => console.log("Err GET /movies/:movieId/edit", err))
+      })
+  })
+  
+  router.post('/movies/:movieId/edit', (req, res, next) => { 
+    const { movieId } = req.params
+  
+    Movie.findById(movieId).then(movieProps => {
+      let { title, genre, plot, cast } = req.body
+  
+      if (!title) title = movieProps?.title
+      if (!genre) genre = movieProps?.genre
+      if (!plot) plot = movieProps?.plot
+      if (!cast) cast = movieProps?.cast
+  
+      Movie.findByIdAndUpdate(movieId, { title, genre, plot, cast })
+        .then(() => res.redirect("/movies"))
+        .catch((err) => console.log("Err POST /movies/:movieId/edit", err))
+    })
+  })
+
 module.exports = router;
