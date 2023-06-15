5a73821e90342d5f34b695493501324fa5aa4a6e
Patricia <marie.lou.trips@gmail.com>
2023-06-16 01:26:54 +0200

    Add Celeb route

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,3 +1,5 @@
+const Celebrity = require("../models/Celebrity.model");
+
 const router = require("express").Router();
 
 // all your routes here
@@ -6,6 +8,38 @@ router.get('/celebrities/create', (req, res, next) => {
     res.render('celebrities/new-celebrity')
 });
 
+router.post('/celebrities/create', (req, res, next) => {
 
+    const { name, occupation, catchPhrase } = req.body;  //defining what req.body contains
+
+    const newCelebrity = new Celebrity({  //defining new instance of celebrity basing on its model
+        name,
+        occupation,
+        catchPhrase
+        })
+
+      newCelebrity.save()
+        .then(ifNewCelebrityIsSaved => {
+        res.redirect('celebrities/celebrities');
+        })
+
+        .catch(ifThereIsAnError => {
+        res.render('celebrities/new-celebrity', { error });
+        })
+});
+
+
+    router.get('/celebrities', (req, res, next) => {
+
+        Celebrity.find()
+            .then(allCelebrities => {
+                res.render('celebrities/celebrities', { allCelebrities });
+            })
+
+            .catch(error => {
+                next(error);
+            })
+        
+    });
 
 module.exports = router;
