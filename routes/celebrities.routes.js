8e7153fa93b0b8771a316efa9c1cc61929a809c6
Patricia <marie.lou.trips@gmail.com>
2023-06-20 00:09:31 +0200

    Iteration 6 shows an error while passing the c

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,17 +1,19 @@
-//const app = require("../app");
+
 const router = require("express").Router();
 const Celebrity = require("../models/Celebrity.model");
+const Movie = require("../models/Movie.model");
 
 // all your routes here
 //IT3: GET & POST mthd, add new celebrities to the database:
 
-router.get('/celebrities/create', (req, res, next) => {
+router.get('/celebrities/create', (req, res) => {
     res.render('celebrities/new-celebrity')
 });
 
-router.post('/celebrities/create', (req, res, next) => {
+router.post('/celebrities/create', (req, res) => {
 
     const { name, occupation, catchPhrase } = req.body;  //defining what req.body contains
+
     const newCelebrity = new Celebrity({  //defining new instance of celebrity basing on its model
         name: name,
         occupation: occupation,
@@ -31,15 +33,15 @@ router.post('/celebrities/create', (req, res, next) => {
 
 //IT4: Listing Our Celebrities:
 
-    router.get('/celebrities', (req, res, next) => {
+    router.get('/celebrities', (req, res) => {
         //Pass all the celebrities from database 
+        
         Celebrity.find()
-            .then(allCelebrities => {
-                res.render('celebrities/celebrities', { allCelebrities }); // render the celebrities/celebrities.hbs view and pass the array of celebrities into the view as a variable
+            .then((celebrities) => {
+                res.render('celebrities/celebrities', { celebrities }); // render the celebrities/celebrities.hbs view and pass the array of celebrities into the view as a variable
             })
-            .catch(error => {
-                next(error);
-            })
+            .catch(() => console.log("error"))
+
     });
 
 
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,11 +1,9 @@
+const router = require("express").Router();
+
 const Celebrity = require("../models/Celebrity.model");
 const Movie = require("../models/Movie.model");
 
-const router = require("express").Router();
-
 // all your routes here
-
-
 
 router.get('/movies/create', (req, res, next) => { res.render('movies/new-movie') });
 
@@ -22,62 +20,49 @@ router.post('/movies/create', (req, res, next) => {
         .then(ifNewMovieIsSaved => {
         res.redirect('/movies');
         })
-
-        .catch(ifThereIsAnError => {
-            res.render('movies/new-movie', console.log({ error }));
-    })
-});
-
-
-//IT6: GET mthd, Adding New Movies:
-router.get('/movies/create', (req, res, next) => {
-    Celebrity.find()
-        .then((celebsFromDB) => {
-            res.render('movies/new-movie')
-    })
-
-    .catch(ifThereIsAnError => { 
-        console.log('Something was wrong. Please try again.')
-        res.render('movies/create', console.log({ error }));
-    })
-})
-
-//IT6: POST mthd Send the data from the form to this route to create the movie 
-router.post('/movies/create', (req, res) => {
-    const { title, genre, plot, cast } = req.body;
-
-    const newMovie = new Movie ({
-    title: req.body.title,
-    genre: req.body.genre,
-    plot: req.body.plot,
-    cast: cast 
-    });
-
-    //and save it to the database
-    newMovie.save()
-        .then(() => {
-            res.redirect('/movies');
-    })
         .catch((err) => {
+            res.render('movies/new-movie')
             console.log(err);
-      });
-  });
-
-
-module.exports = router;
-
+        });
+    })
 
     
-/* ITERATION 6
 
-app.get('/movies/create', (req, res) =>{
+//IT6: GET mthd, Adding New Movies:
+router.get('/movies/create', (req, res) => {
     Celebrity.find()
         .then((celebrities) => {
-            res.render('movies/new-movie', {celebrities});
+            res.render('movies/new-movie', { celebrities });
         })
         .catch((error) => {
-            console.log(error);
-            res.redirect('/');
+            console.log('Something went wrong. Please try again.', error);
+            res.redirect('/movies');
         });
 });
-*/
+
+    //IT6: POST mthd Send the data from the form to this route to create the movie 
+
+    router.post('/movies/create', (req, res) => {
+
+        const { title, genre, plot, cast } = req.body;
+    
+        const newMovie = new Movie({
+            title,
+            genre,
+            plot,
+            cast
+        });
+    
+        newMovie.save()
+            .then((ifNewMovieIsSaved) => {
+                res.render('/movies', { movies } );
+            })
+            .catch((err) => {
+                console.log(err);
+                res.redirect('/movies/create');
+            });
+    });
+    
+
+    module.exports = router;
+
