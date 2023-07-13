af9eafb7357973277d92fcef48c8303339ae66cb
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-07-13 20:26:19 +0200

    Iteration 2

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,22 @@
+const mongoose = require("mongoose");
+
 //  Add your code here
+
+const celebritySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required"],
+  },
+  occupation: {
+    type: String,
+    required: [true, "Occupation is required"],
+  },
+  catchPhrase: {
+    type: String,
+    required: [true, "Catch phrase is required"],
+  },
+});
+
+const Celebrity = mongoose.model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
