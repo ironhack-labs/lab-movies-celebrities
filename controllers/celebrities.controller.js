dee58548b4b1c235b876d67d384f89e39a411289
Juan Diego Sanchez <diegosanchezperez93@gmail.com>
2023-07-15 03:23:05 +0200

    Iteration 4

--- controllers/celebrities.controller.js
+++ controllers/celebrities.controller.js
@@ -1,13 +1,20 @@
-const Celebrity = require('../models/Celebrity.model.js');
+const Celebrity = require("../models/Celebrity.model");
+
+module.exports.list = (req, res, next) => {
+    Celebrity.find()
+      .then(celebrities => {
+        res.render('celebrities/celebrities', { celebrities });
+      })
+  }
 
 module.exports.create = (req, res, next) => {
-    res.render('celebrities/new-celebrity');
+  res.render("celebrities/new-celebrity");
 };
 
 module.exports.doCreate = (req, res, next) => {
-    Celebrity.create(req.body)
-    .then(celebrityDB => {
-        res.redirect('/celebrities')
-    }) 
-    .catch(err => next(err));
-}
+  Celebrity.create(req.body)
+    .then((celebrityDB) => {
+      res.redirect("/celebrities");
+    })
+    .catch((err) => next(err));
+};
