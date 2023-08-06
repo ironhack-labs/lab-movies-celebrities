1af0b5c36b9f734ce4f9b51ad497ffe5a43c3853
AlexOcana <a.ocana.monsalve@hotmail.com>
2023-08-07 00:44:42 +0200

    DONE

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const app = express();
 
 require('./config')(app);
 
-app.locals.title = `Welcome to Movies and Celebrities`;
+app.locals.title = `Movies & celebrities =D `
 
 const index = require('./routes/index');
 app.use('/', index);
@@ -18,6 +18,7 @@ const celebrities = require('./routes/celebrities.routes');
 app.use('/celebrities', celebrities);
 
 const movies = require('./routes/movies.routes');
+const { hydrate } = require('./models/Celebrity.model');
 app.use('/movies', movies);
 
 require('./error-handling')(app);
