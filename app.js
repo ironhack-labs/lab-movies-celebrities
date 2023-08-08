9799ccdc39787a493432089982f90ef669bcf008
Andre Pinto <andre.marpinto@gmail.com>
2023-08-08 20:11:55 +0100

    [LIS-FT-072023]André_Pinto

--- app.js
+++ app.js
@@ -27,6 +27,12 @@ app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+const Celebrity = require('./routes/celebrities.routes') //handling celebrity routes
+app.use('/', Celebrity)
+const Movie = require('./routes/movies.routes') //handling movies routes
+app.use('/', Movie)
+
+
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
