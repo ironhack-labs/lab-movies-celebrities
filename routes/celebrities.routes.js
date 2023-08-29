09ef8514c623593395dd9a23d86f6c0480fc9534
Cyclonej <cyclonejmoto@gmail.com>
2023-08-29 23:02:48 +0200

    I stuggled to get it working on my browser. constantly got error , finally got that working add tried to get further but constantly got issues. guess i need more practice

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,6 +1,50 @@
-// starter code in both routes/celebrities.routes.js and routes/movies.routes.js
 const router = require("express").Router();
-const Celebrity = require('../models/Celebrity.model');
-// all your routes here
+const Celebrity = require("../models/Celebrity.model")
+
+router.get("/celebrities/create", (req, res, next) => {
+    Celebrity.find()
+        .then(celebritiesFromDB => {
+            const data = {
+                celebrities: celebritiesFromDB
+            }
+
+            res.render("celebrities/new-celebrity", data)
+        })
+        .catch(e => {
+            console.log("An error has occured while creating a celebrity", e)
+            next(e)
+        })
+})
+
+router.post("/celebrities/create", (req, res, next) => {
+    const newCelebrity = {
+        name: req.body.name,
+        occupation: req.body.occupation,
+        catchPhrase: req.body.catchPhrase
+    }
+
+    Celebrity.create(newCelebrity)
+    .then((newCelebrity) => {
+        res.redirect("/celebrities")
+    })
+    .catch(e => {
+        console.log("An error has occured while creating a celebrity", e)
+        next(e)
+    })
+})
+
+router.get("/celebrities", (req, res, next) => {
+    Celebrity.find()
+    .then(celebritiesFromDB => {
+        const data = {
+            celebrities: celebritiesFromDB,
+        }
+        res.render("celebrities/celebrities", data)
+    })
+    .catch(e => {
+        console.log("An error has occured while getting the list of celebrities", e)
+        next(e)
+    })
+})
 
 module.exports = router;
