602e9246fdfd0179f5b11243856a0490835f6db1
Priscila Borges <priscila.borges.nl@gmail.com>
2023-08-29 23:20:12 +0200

    done until iteration 7

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,14 @@
-//  Add your code here
+const { Schema, model } = require('mongoose');
+ 
+const CelebritySchema = new Schema(
+  {
+    name: String,
+    occupation: String,
+    catchPhrase: String,    
+  },
+  {
+    timestamps: true
+  }
+);
+ 
+module.exports = model('Celebrity', CelebritySchema);
