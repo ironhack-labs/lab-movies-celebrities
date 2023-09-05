c3f76899b82cc7da4847b64233c8f5c0ff592ce8
Mirabela Tiugan <mirabelatiugan@gmail.com>
2023-09-06 00:22:48 +0200

    Lab partially solved up to point8

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,14 @@
 //  Add your code here
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema(
+  {
+    name: String,
+    occupation: {type:String, enum: ["actor", "singer", "comedian","unknown" ] },
+    catchPhrase: String,
+  }
+);
+
+const Celebrity = model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
