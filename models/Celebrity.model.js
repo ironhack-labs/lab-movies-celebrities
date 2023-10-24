619bad14498fccb539a3f360cb8d051bc30b6f6e
zsaritek <zsaritek@gmail.com>
2023-10-24 17:21:44 +0200

    [WDPTRMT0823]-LAB|Mongoose Movies-Celebrities-Zeynep Sariteke[module2-week3-day3]SOLVED LAB with Bonus Section

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,18 @@
 //  Add your code here
+// item 2
+const { Schema, model } = require("mongoose");
+
+const celebritySchema = new Schema(
+    {
+        name: { type: String, required: true },
+        occupation: { type: String, enum: ['actor', 'comedian', 'singer', 'unknown'] },
+        catchPhrase: { type: String, required: true },
+    },
+    {
+        timestamps: true
+    }
+);
+
+const Celebrity = model("Celebrity", celebritySchema);
+
+module.exports = Celebrity;
