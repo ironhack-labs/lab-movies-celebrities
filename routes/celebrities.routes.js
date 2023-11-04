0b7a06e37a5e99dfa3fbaa51d94e63b4e258fb81
Pablo Portillo <paabloportillo@gmail.com>
2023-11-04 13:51:24 +0100

    Celebrities creados y listados

---
+++ routes/celebrities.routes.js
@@ -0,0 +1,32 @@
+const express = require('express')
+const router = express.Router()
+
+const Celebrity = require('./../models/Celebrity.model')
+
+router.get('/create', (req, res) => {
+    res.render('celebrities/new-celebrity')
+})
+
+router.post('/create', (req, res) => {
+
+    const { name, occupation, catchPhrase } = req.body
+
+    Celebrity
+        .create({ name, occupation, catchPhrase })
+        .then(() => {
+            res.redirect('/celebrities/celebrities')
+        })
+        .catch(() => render('celebrities/new-celebrity'))
+})
+
+router.get('/celebrities', (req, res) => {
+
+
+    Celebrity
+        .find()
+        .then(celebrity => res.render('celebrities/celebrities', { celebrity }))
+        .catch(error => console.log(error))
+})
+
+
+module.exports = router
