9d80cd17b10578c747eaf92434ad74a493eaabd7
Louise Gastardi <l.gastardi@hotmail.com>
2023-11-15 07:31:06 +0100

    #iteration 03

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
+const Celebrity = require('../models/Celebrity.model');
 
-// all your routes here
+router.get('/celebrities/create', (req, res) => {
+  res.render('new-celebrity');
+});
+router.post('celebrities/create', (req, res) => {
+  const { name, occupation, catchPhrase } = req.body;
+  Celebrity.create({ name, occupation, catchPhrase })
+    .then((celebrityFromBD) => console.log(`New celebrity created: ${celebrityFromBD.name}`))
+    .then(() => res.redirect('/celebrities'))
+    .catch((error) => console.error(error).then(() => res.redirect('/new-celebrity')));
+});
 
 module.exports = router;
