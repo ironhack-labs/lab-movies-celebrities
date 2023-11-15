a251c704bb6ee6c56de28231f1ccc32cfa3117df
Louise Gastardi <l.gastardi@hotmail.com>
2023-11-15 06:32:02 +0100

    Iteration #1

--- app.js
+++ app.js
@@ -20,13 +20,18 @@ require('./config')(app);
 
 // default value for title local
 const projectName = 'lab-movies-celebrities';
-const capitalized = string => string[0].toUpperCase() + string.slice(1).toLowerCase();
-
-app.locals.title = `${capitalized(projectName)}- Generated with Ironlauncher`;
+const capitalized = (string) => string[0].toUpperCase() + string.slice(1).toLowerCase();
 
 // 👇 Start handling routes here
 const index = require('./routes/index');
 app.use('/', index);
+app.use('/views');
+
+const moviesRoutes = require('./routes/movies.routes');
+app.use('/', movies.routes);
+
+const celebritiesRoutes = require('./routes/celebrities.routes');
+app.use('/', celebrities.routes);
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require('./error-handling')(app);
