8079bc884047a9250c90ea0539ae8bd82ea565ff
Louise Gastardi <l.gastardi@hotmail.com>
2023-11-18 13:20:30 +0100

    still missisng update movies

--- routes/celebrities.routes.js
+++ routes/celebrities.routes.js
@@ -1,15 +1,24 @@
 const router = require('express').Router();
 const Celebrity = require('../models/Celebrity.model');
 
-router.get('/celebrities/create', (req, res) => {
-  res.render('new-celebrity');
+router.get('/celebrities/create', (req, res, next) => {
+  res.render('celebrities/new-celebrity').catch((error) => next(error));
 });
-router.post('celebrities/create', (req, res) => {
+
+router.post('/celebrities/create', (req, res) => {
   const { name, occupation, catchPhrase } = req.body;
   Celebrity.create({ name, occupation, catchPhrase })
-    .then((celebrityFromBD) => console.log(`New celebrity created: ${celebrityFromBD.name}`))
-    .then(() => res.redirect('/celebrities'))
-    .catch((error) => console.error(error).then(() => res.redirect('/new-celebrity')));
+    .then((celebrityNew) => {
+      console.log(`New celebrity created: `, celebrityNew);
+      res.redirect('/celebrities');
+    })
+    .catch(() => res.render('celebrities/new-celebrity'));
+});
+
+router.get('/celebrities', (req, res, next) => {
+  Celebrity.find()
+    .then((celebritiesFromDb) => res.render('celebrities/celebrities', { celebrities: celebritiesFromDb }))
+    .catch((error) => next(error));
 });
 
 module.exports = router;
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,5 +1,65 @@
 const router = require('express').Router();
+const Movie = require('../models/Movie.model');
+const Celebrity = require('../models/Celebrity.model');
+router.get('/movies/create', (req, res) => {
+  Celebrity.find().then((allCelebrities) => {
+    res.render('movies/new-movie', { cast: allCelebrities });
+  });
+});
 
-// all your routes here
+router.post('/movies/create', (req, res) => {
+  const { title, genre, plot, cast } = req.body;
+  Movie.create({ title, genre, plot, cast })
+    .then((newMovie) => {
+      console.log('New movie created: ', newMovie);
+      res.redirect('/movies');
+    })
+    .catch(() => res.redirect('movies/new-movie'));
+});
+
+router.get('/movies', (req, res, next) => {
+  Movie.find()
+    .then((movieFromDB) => res.render('movies/movies', { movies: movieFromDB }))
+    .catch((error) => next(error));
+});
+
+router.get('/movies/:id', (req, res, next) => {
+  const movieId = req.params.id;
+
+  Movie.findById(movieId)
+    .populate('cast')
+    .then((movieDetails) => res.render(`movies/movie-details`, { movieDetails }))
+    .catch((error) => next(error));
+});
+
+router.get('/movies/:id/edit', (req, res, next) => {
+  const movieId = req.params.id;
+  Movie.findById(movieId)
+    .populate('cast')
+    .then((movieEdit) => {
+      Celebrity.find().then((celebritiesEdit) => res.render(`movies/movie-edit`, { movieEdit, celebritiesEdit }));
+    })
+    .catch((error) => next(error));
+});
+
+router.post('/movies/:id/edit', (req, res, next) => {
+  const { title, genre, plot, cast } = req.body;
+  const movieId = req.params.id;
+
+  Movie.findByIdAndUpdate(movieId, { title, genre, plot, cast }, { new: true })
+    .populate('cast')
+    .then(() => {
+      res.redirect(`/movies/${movieId}`);
+    })
+    .catch((error) => next(error));
+});
+
+router.post('/movies/:id/delete', (req, res, next) => {
+  const movieId = req.params.id;
+
+  Movie.findByIdAndRemove(movieId)
+    .then(() => res.redirect('/movies'))
+    .catch((error) => next(error));
+});
 
 module.exports = router;
