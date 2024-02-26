b6209404c04a0f7f05ae3ba4e2cc34683c4cc9df
Lluis Adan <lluis.adan@gmail.com>
2024-02-26 21:31:47 +0100

    Done without bonus

--- configs/routes.config.js
+++ configs/routes.config.js
@@ -1,4 +1,3 @@
-const express = require('express');
 const router = require("express").Router();
 const celebrities = require("../controllers/celebrities.controller");
 const movies = require("../controllers/movies.controller");
@@ -12,5 +11,9 @@ router.get("/celebrities", celebrities.list);
 router.get("/movies/create", movies.create);
 router.post("/movies/create", movies.doCreate);
 router.get("/movies", movies.list);
+router.get("/movies/:id", movies.details);
+router.post("/movies/:id/delete", movies.delete);
+router.get("/movies/:id/edit", movies.edit);
+router.post("/movies/:id/edit", movies.doEdit);
 
 module.exports = router;
--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,3 +1,4 @@
+const createError = require("http-errors");
 const Movie = require("../models/movie.model");
 const mongoose = require("mongoose");
 
@@ -23,4 +24,67 @@ module.exports.list = (req, res, next) => {
     Movie.find()
         .then((movies) => res.render("movies/list", { movies }))
         .catch((error) => next(error));
+};
+
+module.exports.details = (req, res, next) => {
+    const { id } = req.params;
+    Movie.findById(id)
+        .then((movie) => {
+            if (!movie) {
+            next(createError(404, "Movie not found"));
+            } else {
+            res.render("movies/movie-details", { movie });
+            }
+        })
+        .catch((error) => next(error));
+};
+
+module.exports.delete = (req, res, next) => {
+    const { id } = req.params;
+
+    Movie.findByIdAndDelete(id)
+        .then((movie) => {
+        if (!movie) {
+            next(createError(404, "Movie not found"));
+        } else {
+            res.redirect("/movies");
+        }
+        })
+        .catch((error) => next(error));
+};
+
+module.exports.edit = (req, res, next) => {
+    Movie.findById(req.params.id)
+        .then((movie) => {
+            if (!movie) {
+            next(createError(404, "Movie not found"));
+            } else {
+            res.render(`movies/edit-movie`, { movie });
+            }
+        })
+        .catch(next);
+};
+
+module.exports.doEdit = (req, res, next) => {
+    const movie = req.body;
+    movie.id = req.params.id;
+  
+    Movie.findByIdAndUpdate(req.params.id, req.body, { runValidators: true })
+        .then((movie) => {
+            if (!movie) {
+            next(createError(404, "Movie not found"));
+            } else {
+            res.redirect(`/movies/${movie.id}`);
+            }
+        })
+        .catch((error) => {
+            if (error instanceof mongoose.Error.ValidationError) {
+            res
+                .status(400)
+
+                .render("movies/edit-movie", { movie: req.body, errors: error.errors });
+            } else {
+            next(error);
+            }
+        });
 };
