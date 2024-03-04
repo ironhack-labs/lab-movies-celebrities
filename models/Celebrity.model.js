77821147bf869d7cae85d0b1bc4176f59fe6e1a1
martab <mbsuarez.92@gmail.com>
2024-03-04 18:44:05 +0100

    feat iterations

--- models/Celebrity.model.js
+++ models/Celebrity.model.js
@@ -1,1 +1,20 @@
 //  Add your code here
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const celebritySchema = new Schema(
+    {
+        name: {
+            type: String,
+        },
+        occupation: {
+            type: String,
+        },
+        catchPhrase: {
+            type: String
+        }
+    }
+)
+
+const Celebrity = mongoose.model('Celebrity', celebritySchema);
+module.exports = Celebrity;
